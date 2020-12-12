14f0aa3fc155741eb04ce33b01050f63062471fc
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2020-12-12 23:33:18 +0100

    Expose settings

--- transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
+++ transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
@@ -7,14 +7,14 @@ declare username
 declare password
 
 if ! bashio::fs.directory_exists '/data/transmission'; then
-  mkdir '/data/transmission'
+  mkdir '/share/transmission'
 fi
 
-if ! bashio::fs.file_exists '/data/transmission/settings.json'; then
-  echo "{}" > /data/transmission/settings.json
+if ! bashio::fs.file_exists '/share/transmission/settings.json'; then
+  echo "{}" > /share/transmission/settings.json
 fi
 
-CONFIG=$(</data/transmission/settings.json)
+CONFIG=$(</share/transmission/settings.json)
 
 # Defaults
 CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"/share/incomplete\"")
@@ -35,4 +35,4 @@ CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-username\"=\"${username}\"")
 password=$(bashio::config 'password')
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-password\"=\"${password}\"")
 
-echo "${CONFIG}" > /data/transmission/settings.json
+echo "${CONFIG}" > /share/transmission/settings.json
