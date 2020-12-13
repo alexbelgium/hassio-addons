fbf655cba90c8ed07e9f43fdff998cad52496234
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2020-12-13 12:11:58 +0100

    Update 20-transmission-configuration.sh

--- transmission_ls/rootfs/etc/cont-init.d/20-transmission-configuration.sh
+++ transmission_ls/rootfs/etc/cont-init.d/20-transmission-configuration.sh
@@ -2,9 +2,6 @@
 # ==============================================================================
 
 declare CONFIG
-declare authentication_required
-declare username
-declare password
 declare incomplete_bool
 declare download_dir
 declare incomplete_dir
@@ -33,16 +30,5 @@ CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=${incomplete_bool}"
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-host-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
-
-authentication_required=$(bashio::config 'authentication_required')
-CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-authentication-required\"=${authentication_required}")
-
-
-username=$(bashio::config 'username')
-CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-username\"=\"${username}\"")
-
-
-password=$(bashio::config 'password')
-CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-password\"=\"${password}\"")
 
 echo "${CONFIG}" > /share/transmission/settings.json
