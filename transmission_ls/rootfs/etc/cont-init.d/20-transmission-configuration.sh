e0d0be9d7e21e1cb35a05409adeba042fcce6b42
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2020-12-13 13:16:50 +0100

    Incomplete folder true

--- transmission_ls/rootfs/etc/cont-init.d/20-transmission-configuration.sh
+++ transmission_ls/rootfs/etc/cont-init.d/20-transmission-configuration.sh
@@ -18,15 +18,11 @@ CONFIG=$(</share/transmission/settings.json)
 
 download_dir=$(bashio::config 'download_dir')
 incomplete_dir=$(bashio::config 'incomplete_dir')
-if incomplete_dir=""; then
-  incomplete_bool=false ; else
-  incomplete_bool=true
-fi
 
 # Defaults
 CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"${incomplete_dir}\"")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"${download_dir}\"")
-CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=${incomplete_bool}")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=true")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-host-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
