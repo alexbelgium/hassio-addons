20d0a2ef66898390156b1cfe74e08688c2f12fb0
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2020-12-20 11:08:57 +0100

    Update run.sh

--- joal/run.sh
+++ joal/run.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-
 cp -R -n /joal /data/joal
 
 java -jar /joal/joal.jar --joal-conf=/data/joal
