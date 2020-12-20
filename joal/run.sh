87fae48086808f3b48d8aa464dccc02ee38de658
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2020-12-20 11:43:44 +0100

    Update run.sh

--- joal/run.sh
+++ joal/run.sh
@@ -1,3 +1,3 @@
 cp -R -n /joal /data/joal
 
-java -jar /joal/joal.jar --joal-conf=/data/joal
+java -jar /joal/joal.jar --joal-conf=/data/joal --spring.main.web-environment=true --server.port="8081" --joal.ui.path.prefix="joal" --joal.ui.secret-token="lrMY24Byhx"
