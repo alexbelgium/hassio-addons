b3887ce789f0e228251417e109d2fb7039bef3bb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-01-07 18:39:48 +0100

    Update

--- joal/run.sh
+++ joal/run.sh
@@ -3,4 +3,4 @@ TOKEN=(bashio::config 'secret_token')
 
 cp -R -n /joal /data/joal
 
-java -jar /joal/joal.jar --joal-conf=/data/joal --spring.main.web-environment=true --server.port="8081" --joal.ui.path.prefix="joal" --joal.ui.secret-token=$TOKEN
+java -jar /joal/joal.jar --joal-conf=/data/joal --spring.main.web-environment=true --server.port="8081" --joal.ui.path.prefix="joal" --joal.ui.secret-token="lrMY24Byhx"
