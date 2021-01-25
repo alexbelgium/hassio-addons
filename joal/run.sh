30e07f688eae6026a82a9dda1674dae7464b241d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-01-25 14:29:38 +0100

    removed unused code

--- joal/run.sh
+++ joal/run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bashio
 declare TOKEN
-CONFIG_PATH=/data/options.json
 TOKEN=$(bashio::config 'secret_token')
-
-cp -R -n /joal /data/joal
 
 java -jar /joal/joal.jar --joal-conf=/data/joal --spring.main.web-environment=true --server.port="8081" --joal.ui.path.prefix="joal" --joal.ui.secret-token=$TOKEN
