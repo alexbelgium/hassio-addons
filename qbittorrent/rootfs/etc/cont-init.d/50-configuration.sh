d4eb9dbc6aa212ba8280f159da62e6ebf23999b1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-05 18:32:35 +0100

    Update 50-configuration.sh

--- qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
+++ qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+echo "****************************************************"
 echo "Default username/password : homeassistant/adminadmin"
+echo "****************************************************"
+echo "Configuration can be found in /config/qBittorrent   "
+echo "****************************************************"
