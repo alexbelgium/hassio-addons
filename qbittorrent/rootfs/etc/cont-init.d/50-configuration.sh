f577c09461bbab28003265f578ff20ca30c5d7d7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-05 19:41:13 +0100

    Update 50-configuration.sh

--- qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
+++ qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 
-echo "****************************************************"
-echo "Default username/password : Homeassistant/adminadmin"
-echo "****************************************************"
-echo "Configuration can be found in /config/qBittorrent   "
-echo "****************************************************"
+bashio::log.info "Default username/password : Homeassistant/adminadmin"
+bashio::log.info "Configuration can be found in /config/qBittorrent"
