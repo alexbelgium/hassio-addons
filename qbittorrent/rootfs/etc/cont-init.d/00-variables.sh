b4cb7b08543c0347f97708fbea6d9faf440aa028
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-05 19:52:54 +0100

    Update and rename 50-configuration.sh to 00-variables.sh

---
+++ qbittorrent/rootfs/etc/cont-init.d/00-variables.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/with-contenv bashio
+
+export PUID=$(bashio::config 'PUID')
+export PGID=$(bashio::config 'PGID')
--- qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-bashio::log.info "Default username/password : Homeassistant/adminadmin"
-bashio::log.info "Configuration can be found in /config/qBittorrent"
