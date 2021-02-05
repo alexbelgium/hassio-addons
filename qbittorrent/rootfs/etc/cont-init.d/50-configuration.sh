303da69e63d0d1ea628b46eac50c8b06b358b7bc
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-05 18:21:22 +0100

    Update 50-configuration.sh

--- qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
+++ qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
@@ -1,22 +1,3 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 
-if [ bashio::config.exists 'certfile' ] && [ bashio::config.exists 'keyfile' ]; then
-#Set variables
-CERTFILE=$(bashio::config 'certfile')
-KEYFILE=$(bashio::config 'keyfile')
-bashio::log.info "Ssl certificates options were set..."
-bashio::log.info "... cleaning previous information"
-sed -i '/HTTPS/d' /config/qBittorrent/qBittorrent.conf 
-
-#Check if certificates exist
-  if [ bashio::fs.file_exists "$CERTFILE" ] && [ ! bashio::fs.file_exists "$KEYFILE" ]; then  
-  echo 'WebUI\HTTPS\Enabled=True' >> /config/qbittorrent.conf
-  echo "WebUI\HTTPS\CertificatePath=$CERTFILE" >> /config/qbittorrent.conf
-  echo "WebUI\HTTPS\KeyPath=$KEYFILE" >> /config/qbittorrent.conf
-  bashio::log.info "... and certificates were set"
-  else
-  bashio::log.info "... but certificates not found in $CERTFILE and/or $KEYFILE "
-  fi
-fi
-
-bashio::log.info "Default username/password : admin/adminadmin"
+echo "Default username/password : admin/adminadmin"
