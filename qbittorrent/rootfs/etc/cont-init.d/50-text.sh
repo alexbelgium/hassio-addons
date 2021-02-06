205658b19c089c25ed058446ea9663be60425fbb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-06 11:52:45 +0100

    Added ssl

--- qbittorrent/rootfs/etc/cont-init.d/50-text.sh
+++ qbittorrent/rootfs/etc/cont-init.d/50-text.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/with-contenv bashio
 
+#clean HTTPS data
+sed -i '/HTTPS/d' /config/qBittorrent/qBittorrent.conf
+
+bashio::config.require.ssl
+if bashio::config.true 'ssl'; then
+  bashio::log.info "ssl enabled. If webui don't work, disable ssl or check your certificate paths"
+  #set variables 
+  CERTFILE=$(bashio::config 'certfile') 
+  KEYFILE=$(bashio::config 'keyfile') 
+  #Check if certificates exist 
+  if [ bashio::fs.file_exists "$CERTFILE" ] && [ bashio::fs.file_exists "$KEYFILE" ]; then 
+    echo 'WebUI\HTTPS\Enabled=True' >> /config/qbittorrent.conf 
+    echo "WebUI\HTTPS\CertificatePath=$CERTFILE" >> /config/qbittorrent.conf 
+    echo "WebUI\HTTPS\KeyPath=$KEYFILE" >> /config/qbittorrent.conf 
+  else bashio::log.error "Certificates not found in $CERTFILE and/or $KEYFILE" 
+  fi 
+fi
+
 bashio::log.info "Default username/password : admin/adminadmin"
 bashio::log.info "Configuration can be found in /config/qBittorrent"
