305221b7203e76d116051e47e5dddc9f60be819c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-06 12:01:47 +0100

    Update 50-configuration.sh

--- qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
+++ qbittorrent/rootfs/etc/cont-init.d/50-configuration.sh
@@ -10,10 +10,10 @@ if bashio::config.true 'ssl'; then
   CERTFILE=$(bashio::config 'certfile') 
   KEYFILE=$(bashio::config 'keyfile') 
   #Check if certificates exist 
-  if [ bashio::fs.file_exists "$CERTFILE" ] && [ bashio::fs.file_exists "$KEYFILE" ]; then 
+  if [ bashio::fs.file_exists "/ssl/$CERTFILE" ] && [ bashio::fs.file_exists "/ssl/$KEYFILE" ]; then 
     echo 'WebUI\HTTPS\Enabled=True' >> /config/qbittorrent.conf 
-    echo "WebUI\HTTPS\CertificatePath=$CERTFILE" >> /config/qbittorrent.conf 
-    echo "WebUI\HTTPS\KeyPath=$KEYFILE" >> /config/qbittorrent.conf 
+    echo "WebUI\HTTPS\CertificatePath=/ssl/$CERTFILE" >> /config/qbittorrent.conf 
+    echo "WebUI\HTTPS\KeyPath=/ssl/$KEYFILE" >> /config/qbittorrent.conf 
   else bashio::log.error "Certificates not found in $CERTFILE and/or $KEYFILE" 
   fi 
 fi
