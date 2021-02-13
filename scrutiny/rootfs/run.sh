cf14d2acbc77de92371574ddbe796676e3517191
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-14 00:11:11 +0100

    Update run.sh

--- scrutiny/rootfs/run.sh
+++ scrutiny/rootfs/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-scrutiny-collector-metrics run
+scrutiny-collector-metrics run \
+|| bashio::log.error "Privileged mode is disabled, the addon will stop"; \
+s6-svscanctl -t /var/run/s6/services
