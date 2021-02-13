534b974e891a5cec556b03abb6a65cf0c7fedf53
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-14 00:12:57 +0100

    Update run.sh

--- scrutiny/rootfs/run.sh
+++ scrutiny/rootfs/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
 scrutiny-collector-metrics run && bashio::log.info "Local Devices Added" \
 || bashio::log.error "Privileged mode is disabled, the addon will stop"; \
