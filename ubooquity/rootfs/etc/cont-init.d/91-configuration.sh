723995384ed8a978cb32b63cc5200ef28cbff571
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-16 12:14:54 +0100

    Update 91-configuration.sh

--- ubooquity/rootfs/etc/cont-init.d/91-configuration.sh
+++ ubooquity/rootfs/etc/cont-init.d/91-configuration.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
-# Empty
+###########
+# FOLDERS #
+###########
+FILES=&(bashio::config "FilesFolder")
+COMICS=&(bashio::config "ComicsFolder")
+BOOKS=&(bashio::config "BooksFolder")
+
+jq ".filesPaths.pathString = $FILES" /config/ubooquity/preferences.json
+jq ".comicsPaths.pathString = $COMICS" /config/ubooquity/preferences.json
+jq ".booksPaths.pathString = $BOOKS" /config/ubooquity/preferences.json
+
+mkdir -p FILES COMICS BOOKS
+
+bashio::log.info "Default folders set. Files : $FILES ; Comics : $COMICS ; Books : $BOOKS"
