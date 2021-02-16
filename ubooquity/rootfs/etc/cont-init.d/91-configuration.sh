4537be17a5b8ba4bcc342ec7478fb8c7042c5d7b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-16 14:06:43 +0100

    Update 91-configuration.sh

--- ubooquity/rootfs/etc/cont-init.d/91-configuration.sh
+++ ubooquity/rootfs/etc/cont-init.d/91-configuration.sh
@@ -3,25 +3,9 @@
 ###########
 # FOLDERS #
 ###########
-mkdir -p $FILES $COMICS $BOOKS /config/ubooquity
-chown -R abc:abc $FILES $COMICS $BOOKS /config/ubooquity
-
 FILES=$(jq ".filesPaths[0].pathString" /config/ubooquity/preferences.json)
 COMICS=$(jq ".comicsPaths[0].pathString" /config/ubooquity/preferences.json)
 BOOKS=$(jq ".booksPaths[0].pathString" /config/ubooquity/preferences.json)
 
-mkdir -p $FILES $COMICS $BOOKS /config/ubooquity
-chown -R abc:abc $FILES $COMICS $BOOKS /config/ubooquity
-
-#FILES='"'$(bashio::config "FilesFolder")'"'
-#COMICS='"'$(bashio::config "ComicsFolder")'"'
-#BOOKS='"'$(bashio::config "BooksFolder")'"'
-
-
-#sed -i "s|$CURRENTFILES|$FILES|g" /config/ubooquity/preferences.json
-#sed -i "s|$CURRENTCOMICS|$COMICS|g" /config/ubooquity/preferences.json
-#sed -i "s|$CURRENTBOOKS|$BOOKS|g" /config/ubooquity/preferences.json
-
-#mkdir -p FILES COMICS BOOKS
-
-#bashio::log.info "Default folders set. Files : $FILES ; Comics : $COMICS ; Books : $BOOKS"
+mkdir -p $FILES $COMICS $BOOKS /config/ubooquity || true
+chown -R abc:abc $FILES $COMICS $BOOKS /config/ubooquity || true
