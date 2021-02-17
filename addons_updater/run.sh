08152cd110964060e04fb70c6bcdb17cf8e713a8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-17 22:24:30 +0100

    Update run.sh

--- addons_updater/run.sh
+++ addons_updater/run.sh
@@ -39,7 +39,7 @@ for addons in $(bashio::config "addon|keys"); do
       else
         LOGINFO="... $SLUG : updating ${REPOSITORY}" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
         cd "/data/$BASENAME"
-        git pull --rebase || "rm -r /data/$BASENAME && cd /data/ && git clone https://github.com/${REPOSITORY}"
+        git pull --rebase
       fi
 
       #Define the folder addon
