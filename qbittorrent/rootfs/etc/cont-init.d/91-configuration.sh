a260e2506b57ba86c4fed21f8eef87b2c3eff121
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-20 09:31:13 +0100

    Update 91-configuration.sh

--- qbittorrent/rootfs/etc/cont-init.d/91-configuration.sh
+++ qbittorrent/rootfs/etc/cont-init.d/91-configuration.sh
@@ -91,7 +91,8 @@ if bashio::config.has_value 'customUI'; then
   ### Download WebUI
   case $CUSTOMUI in
   "vuetorrent")
-    curl -S -J -L -o /data/webui/release.zip $(curl -s https://api.github.com/repos/WDaan/VueTorrent/releases/latest | grep -o "http*.zip") >/dev/null
+    curl -s -S -J -L -o /data/webui/release.zip $(curl -s https://api.github.com/repos/WDaan/VueTorrent/releases/latest | grep -o "http.*vuetorrent.zip") >/dev/null \
+    || curl -s -S -J -L -o /data/webui/release.zip $(curl -s https://api.github.com/repos/WDaan/VueTorrent/releases/latest | grep -o "http.*release.zip") >/dev/null \
     ;;
 
   "qbit-matUI")
