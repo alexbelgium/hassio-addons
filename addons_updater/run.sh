ab7affe797bef7f90d6b0d0ed41ed38477662d79
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-02-21 08:47:19 +0100

    change tag update from version to upstream

--- addons_updater/run.sh
+++ addons_updater/run.sh
@@ -3,8 +3,8 @@
 bashio::log.info "Checking status of referenced repositoriess..."
 VERBOSE=$(bashio::config 'verbose')
 
-#Defining github value      
-LOGINFO="... github authentification" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
+#Defining github value
+LOGINFO="... github authentification" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
 
 GITUSER=$(bashio::config 'gituser')
 GITPASS=$(bashio::config 'gitpass')
@@ -16,94 +16,92 @@ git config --system user.password ${GITPASS}
 git config --system user.email ${GITMAIL}
 
 if bashio::config.has_value 'gitapi'; then
-LOGINFO="... setting github API" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+  LOGINFO="... setting github API" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
 
-export GITHUB_API_TOKEN=$(bashio::config 'gitapi')
+  export GITHUB_API_TOKEN=$(bashio::config 'gitapi')
 fi
 
-LOGINFO="... parse addons" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
+LOGINFO="... parse addons" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
 
 for addons in $(bashio::config "addon|keys"); do
-    SLUG=$(bashio::config "addon[${addons}].slug")
-    REPOSITORY=$(bashio::config "addon[${addons}].repository")
-    UPSTREAM=$(bashio::config "addon[${addons}].upstream")
-    BETA=$(bashio::config "addon[${addons}].beta")
-    FULLTAG=$(bashio::config "addon[${addons}].fulltag")
-    BASENAME=$(basename "https://github.com/$REPOSITORY")
-  
-      #Create or update local version
-      if [ ! -d /data/$BASENAME ]; then
-        LOGINFO="... $SLUG : cloning ${REPOSITORY}" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
-        cd /data/
-        git clone "https://github.com/${REPOSITORY}"
-      else
-        LOGINFO="... $SLUG : updating ${REPOSITORY}" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-        cd "/data/$BASENAME"
-        git pull --rebase || git reset --hard
-      fi
+  SLUG=$(bashio::config "addon[${addons}].slug")
+  REPOSITORY=$(bashio::config "addon[${addons}].repository")
+  UPSTREAM=$(bashio::config "addon[${addons}].upstream")
+  BETA=$(bashio::config "addon[${addons}].beta")
+  FULLTAG=$(bashio::config "addon[${addons}].fulltag")
+  BASENAME=$(basename "https://github.com/$REPOSITORY")
 
-      #Define the folder addon
-      LOGINFO="... $SLUG : checking slug exists in repo" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-      cd /data/${BASENAME}/${SLUG} || bashio::log.error "$SLUG addon not found in this repository. Exiting." exit
-  
-      #Find current version
-      LOGINFO="... $SLUG : get current version" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-      CURRENT=$(jq .version config.json) || bashio::log.error "$SLUG addon version in config.json not found. Exiting." exit
-      
-#Prepare tag flag
-if [ ${FULLTAG} = true ]; then
-LOGINFO="... $SLUG : fulltag is on" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-FULLTAG="--format tag"
-else
-LOGINFO="... $SLUG : fulltag is off" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-FULLTAG=""
-fi 
+  #Create or update local version
+  if [ ! -d /data/$BASENAME ]; then
+    LOGINFO="... $SLUG : cloning ${REPOSITORY}" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+    cd /data/
+    git clone "https://github.com/${REPOSITORY}"
+  else
+    LOGINFO="... $SLUG : updating ${REPOSITORY}" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+    cd "/data/$BASENAME"
+    git pull --rebase || git reset --hard
+  fi
 
-      #If beta flag, select beta version
-      if [ ${BETA} = true ]; then
-      LOGINFO="... $SLUG : beta is on" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-      LASTVERSION=$(lastversion --pre "https://github.com/$UPSTREAM" $FULLTAG)
-      else 
-      LOGINFO="... $SLUG : beta is off" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
-      LASTVERSION=$(lastversion "https://github.com/$UPSTREAM" $FULLTAG)
-      fi
+  #Define the folder addon
+  LOGINFO="... $SLUG : checking slug exists in repo" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+  cd /data/${BASENAME}/${SLUG} || bashio::log.error "$SLUG addon not found in this repository. Exiting." exit
 
-     # Take only into account first part of tag
-     # CURRENT=${CURRENT:0:${#LASTVERSION}}
+  #Find current version
+  LOGINFO="... $SLUG : get current version" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+  CURRENT=$(jq .upstream config.json) || bashio::log.error "$SLUG addon upstream tag not found in config.json. Exiting." exit
 
-     # Add brackets
-     LASTVERSION='"'$LASTVERSION'"'
+  #Prepare tag flag
+  if [ ${FULLTAG} = true ]; then
+    LOGINFO="... $SLUG : fulltag is on" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+    FULLTAG="--format tag"
+  else
+    LOGINFO="... $SLUG : fulltag is off" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+    FULLTAG=""
+  fi
 
-     # Update if needed 
-    if [ ${CURRENT} != ${LASTVERSION} ]; then
-      LOGINFO="... $SLUG : update from $CURRENT to $LASTVERSION" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-        
-        #Change all instances of version
-      LOGINFO="... $SLUG : updating files" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-      files=$(grep -rl ${CURRENT} /data/${BASENAME}/${SLUG}) && echo $files | xargs sed -i "s/${CURRENT}/${LASTVERSION}/g"
-      
-      #Update changelog
-      touch /data/${BASENAME}/${SLUG}/CHANGELOG.md
-      sed -i "1i\- Update to latest version from $UPSTREAM" /data/${BASENAME}/${SLUG}/CHANGELOG.md 
-      sed -i "1i\## ${LASTVERSION:1:-1}" /data/${BASENAME}/${SLUG}/CHANGELOG.md
-      sed -i "1i\ " /data/${BASENAME}/${SLUG}/CHANGELOG.md
-           
-if [ $files != null ]; then
+  #If beta flag, select beta version
+  if [ ${BETA} = true ]; then
+    LOGINFO="... $SLUG : beta is on" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+    LASTVERSION=$(lastversion --pre "https://github.com/$UPSTREAM" $FULLTAG)
+  else
+    LOGINFO="... $SLUG : beta is off" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+    LASTVERSION=$(lastversion "https://github.com/$UPSTREAM" $FULLTAG)
+  fi
+
+  # Add brackets
+  LASTVERSION='"'$LASTVERSION'"'
+
+  # Update if needed
+  if [ ${CURRENT} != ${LASTVERSION} ]; then
+    LOGINFO="... $SLUG : update from $CURRENT to $LASTVERSION" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+
+    #Change all instances of version
+    LOGINFO="... $SLUG : updating files" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+    files=$(grep -rl ${CURRENT} /data/${BASENAME}/${SLUG}) && echo $files | xargs sed -i "s/${CURRENT}/${LASTVERSION}/g" # change all tags in all addon files
+    jq --arg LASTVERSION "$LASTVERSION" '.upstream = $LASTVERSION' /data/${BASENAME}/${SLUG}/config.json                 # Correct config upstream
+
+    #Update changelog
+    touch /data/${BASENAME}/${SLUG}/CHANGELOG.md
+    sed -i "1i\- Update to latest version from $UPSTREAM" /data/${BASENAME}/${SLUG}/CHANGELOG.md
+    sed -i "1i\## ${LASTVERSION:1:-1}" /data/${BASENAME}/${SLUG}/CHANGELOG.md
+    sed -i "1i\ " /data/${BASENAME}/${SLUG}/CHANGELOG.md
+
+    if [ $files != null ]; then
       git commit -m "Update to $LASTVERSION" $files || true
       git commit -m "Update to $LASTVERSION" /data/${BASENAME}/${SLUG}/CHANGELOG.md || true
-      
+
       #Git commit and push
-      LOGINFO="... $SLUG : push to github" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi   
-      git remote set-url origin "https://${GITUSER}:${GITPASS}@github.com/${REPOSITORY}" |  echo                                  
-      git push | echo "No changes"                                                   
-                                                                     
-      #Log                                                 
-      bashio::log.info "$SLUG updated to $LASTVERSION"  
-else
-bashio::log.error "... $SLUG : update failed"
-fi
-      
-    else                                                                                
-      bashio::log.info "Addon $SLUG is already up-to-date"                        
-    fi                                                               
-done                                                                 
+      LOGINFO="... $SLUG : push to github" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
+      git remote set-url origin "https://${GITUSER}:${GITPASS}@github.com/${REPOSITORY}" | echo
+      git push | echo "No changes"
+
+      #Log
+      bashio::log.info "$SLUG updated to $LASTVERSION"
+    else
+      bashio::log.error "... $SLUG : update failed"
+    fi
+
+  else
+    bashio::log.info "Addon $SLUG is already up-to-date"
+  fi
+done
