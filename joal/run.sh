5b42246e066db0f0a5f5bba538797425eb12d126
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-03-07 17:16:22 +0100

    Update run.sh

--- joal/run.sh
+++ joal/run.sh
@@ -2,7 +2,7 @@
 declare TOKEN
 TOKEN=$(bashio::config 'secret_token')
 
-mv /data/joal/config.json /tmp/ || true
+mv -f /data/joal/config.json / || true
 
 curl -s -L -o /tmp/joal.tar.gz "https://github.com/anthonyraymond/joal/releases/download/2.1.24/joal.tar.gz"
 mkdir -p /data/joal
@@ -10,6 +10,6 @@ tar zxvf /tmp/joal.tar.gz -C /data/joal
 chown -R $(id -u):$(id -g) /data/joal
 rm /data/joal/jack-of*
 
-mv /tmp/config.json /data/joal/ || true
+mv -f /config.json /data/joal/ || true
 
 java -jar /joal/joal.jar --joal-conf=/data/joal --spring.main.web-environment=true --server.port="8081" --joal.ui.path.prefix="joal" --joal.ui.secret-token=$TOKEN
