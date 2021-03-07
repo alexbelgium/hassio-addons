781394e1652d37e33e7af00ccf7bc8bdbd67ae53
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-03-07 16:24:46 +0100

    Add joal download code

--- joal/run.sh
+++ joal/run.sh
@@ -2,4 +2,10 @@
 declare TOKEN
 TOKEN=$(bashio::config 'secret_token')
 
+curl -s -L -o /tmp/joal.tar.gz "https://github.com/anthonyraymond/joal/releases/download/UPSTREAM/joal.tar.gz"
+mkdir -p /data/joal
+tar zxvf /tmp/joal.tar.gz -C /data/joal
+chown -R $(id -u):$(id -g) /data/joal
+rm /data/joal/jack-of*
+
 java -jar /joal/joal.jar --joal-conf=/data/joal --spring.main.web-environment=true --server.port="8081" --joal.ui.path.prefix="joal" --joal.ui.secret-token=$TOKEN
