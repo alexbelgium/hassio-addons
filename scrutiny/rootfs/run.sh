fd4db20afe5da00d2d095551d275aefce67b9df3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-03-07 10:07:32 +0100

    Update run.sh

--- scrutiny/rootfs/run.sh
+++ scrutiny/rootfs/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
-scrutiny-collector-metrics run && bashio::log.info "Local Devices Added"
+scrutiny-collector-metrics run && bashio::log.info "Local Devices Added" || bashio::log.error "Local Devices Not Added" 
 #|| bashio::log.error "Privileged mode is disabled, the addon will stop"
 # ; \ s6-svscanctl -t /var/run/s6/services
