038df7b76c5de975aa7c9423efae29e77a0e7a44
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-03 12:14:18 +0200

    Update run.sh

--- scrutiny/rootfs/run.sh
+++ scrutiny/rootfs/run.sh
@@ -3,20 +3,6 @@
 # wait for scrutiny to load
 bashio::net.wait_for 8080
 
-#######################
-# VIEWPORT CORRECTION #
-#######################
-
- # correct viewport bug
-grep -rl '"lt-md":"(max-width:  959px)"' /app | xargs sed -i 's|"lt-md":"(max-width:  959px)"|"lt-md":"(max-width:  100px)"|g' || true
-    
-######################
-# API URL CORRECTION #
-######################
-
-# allow true url for ingress 
-grep -rl '/api/' /app | xargs sed -i 's|/api/|api/|g' || true
-grep -rl 'api/' /app | xargs sed -i 's|api/|./api/|g' || true
 
 #####################
 # ADD LOCAL DEVICES #
