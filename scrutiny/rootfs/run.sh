e5331db8fc68ce01affc63334455bf6662e90497
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-03 10:45:22 +0200

    Update run.sh

--- scrutiny/rootfs/run.sh
+++ scrutiny/rootfs/run.sh
@@ -3,5 +3,24 @@
 # wait for scrutiny to load
 bashio::net.wait_for 8080
 
+#######################
+# VIEWPORT CORRECTION #
+#######################
+
+ # correct viewport bug
+grep -rl '"lt-md":"(max-width:  959px)"' /app | xargs sed -i 's|"lt-md":"(max-width:  959px)"|"lt-md":"(max-width:  100px)"|g' || true
+    
+######################
+# API URL CORRECTION #
+######################
+
+# allow true url for ingress 
+grep -rl '/api/' /app | xargs sed -i 's|/api/|api/|g' || true
+grep -rl 'api/' /app | xargs sed -i 's|api/|./api/|g' || true
+
+#####################
+# ADD LOCAL DEVICES #
+#####################
+
 # search for local devices
 scrutiny-collector-metrics run >/dev/null && bashio::log.info "Local Devices Added" || bashio::log.error "Local Devices Not Added" 
