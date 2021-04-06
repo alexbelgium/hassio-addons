5852fb71fbc181b5ca86718117640a7b02f52a5f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-06 21:53:22 +0200

    Create up.sh

---
+++ qbittorrent/rootfs/etc/openvpn/up.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bash
+
+WEBUI_PORT=${WEBUI_PORT:-8080}
+
+if [ -n "${UMASK_SET}" ] && [ -z "${UMASK}" ]; then
+  echo -e "You are using a legacy method of defining umask\nplease update your environment variable from UMASK_SET to UMASK\nto keep the functionality after July 2021"
+  umask ${UMASK_SET}
+fi
+
+exec /usr/bin/transmission-daemon --foreground --config-dir /data/transmission
