71ce3d439e196915088dcc27c5ef3f4225033f06
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-06 23:02:53 +0200

    Update up-qbittorrent.sh

--- qbittorrent/rootfs/etc/openvpn/up-qbittorrent.sh
+++ qbittorrent/rootfs/etc/openvpn/up-qbittorrent.sh
@@ -1,10 +1,5 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 
 WEBUI_PORT=${WEBUI_PORT:-8080}
-
-if [ -n "${UMASK_SET}" ] && [ -z "${UMASK}" ]; then
-  echo -e "You are using a legacy method of defining umask\nplease update your environment variable from UMASK_SET to UMASK\nto keep the functionality after July 2021"
-  umask ${UMASK_SET}
-fi
 
 exec s6-setuidgid abc /usr/bin/qbittorrent-nox --webui-port="${WEBUI_PORT}"
