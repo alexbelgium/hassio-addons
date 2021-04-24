827f8ed008cedae8709121fb6fbe18d916a7480c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-24 10:54:45 +0200

    Update and rename 92-smb_mounts_v1.1.sh to 92-smb_mounts_v1.2.sh

--- qbittorrent/rootfs/etc/cont-init.d/92-smb_mounts_v1.2.sh
+++ qbittorrent/rootfs/etc/cont-init.d/92-smb_mounts_v1.2.sh
@@ -12,13 +12,6 @@ if bashio::config.has_value 'networkdisks'; then
     CIFS_USERNAME=$(bashio::config 'cifsusername')
     CIFS_PASSWORD=$(bashio::config 'cifspassword')
 
-    # Allow SMB1
-    if bashio::config.true 'smbv1'; then
-      SMBVERS=",vers=1.0"
-    else
-      SMBVERS=",vers=2.1"
-    fi
-
     # Mounting disks
     for disk in ${MOREDISKS//,/ }  # Separate comma separated values
     do
@@ -27,40 +20,48 @@ if bashio::config.has_value 'networkdisks'; then
       diskname=${diskname##*/} # Get only last part of the name
       mkdir -p /mnt/$diskname  # Create dir
       chown -R root:root /mnt/$diskname  # Permissions
-      mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD$SMBVERS $disk /mnt/$diskname && \
+      mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD $disk /mnt/$diskname && \
       bashio::log.info "... $disk successfully mounted to /mnt/$diskname"
 
       # if Fail test smbv1
       if [ $? != 0 ]; then
         mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD,vers=1.0 $disk /mnt/$diskname && \
-        bashio::log.info "... $disk successfully mounted to /mnt/$diskname" && \
-        bashio::log.error "Your smb share uses smbv1. Please check the relevant option in the addons options." # Mount share
+        bashio::log.info "... $disk successfully mounted to /mnt/$diskname with smbv1"
       fi
 
       # Test smbv2.1
       if [ $? != 0 ]; then
         mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD,vers=2.1 $disk /mnt/$diskname && \
-        bashio::log.info "... $disk successfully mounted to /mnt/$diskname" && \
-        bashio::log.error "Your smb share uses smbv2.1, please remove smbv1 option."
+        bashio::log.info "... $disk successfully mounted to /mnt/$disk name with smbv2.1"
       fi
 
       # Test smbv3
       if [ $? != 0 ]; then
         mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD,vers=3.0 $disk /mnt/$diskname && \
-        bashio::log.info "... $disk successfully mounted to /mnt/$diskname" && \
-        bashio::log.error "Your smb share uses smbv3."
+        bashio::log.info "... $disk successfully mounted to /mnt/$disk name with smbv3"
       fi
 
       # Test ntlmv2
       if [ $? != 0 ]; then
         mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD,sec=ntlmv2 $disk /mnt/$diskname && \
-        bashio::log.info "... $disk successfully mounted to /mnt/$diskname" && \
-        bashio::log.error "Your smb share requires ntlmv2."
+        bashio::log.info "... $disk successfully mounted to /mnt/$disk name with ntlmv2"
+      fi
+
+      # Test ntlmv2 and smbv3
+      if [ $? != 0 ]; then
+        mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD,sec=ntlmv2,vers=2.1 $disk /mnt/$diskname && \
+        bashio::log.info "... $disk successfully mounted to /mnt/$disk name with ntlmv2 and smbv2.1"
+      fi
+
+      # Test ntlmv2 and smbv3
+      if [ $? != 0 ]; then
+        mount -t cifs -o username=$CIFS_USERNAME,password=$CIFS_PASSWORD,sec=ntlmv2,vers=3 $disk /mnt/$diskname && \
+        bashio::log.info "... $disk successfully mounted to /mnt/$disk name with ntlmv2 and smbv3"
       fi
 
       # if still fail 
       if [ $? != 0 ]; then
-        bashio::log.error "Unable to mount $disk to /mnt/$diskname with username $CIFS_USERNAME, $CIFS_PASSWORD . Please check your remote share path, the username and password, and try to check the smbv1 box in option if your share is using smb v1" # Mount share
+        bashio::log.critical "Unable to mount $disk to /mnt/$diskname with username $CIFS_USERNAME, $CIFS_PASSWORD . Please check your remote share path, the username and password, and try to check the smbv1 box in option if your share is using smb v1" # Mount share
       fi
 
     done
