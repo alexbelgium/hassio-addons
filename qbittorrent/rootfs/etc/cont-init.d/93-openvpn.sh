e719d3ab01b18a4e2f98af999771316752abf08f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-27 17:47:54 +0200

    Update 93-openvpn.sh

--- qbittorrent/rootfs/etc/cont-init.d/93-openvpn.sh
+++ qbittorrent/rootfs/etc/cont-init.d/93-openvpn.sh
@@ -46,7 +46,6 @@ if bashio::config.true 'openvpn_enabled'; then
       # Bind tun0
       sed -i "$LINE i\Connection\\\Interface=tun0" qBittorrent.conf
       sed -i "$LINE i\Connection\\\InterfaceName=tun0" qBittorrent.conf
-      sed -i "$LINE i\Connection\\\PortRangeMin=$1" qBittorrent.conf
   else
       bashio::log.error "qBittorrent config file doesn't exist, openvpn must be added manually to qbittorrent options "
       exit 1
@@ -57,7 +56,6 @@ else
   ##################
   # Ensure no redirection by removing the direction tag
   cd /config/qBittorrent/
-  sed -i '/PortRangeMin/d' qBittorrent.conf
   sed -i '/Interface/d' qBittorrent.conf 
   sed -i "$LINE i\Connection\\\Interface=" qBittorrent.conf
   sed -i "$LINE i\Connection\\\InterfaceName=" qBittorrent.conf
