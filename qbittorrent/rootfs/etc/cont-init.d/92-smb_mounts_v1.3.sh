a30d5a30b1b3c856f496dcb51668707f000828e4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-04-29 13:56:58 +0200

    Update 92-smb_mounts_v1.3.sh

--- qbittorrent/rootfs/etc/cont-init.d/92-smb_mounts_v1.3.sh
+++ qbittorrent/rootfs/etc/cont-init.d/92-smb_mounts_v1.3.sh
@@ -45,11 +45,11 @@ if bashio::config.has_value 'networkdisks'; then
         for SMBVERS in ",vers=2.1" ",vers=3.0" ",vers=1.0" ",vers=3.1.1" ",vers=2.0" ",vers=3.0.2"
         do
            bashio::log.warning "... trying to mount with $SMBVERS"
-           mount -t cifs -o rw,iocharset=utf8,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$DOMAIN$SMBVERS $disk /mnt/$diskname && MOUNTED=true && break  || MOUNTED=false
+           mount -t cifs -o rw,iocharset=utf8,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$DOMAIN$SMBVERS $disk /mnt/$diskname 2>/dev/null && MOUNTED=true && break  || MOUNTED=false
            for SECVERS in ",sec=ntlmi" ",sec=ntlmv2" ",sec=ntlmv2i" ",sec=ntlmssp" ",sec=ntlmsspi" ",sec=ntlm" ",sec=krb5i" ",sec=krb5"
            do
                 bashio::log.warning "... trying to mount with $SMBVERS $SECVERS"
-                mount -t cifs -o rw,iocharset=utf8,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$DOMAIN$SMBVERS$SECVERS $disk /mnt/$diskname && MOUNTED=true && break 2 && break || MOUNTED=false
+                mount -t cifs -o rw,iocharset=utf8,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$DOMAIN$SMBVERS$SECVERS $disk /mnt/$disk name 2>/dev/null && MOUNTED=true && break 2 && break || MOUNTED=false
            done
         done
       fi
