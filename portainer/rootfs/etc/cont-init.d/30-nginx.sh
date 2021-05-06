95f4220448076bb3b6cb582afdba9adf010e4801
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-05-06 14:15:21 +0200

    correct nginx

---
+++ portainer/rootfs/etc/cont-init.d/30-nginx.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/with-contenv bashio
+
+#################
+# NGINX SETTING #
+#################
+
+declare admin_port
+declare portainer_protocol=http
+
+# Generate Ingress configuration
+if bashio::config.true 'ssl'; then
+portainer_protocol=https
+
+bashio::var.json \
+    interface "$(bashio::addon.ip_address)" \
+    port "^$(bashio::addon.ingress_port)" \
+    protocol "${portainer_protocol}" \
+    certfile "$(bashio::config 'certfile')" \
+    keyfile "$(bashio::config 'keyfile')" \
+    ssl "^$(bashio::config 'ssl')" \
+    | tempio \
+        -template /etc/nginx/templates/ingress.gtpl \
+        -out /etc/nginx/servers/ingress.conf
+fi
