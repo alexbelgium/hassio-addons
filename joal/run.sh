d905e783b2ba1bb3bfb1dc9fad4575f1a11f7eed
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-05-12 11:22:42 +0200

    add log info

--- joal/run.sh
+++ joal/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bashio
+bashio::log.info "Starting addon..."
 declare TOKEN
 TOKEN=$(bashio::config 'secret_token')
 
@@ -9,9 +10,11 @@ mkdir -p /data/joal
 tar zxvf /tmp/joal.tar.gz -C /data/joal >/dev/null
 chown -R $(id -u):$(id -g) /data/joal
 rm /data/joal/jack-of*
+bashio::log.info "... Joal updated"
 
 mv -f /config.json /data/joal/ || true
 java -jar /joal/joal.jar --joal-conf=/data/joal --spring.main.web-environment=true --server.port="8081" --joal.ui.path.prefix="joal" --joal.ui.secret-token=$TOKEN
+bashio::log.info "Joal started with secret token $TOKEN..."
 
 # Wait for transmission to become available
 bashio::net.wait_for 8081 localhost 900
