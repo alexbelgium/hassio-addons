a05890b38f615976a5b5c5eeeaa9b160f2677193
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-05-13 08:11:13 +0200

    Get latest

--- joal/rootfs/run.sh
+++ joal/rootfs/run.sh
@@ -23,14 +23,16 @@ sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 declare TOKEN
 TOKEN=$(bashio::config 'secret_token')
 
-#mv -f /data/joal/config.json / || true
+mv -f /data/joal/config.json / || true
+#get latest version
+curl -s -S -J -L -o /tmp/joal.tar.gz $(curl -s https://api.github.com/repos/anthonyraymond/joal/releases/latest | grep -o "http.*joal.tar.gz") >/dev/null
 #wget -O /tmp/joal.tar.gz "https://github.com/anthonyraymond/joal/releases/download/$UPSTREAM/joal.tar.gz"
-#mkdir -p /data/joal
-#tar zxvf /tmp/joal.tar.gz -C /data/joal >/dev/null
-#chown -R $(id -u):$(id -g) /data/joal
-#rm /data/joal/jack-of*
-#bashio::log.info "... Joal updated"
-#mv -f /config.json /data/joal/ || true
+mkdir -p /data/joal
+tar zxvf /tmp/joal.tar.gz -C /data/joal >/dev/null
+chown -R $(id -u):$(id -g) /data/joal
+rm /data/joal/jack-of*
+bashio::log.info "... Joal updated"
+mv -f /config.json /data/joal/ || true
 
 ###############
 # LAUNCH APPS #
