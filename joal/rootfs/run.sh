e3f360d8ac8dcf600e6cf6e369a32787fd7bafb5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-05-13 20:15:25 +0200

    Create nginx log
    touch: cannot touch '/var/log/nginx/error.log': No such file or directory

--- joal/rootfs/run.sh
+++ joal/rootfs/run.sh
@@ -16,7 +16,7 @@ ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-touch /var/log/nginx/error.log
+mkdir -p /var/log/nginx && touch /var/log/nginx/error.log
 
 ################
 # JOAL SETTING #
