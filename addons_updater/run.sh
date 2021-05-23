2c6f22b30e509e977becb1320e3b2b0e093a23d7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-05-23 06:29:23 +0200

    Update run.sh

--- addons_updater/run.sh
+++ addons_updater/run.sh
@@ -145,8 +145,8 @@ for addons in $(bashio::config "addon|keys"); do
     git commit -m "Update to ${LASTVERSION}" >/dev/null
 
     LOGINFO="... $SLUG : push to github" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
-    git remote set-url origin "https://${GITUSER}:${GITPASS}@github.com/${REPOSITORY}" >/dev/null
-    git push  >/dev/null
+    git remote set-url origin "https://${GITUSER}:${GITPASS}@github.com/${REPOSITORY}" --quiet >/dev/null
+    git push --quiet >/dev/null
 
     #Log
     bashio::log.yellow "... $SLUG updated from ${CURRENT} to ${LASTVERSION}"
