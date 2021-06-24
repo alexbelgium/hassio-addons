122656fbef5ba96c9160d82520893bf0751a211a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-06-24 11:03:41 +0200

    Update run.sh

--- inadyn/rootfs/run.sh
+++ inadyn/rootfs/run.sh
@@ -43,7 +43,7 @@ fi
 # Create config #
 #################
 
-mustache-cli /data/options.json /templates/inadyn.mustache > /usr/local/etc/inadyn.conf
+mustache /data/options.json /templates/inadyn.mustache > /etc/inadyn.conf
 
 ##############
 # Launch App #
