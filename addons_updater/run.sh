000b7fe43e8524e85776b0bb60d40c7b6002231e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-06-25 05:46:08 +0200

    Update run.sh

--- addons_updater/run.sh
+++ addons_updater/run.sh
@@ -125,7 +125,7 @@ for addons in $(bashio::config "addon|keys"); do
     LOGINFO="... $SLUG : updating files" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
     #files=$(grep -rl ${CURRENT} /data/${BASENAME}/${SLUG}) && echo $files | xargs sed -i "s/${CURRENT}/${LASTVERSION}/g" # Change all tags in all addon files
     sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/config.json
-    sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/Dockerfile
+    sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/Dockerfile || true #Allow absence
     sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/build.json || true #Allow absence of build.json
 
     # Remove " and modify version
