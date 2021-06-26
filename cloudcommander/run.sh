6c91d68f9dab84556a694bb3abea0f4dd2bf1ba5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-06-26 15:05:39 +0200

    add banner

--- cloudcommander/run.sh
+++ cloudcommander/run.sh
@@ -1,5 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 
-echo "The server will start. Please wait 30 seconds before the web UI is available. Enjoy !"
+##########
+# BANNER #
+##########
+
+if bashio::supervisor.ping; then
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+    bashio::log.blue " Add-on: $(bashio::addon.name)"
+    bashio::log.blue " $(bashio::addon.description)"
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+
+    bashio::log.blue " Add-on version: $(bashio::addon.version)"
+    if bashio::var.true "$(bashio::addon.update_available)"; then
+        bashio::log.magenta ' There is an update available for this add-on!'
+        bashio::log.magenta \
+            " Latest add-on version: $(bashio::addon.version_latest)"
+        bashio::log.magenta ' Please consider upgrading as soon as possible.'
+    else
+        bashio::log.green ' You are running the latest version of this add-on.'
+    fi
+
+    bashio::log.blue " System: $(bashio::info.operating_system)" \
+        " ($(bashio::info.arch) / $(bashio::info.machine))"
+    bashio::log.blue " Home Assistant Core: $(bashio::info.homeassistant)"
+    bashio::log.blue " Home Assistant Supervisor: $(bashio::info.supervisor)"
+
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+    bashio::log.blue \
+        ' Please, share the above information when looking for help'
+    bashio::log.blue \
+        ' or support in, e.g., GitHub, forums or the Discord chat.'
+    bashio::log.green \
+        ' https://github.com/alexbelgium/hassio-addons'
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+fi
+
+###############
+# LAUNCH APPS #
+###############
+
+bashio::log.info "The server will start. Please wait 30 seconds before the web UI is available. Enjoy !"
 
 ./usr/src/app/bin/cloudcmd.mjs
