52998708c3e0313115e16c5891513107e6f61031
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-06-27 10:13:19 +0200

    Update run.sh

--- filebrowser/run.sh
+++ filebrowser/run.sh
@@ -153,10 +153,10 @@ PGID=0
 ######################
 
 if bashio::config.true 'NoAuth'; then
-  filebrowser config set --auth.method=noauth
+  filebrowser config set --auth.method=noauth || true
   bashio::log.info "NoAuth option selected"
 else
-  filebrowser config set --auth.method=json
+  filebrowser config set --auth.method=json || true
   bashio::log.info "Default username/password : admin/admin"
 fi
 
