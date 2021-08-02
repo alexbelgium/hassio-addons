24598938f400366dddde3740c6bbffcb95b1b31f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-08-02 14:00:26 +0200

    Update run.sh

--- cloudcommander/rootfs/run.sh
+++ cloudcommander/rootfs/run.sh
@@ -142,7 +142,7 @@ sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 mkdir -p /var/log/nginx && touch /var/log/nginx/error.log
 
-CLOUDCMD_PREFIX="test"
+CLOUDCMD_PREFIX=$(bashio::addon.ingress_entry)
 export CLOUDCMD_PREFIX
 
 ###############
