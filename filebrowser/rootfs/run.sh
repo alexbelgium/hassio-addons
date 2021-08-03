aee3e802c649d771ddc0a33c0016d2d14604c1cb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-08-03 21:38:11 +0200

    Update run.sh

--- filebrowser/rootfs/run.sh
+++ filebrowser/rootfs/run.sh
@@ -156,9 +156,16 @@ declare keyfile
 FB_BASEURL=$(bashio::addon.ingress_entry)
 export FB_BASEURL
 
+declare ADDON_PROTOCOL=http
+# Generate Ingress configuration
+if bashio::config.true 'ssl'; then
+ADDON_PROTOCOL=https
+fi
+
 port=$(bashio::addon.port 80)
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
+sed -i "s|%%protocol%%|${ADDON_PROTOCOL}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%port%%|${ingress_port}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%interface%%|${ingress_interface}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%subpath%%|${FB_BASEURL}/|g" /etc/nginx/servers/ingress.conf
