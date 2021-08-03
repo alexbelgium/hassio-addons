9b29408f20b82f89fdb2e793650b0db21925d6b7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-08-03 21:40:39 +0200

    Update run.sh

--- cloudcommander/rootfs/run.sh
+++ cloudcommander/rootfs/run.sh
@@ -138,9 +138,15 @@ declare keyfile
 CLOUDCMD_PREFIX=$(bashio::addon.ingress_entry)
 export CLOUDCMD_PREFIX
 
+declare ADDON_PROTOCOL=http
+if bashio::config.true 'ssl'; then
+ADDON_PROTOCOL=https
+fi
+
 port=$(bashio::addon.port 80)
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
+sed -i "s|%%protocol%%|${ADDON_PROTOCOL}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%port%%|${ingress_port}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%interface%%|${ingress_interface}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%subpath%%|${CLOUDCMD_PREFIX}/|g" /etc/nginx/servers/ingress.conf
