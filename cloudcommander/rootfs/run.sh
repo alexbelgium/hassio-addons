b465c3b48c066cd1a2f0ea42a7fb039f68275110
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-08-03 14:25:40 +0200

    Update run.sh

--- cloudcommander/rootfs/run.sh
+++ cloudcommander/rootfs/run.sh
@@ -144,9 +144,9 @@ fi
 port=$(bashio::addon.port 80)
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%subpath%%/${CLOUDCMD_PREFIX}/g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%port%%|${ingress_port}|g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%interface%%|${ingress_interface}|g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%subpath%%|${CLOUDCMD_PREFIX}|g" /etc/nginx/servers/ingress.conf
 mkdir -p /var/log/nginx && touch /var/log/nginx/error.log
 
 ###############
