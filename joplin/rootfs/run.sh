4c425b28fed49e5338f83c24b03d00555a7be449
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-08-27 22:14:56 +0200

    Update run.sh

--- joplin/rootfs/run.sh
+++ joplin/rootfs/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 echo "Warning - minimum configuration recommended : 2 cpu cores and 4 GB of memory. Otherwise the system will become unresponsive and crash." 
 
@@ -6,7 +6,6 @@ echo "Warning - minimum configuration recommended : 2 cpu cores and 4 GB of memo
 # LAUNCH APP #
 ##############
 
-# Configure app
-export APP_BASE_URL=$(bashio::config 'APP_BASE_URL')
-bashio::log.info 'Starting Joplin'
+export APP_BASE_URL=$(jq --raw-output ".APP_BASE_URL" "/data/config.json")
+echo 'Starting Joplin'
 npm --prefix packages/server start
