384be818856fc997559b2c073f12851bee3cfc50
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-08-28 13:11:57 +0200

    Update run.sh

--- joplin/rootfs/run.sh
+++ joplin/rootfs/run.sh
@@ -2,7 +2,7 @@
 
 bashio::log.warning "Warning - minimum configuration recommended : 2 cpu cores and 4 GB of memory. Otherwise the system will become unresponsive and crash." 
 
-chmod u+r /data/options.json
+sudo chmod u+r /data/options.json
 
 ##########
 # BANNER #
