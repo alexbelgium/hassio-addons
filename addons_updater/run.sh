236be291fce224a2805b908ed1b81926e46ca260
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-09-03 22:21:20 +0200

    Update run.sh

--- addons_updater/run.sh
+++ addons_updater/run.sh
@@ -135,10 +135,9 @@ for addons in $(bashio::config "addon|keys"); do
 
     #Change all instances of version
     LOGINFO="... $SLUG : updating files" && if [ $VERBOSE = true ]; then bashio::log.info $LOGINFO; fi
-    #files=$(grep -rl ${CURRENT} /data/${BASENAME}/${SLUG}) && echo $files | xargs sed -i "s/${CURRENT}/${LASTVERSION}/g" # Change all tags in all addon files
     sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/config.json
-    sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/Dockerfile || true #Allow absence
-    sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/build.json &>/dev/null || true #Allow absence of build.json
+    sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/Dockerfile || true #Allow absence of Dockerfile
+    [ -f "/data/${BASENAME}/${SLUG}/build.json" ] && sed -i "s/${CURRENT}/${LASTVERSION}/g" /data/${BASENAME}/${SLUG}/build.json || true #Allow absence of build.json
 
     # Remove " and modify version
     LASTVERSION=${LASTVERSION//\"}
