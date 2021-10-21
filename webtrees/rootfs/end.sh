4ae554dda3d0cf9333679042f3912cab2ad51aa6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-21 22:44:39 +0200

    Update end.sh

--- webtrees/rootfs/end.sh
+++ webtrees/rootfs/end.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-if [ -f /data/config.ini.php ]; then
+if [ ! -f /data/config.ini.php ] && [ -f /var/www/webtrees/data/config.ini.php ]; then
 mv /var/www/webtrees/data/config.ini.php /data
 ln -s /data/config.ini.php /var/www/webtrees/data
+else
+echo "error : config not found"
 fi
 
-if [ -f /data/webtrees.sqlite ]; then
-mv /var/www/webtrees/data/webtrees.sqlite /data
-ln -s /data/webtrees.sqlite /var/www/webtrees/data
+if [ ! -f /data/$DB_NAME ] && [ -f /var/www/webtrees/data/$DB_NAME]; then
+mv /var/www/webtrees/data/$DB_NAME /data
+ln -s /data/$DB_NAME /var/www/webtrees/data
+else
+echo "error : database not found"
 fi
 
 exec apache2-foreground
