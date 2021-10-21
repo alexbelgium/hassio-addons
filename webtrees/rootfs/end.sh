53a3335495a6de626ebf3e17724d5633b28562de
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-21 23:16:39 +0200

    Update end.sh

--- webtrees/rootfs/end.sh
+++ webtrees/rootfs/end.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bashio
 
+DB_NAME=$(echo $DB_NAME | tr -d '"')
+
 if [ ! -f /data/config.ini.php ] && [ -f /var/www/webtrees/data/config.ini.php ]; then
 mv /var/www/webtrees/data/config.ini.php /data
 ln -s /data/config.ini.php /var/www/webtrees/data
@@ -7,11 +9,11 @@ else
 bashio::log.fatal "error : config not found"
 fi
 
-if [ ! -f /data/$DB_NAME.sqlite ] && [ -f /var/www/webtrees/data/$DB_NAME.sqlite ]; then
-mv /var/www/webtrees/data/$DB_NAME.sqlite /data
-ln -s /data/$DB_NAME.sqlite /var/www/webtrees/data
+if [ ! -f "/data/$DB_NAME.sqlite" ] && [ -f "/var/www/webtrees/data/$DB_NAME.sqlite" ]; then
+mv "/var/www/webtrees/data/$DB_NAME.sqlite" /data
+ln -s "/data/$DB_NAME.sqlite" /var/www/webtrees/data
 else
-bashio::log.fatal "error : database not found"
+bashio::log.fatal "error : database /var/www/webtrees/data/$DB_NAME.sqlite not found"
 fi
 
 bashio::log.info "Starting apache, please wait then login with $WT_USER : $WT_PASS"
