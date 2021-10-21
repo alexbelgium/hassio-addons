db85698358ed2473951d8e76a9879a16c282406b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-21 22:47:23 +0200

    Update end.sh

--- webtrees/rootfs/end.sh
+++ webtrees/rootfs/end.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 
 if [ ! -f /data/config.ini.php ] && [ -f /var/www/webtrees/data/config.ini.php ]; then
 mv /var/www/webtrees/data/config.ini.php /data
 ln -s /data/config.ini.php /var/www/webtrees/data
 else
-echo "error : config not found"
+bashio::log.fatal "error : config not found"
 fi
 
 if [ ! -f /data/$DB_NAME ] && [ -f /var/www/webtrees/data/$DB_NAME ]; then
 mv /var/www/webtrees/data/$DB_NAME /data
 ln -s /data/$DB_NAME /var/www/webtrees/data
 else
-echo "error : database not found"
+bashio::log.fatal "error : database not found"
 fi
+
+bashio::log.info "Starting apache, please wait then login with $WT_USER : $WT_PASS"
 
 exec apache2-foreground
