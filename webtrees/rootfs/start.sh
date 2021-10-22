460ee132215ea5151a9f4af55c0958686b274904
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-22 22:55:04 +0200

    Update start.sh

--- webtrees/rootfs/start.sh
+++ webtrees/rootfs/start.sh
@@ -84,4 +84,14 @@ fi
 
 cd /
 ./docker-entrypoint.sh
-./end.sh
+
+############
+# END INFO #
+############
+
+DB_NAME=$(echo $DB_NAME | tr -d '"')
+
+bashio::log.info "Starting apache, using database $WEBTREES_HOME/$DB_NAME please wait then login with $WT_USER : $WT_PASS"
+bashio::log.info "Webui can be accessed at : $BASE_URL"
+
+exec apache2-foreground
