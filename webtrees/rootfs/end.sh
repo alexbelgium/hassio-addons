6fc0332f9528ceb6a24a305246ed53cb6e33319c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-22 21:47:48 +0200

    Update end.sh

--- webtrees/rootfs/end.sh
+++ webtrees/rootfs/end.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bashio
 
-#DB_NAME=$(echo $DB_NAME | tr -d '"')
+DB_NAME=$(echo $DB_NAME | tr -d '"')
 
-#if [ ! -f "/data/$DB_NAME.sqlite" ]; then
-#  mv "/var/www/webtrees/data/$DB_NAME.sqlite" /data || bashio::log.fatal "error : database /var/www/webtrees/data/$DB_NAME.sqlite not found"
-#  ln -s "/data/$DB_NAME.sqlite" /var/www/webtrees/data
-#  bashio::log.warning "No existing database found with the name selected, creating a new one"
-#fi
-
-bashio::log.info "Starting apache, please wait then login with $WT_USER : $WT_PASS"
+bashio::log.info "Starting apache, using database $WEBTREES_HOME/$DB_NAME please wait then login with $WT_USER : $WT_PASS"
 
 exec apache2-foreground
