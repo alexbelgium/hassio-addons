f1321434aa77c40066f54269fb50cc97d236eb8e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-22 06:57:28 +0200

    Update end.sh

--- webtrees/rootfs/end.sh
+++ webtrees/rootfs/end.sh
@@ -2,10 +2,10 @@
 
 DB_NAME=$(echo $DB_NAME | tr -d '"')
 
-if [ ! -f /data/config.ini.php ]; then
-mv /var/www/webtrees/data/config.ini.php /data || bashio::log.fatal "error : config not found"
-ln -s /data/config.ini.php /var/www/webtrees/data
-fi
+#if [ ! -f /data/config.ini.php ]; then
+#mv /var/www/webtrees/data/config.ini.php /data || bashio::log.fatal "error : config not found"
+#ln -s /data/config.ini.php /var/www/webtrees/data
+#fi
 
 if [ ! -f "/data/$DB_NAME.sqlite" ]; then
 mv "/var/www/webtrees/data/$DB_NAME.sqlite" /data || bashio::log.fatal "error : database /var/www/webtrees/data/$DB_NAME.sqlite not found"
