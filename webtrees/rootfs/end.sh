2c77f4c63d4a9bf24a7819ecb02c8590561f4520
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-22 23:16:30 +0200

    Delete end.sh

--- webtrees/rootfs/end.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bashio
-
-DB_NAME=$(echo $DB_NAME | tr -d '"')
-
-bashio::log.info "Starting apache, using database $WEBTREES_HOME/$DB_NAME please wait then login with $WT_USER : $WT_PASS"
-bashio::log.info "Webui can be accessed at : $BASE_URL"
-
-exec apache2-foreground
