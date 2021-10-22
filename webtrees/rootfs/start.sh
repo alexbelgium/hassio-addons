df50f57cd0dcdcc7feb9af44a25aa16dec045859
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-22 21:02:03 +0200

    Update start.sh

--- webtrees/rootfs/start.sh
+++ webtrees/rootfs/start.sh
@@ -43,8 +43,8 @@ fi
 # PERSISTENCE #
 ###############
 
-DB_NAME=$(bashio::config 'DB_NAME')
-if [ -f "/data/$DB_NAME.sqlite" ]; then
-bashio::log.info "Using existing database $DB_NAME.sqlite"
-ln -s "/data/$DB_NAME.sqlite" /var/www/webtrees/data
-fi
+#DB_NAME=$(bashio::config 'DB_NAME')
+#if [ -f "/data/$DB_NAME.sqlite" ]; then
+#bashio::log.info "Using existing database $DB_NAME.sqlite"
+#ln -s "/data/$DB_NAME.sqlite" /var/www/webtrees/data
+#fi
