b34e28d9ff00da038f81fcc6062c30bdafc49ad3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-22 07:46:31 +0200

    Update end.sh

--- webtrees/rootfs/end.sh
+++ webtrees/rootfs/end.sh
@@ -2,14 +2,10 @@
 
 DB_NAME=$(echo $DB_NAME | tr -d '"')
 
-#if [ ! -f /data/config.ini.php ]; then
-#mv /var/www/webtrees/data/config.ini.php /data || bashio::log.fatal "error : config not found"
-#ln -s /data/config.ini.php /var/www/webtrees/data
-#fi
-
 if [ ! -f "/data/$DB_NAME.sqlite" ]; then
-mv "/var/www/webtrees/data/$DB_NAME.sqlite" /data || bashio::log.fatal "error : database /var/www/webtrees/data/$DB_NAME.sqlite not found"
-ln -s "/data/$DB_NAME.sqlite" /var/www/webtrees/data
+  mv "/var/www/webtrees/data/$DB_NAME.sqlite" /data || bashio::log.fatal "error : database /var/www/webtrees/data/$DB_NAME.sqlite not found"
+  ln -s "/data/$DB_NAME.sqlite" /var/www/webtrees/data
+  bashio:log.warning "No existing database found with the name selected, creating a new one"
 fi
 
 bashio::log.info "Starting apache, please wait then login with $WT_USER : $WT_PASS"
