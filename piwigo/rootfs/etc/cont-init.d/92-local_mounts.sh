27e0c541fec09415e7215d29328331695ea9e203
Alexandre <alexandre.pary@gmail.com>
2021-10-23 22:00:36 +0200

    Correct smb code

--- piwigo/rootfs/etc/cont-init.d/92-local_mounts.sh
+++ piwigo/rootfs/etc/cont-init.d/92-local_mounts.sh
@@ -18,7 +18,6 @@ if bashio::config.has_value 'localdisks'; then
     mkdir -p /mnt/$disk
     chown -R $(id -u):$(id -g) /mnt/$disk
     [ -d /share/$disk ] && mount /dev/$disk /share/$disk || true
-    mount /dev/$disk /mnt/$disk || (bashio::log.warning "Unable to mount local drives!" && rmdir /mnt/$disk)
-    bashio::log.info "Success! $disk mounted to /mnt/$disk"
+    mount /dev/$disk /mnt/$disk && bashio::log.info "Success! $disk mounted to /mnt/$disk" || (bashio::log.fatal "Unable to mount local drives!" && rmdir /mnt/$disk)
   done
 fi
