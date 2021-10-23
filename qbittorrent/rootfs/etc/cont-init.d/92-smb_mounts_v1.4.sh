8a388fdd80e829e7b0187ed52b3c52b1261e1fb1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-23 08:39:08 +0200

    Update 92-smb_mounts_v1.4.sh

--- qbittorrent/rootfs/etc/cont-init.d/92-smb_mounts_v1.4.sh
+++ qbittorrent/rootfs/etc/cont-init.d/92-smb_mounts_v1.4.sh
@@ -55,7 +55,7 @@ if bashio::config.has_value 'networkdisks'; then
     fi
 
     # Messages
-    if [ $MOUNTED = true ] && [ mountpoint -q "/mnt/$diskname" ]; then
+    if [ $MOUNTED = true ] && [ (mountpoint -q "/mnt/$diskname") ]; then
       bashio::log.info "... $disk successfully mounted to /mnt/$diskname with options $SMBVERS$SECVERS"
       #Test write permissions
       touch /mnt/$diskname/testaze && rm /mnt/$diskname/testaze && bashio::log.info "... $disk successfully mounted to /mnt/$diskname with options $SMBVERS$SECVERS" || bashio::log.fatal "Disk is mounted, however unable to write in the shared disk. Please check UID/GID for permissions, and if the share is rw"
