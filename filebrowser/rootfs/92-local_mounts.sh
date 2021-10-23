98faea5ed018d7eac75403888d8d46bd1fbc8a13
Alexandre <alexandre.pary@gmail.com>
2021-10-23 20:21:21 +0200

    clean code

--- filebrowser/rootfs/92-local_mounts.sh
+++ filebrowser/rootfs/92-local_mounts.sh
@@ -3,24 +3,25 @@
 ######################
 # MOUNT LOCAL SHARES #
 ######################
+MOUNTPOINTS="share"
 
 bashio::log.info 'Mounting external hdd...'
 
 # Mount local Share if configured and if Protection Mode is active
 if bashio::config.has_value 'localdisks'; then
+
   MOREDISKS=$(bashio::config 'localdisks')
-  bashio::log.info "Local Disks mounting.. ${MOREDISKS}" &&
-    # Separate comma separated values
-    for disk in ${MOREDISKS//,/ }; do
-      bashio::log.info "Mount ${disk}"
-      mkdir -p /share/$disk &&
-        if [ ! -d /share/$disk ]; then
-          echo "Creating /share/$disk"
-          mkdir -p /share/$disk
-          chown -R abc:abc /share/$disk
-        fi
-      mount /dev/$disk /share/$disk &&
-        bashio::log.info "Success!"
-    done ||
-    bashio::log.warning "Protection mode is ON. Unable to mount local drives!"
+  bashio::log.info "Local Disks mounting..."
+
+  # Separate comma separated values
+  for disk in ${MOREDISKS//,/ }; do
+    # Mount each disk
+    mkdir -p /$MOUNTPOINTS/$disk
+    chown -R $(id -u):$(id -g) /$MOUNTPOINTS/$disk
+    mount /dev/$disk /$MOUNTPOINTS/$disk
+    bashio::log.info "Success! $disk mounted to /$MOUNTPOINTS/$disk" ||
+      # Error message and clean remaining folder
+      (bashio::log.warning "Unable to mount local drives!" &&
+        rmdir /$MOUNTPOINTS/$disk)
+  done
 fi
--- readarr/root/etc/cont-init.d/92-local_mounts.sh
+++ readarr/root/etc/cont-init.d/92-local_mounts.sh
@@ -9,19 +9,19 @@ bashio::log.info 'Mounting external hdd...'
 
 # Mount local Share if configured and if Protection Mode is active
 if bashio::config.has_value 'localdisks'; then
+
   MOREDISKS=$(bashio::config 'localdisks')
-  bashio::log.info "Local Disks mounting.. ${MOREDISKS}" &&
-    # Separate comma separated values
-    for disk in ${MOREDISKS//,/ }; do
-      bashio::log.info "Mount ${disk}"
-      mkdir -p /$MOUNTPOINTS/$disk &&
-        if [ ! -d /$MOUNTPOINTS/$disk ]; then
-          echo "Creating /$MOUNTPOINTS/$disk"
-          mkdir -p /$MOUNTPOINTS/$disk
-          chown -R abc:abc /$MOUNTPOINTS/$disk
-        fi
-      mount /dev/$disk /$MOUNTPOINTS/$disk &&
-        bashio::log.info "Success! Mounted to /$MOUNTPOINTS/$disk"
-    done ||
-    bashio::log.warning "Unable to mount local drives!"
+  bashio::log.info "Local Disks mounting..."
+
+  # Separate comma separated values
+  for disk in ${MOREDISKS//,/ }; do
+    # Mount each disk
+    mkdir -p /$MOUNTPOINTS/$disk
+    chown -R $(id -u):$(id -g) /$MOUNTPOINTS/$disk
+    mount /dev/$disk /$MOUNTPOINTS/$disk
+    bashio::log.info "Success! $disk mounted to /$MOUNTPOINTS/$disk" ||
+      # Error message and clean remaining folder
+      (bashio::log.warning "Unable to mount local drives!" &&
+        rmdir /$MOUNTPOINTS/$disk)
+  done
 fi
