9166948688d4c3537a7678dbd8f7bc5176c032c4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-24 17:49:05 +0200

    Update run.sh

--- filebrowser/rootfs/run.sh
+++ filebrowser/rootfs/run.sh
@@ -4,11 +4,11 @@
 # SCRIPTS #
 ###########
 
-for SCRIPTS in "00-banner.sh" "92-local_mounts.sh" "92-smb_mounts.sh"
-  chown $(id -u):$(id -g) /$SCRIPTS
-  chmod a+x /$SCRIPTS
-  sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' /$SCRIPTS
-  /./$SCRIPTS & true # Prevents script crash on failure
+for SCRIPTS in "/00-banner.sh" "/92-local_mounts.sh" "/92-smb_mounts.sh"
+  chown $(id -u):$(id -g) $SCRIPTS
+  chmod a+x $SCRIPTS
+  sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' $SCRIPTS
+  /.$SCRIPTS & true # Prevents script crash on failure
 done
 
 ###################
