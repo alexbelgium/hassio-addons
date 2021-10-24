de83973a28b89c8c5ef25f73189e17060fccb38a
Alexandre <alexandre.pary@gmail.com>
2021-10-24 17:45:50 +0200

    clean runtime of run.sh scripts

--- cloudcommander/rootfs/run.sh
+++ cloudcommander/rootfs/run.sh
@@ -1,61 +1,15 @@
 #!/usr/bin/env bashio
 
-##########
-# BANNER #
-##########
+###########
+# SCRIPTS #
+###########
 
-if bashio::supervisor.ping; then
-  bashio::log.blue \
-  '-----------------------------------------------------------'
-  bashio::log.blue " Add-on: $(bashio::addon.name)"
-  bashio::log.blue " $(bashio::addon.description)"
-  bashio::log.blue \
-  '-----------------------------------------------------------'
-
-  bashio::log.blue " Add-on version: $(bashio::addon.version)"
-  if bashio::var.true "$(bashio::addon.update_available)"; then
-    bashio::log.magenta ' There is an update available for this add-on!'
-    bashio::log.magenta \
-    " Latest add-on version: $(bashio::addon.version_latest)"
-    bashio::log.magenta ' Please consider upgrading as soon as possible.'
-  else
-    bashio::log.green ' You are running the latest version of this add-on.'
-  fi
-
-  bashio::log.blue " System: $(bashio::info.operating_system)" \
-  " ($(bashio::info.arch) / $(bashio::info.machine))"
-  bashio::log.blue " Home Assistant Core: $(bashio::info.homeassistant)"
-  bashio::log.blue " Home Assistant Supervisor: $(bashio::info.supervisor)"
-
-  bashio::log.blue \
-  '-----------------------------------------------------------'
-  bashio::log.blue \
-  ' Please, share the above information when looking for help'
-  bashio::log.blue \
-  ' or support in, e.g., GitHub, forums or the Discord chat.'
-  bashio::log.green \
-  ' https://github.com/alexbelgium/hassio-addons'
-  bashio::log.blue \
-  '-----------------------------------------------------------'
-fi
-
-######################
-# MOUNT LOCAL SCRIPT #
-######################
-chown $(id -u):$(id -g) /92-local_mounts.sh
-chmod a+x /92-local_mounts.sh
-sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' /92-local_mounts.sh
-/./92-local_mounts.sh &
-true # Prevents script crash on failure
-
-######################
-# EXECUTE SMB SCRIPT #
-######################
-chown $(id -u):$(id -g) /92-smb_mounts.sh
-chmod a+x /92-smb_mounts.sh
-sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' /92-smb_mounts.sh
-/./92-smb_mounts.sh &
-true # Prevents script crash on failure
+for SCRIPTS in "00-banner.sh" "92-local_mounts.sh" "92-smb_mounts.sh"
+  chown $(id -u):$(id -g) /$SCRIPTS
+  chmod a+x /$SCRIPTS
+  sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' /$SCRIPTS
+  /./$SCRIPTS & true # Prevents script crash on failure
+done
 
 #################
 # NGINX SETTING #
