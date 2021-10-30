0d57c9f2b7678abc909253cacb894325e859c68d
Alexandre <alexandre.pary@gmail.com>
2021-10-30 17:53:26 +0200

    correct code

--- freqtrade/rootfs/run.sh
+++ freqtrade/rootfs/run.sh
@@ -4,11 +4,10 @@ echo "Starting"
 
 if [ ! -f /data/config.json ]; then
   echo "building userdir"
-  freqtrade create-userdir --userdir /data \
+  freqtrade create-userdir --userdir /data
   echo "building initial config"
-  freqtrade new-config --config /data/config.json \
+  freqtrade new-config --config /data/config.json
 fi
 
 echo "Starting app"
-pause 36000000
-#bash freqtrade trade --logfile /data/logs/freqtrade.log --db-url sqlite://///data/tradesv3.sqlite --config /data/config.json --strategy SampleStrategy
+freqtrade trade --logfile /data/logs/freqtrade.log --db-url sqlite://///data/tradesv3.sqlite --config /data/config.json --strategy SampleStrategy
