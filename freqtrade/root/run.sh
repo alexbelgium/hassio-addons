bc95d08c5386ce9f3e3dc8a8f7fdc6208c57ac90
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-30 17:07:49 +0200

    Create run.sh

---
+++ freqtrade/root/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bah
+
+trade --logfile /freqtrade/user_data/logs/freqtrade.log --db-url sqlite:////freqtrade/user_data/tradesv3.sqlite --config /freqtrade/user_data/config.json --strategy SampleStrategy
