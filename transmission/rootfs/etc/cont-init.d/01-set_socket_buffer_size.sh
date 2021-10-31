0c6f9424ed56083a5faf75c2128de14b4064692c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-10-31 06:27:58 +0100

    Delete 01-set_socket_buffer_size.sh

--- transmission/rootfs/etc/cont-init.d/01-set_socket_buffer_size.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-##########################
-# SET_SOCKET_BUFFER_SIZE #
-##########################
-
-echo "Setting socket buffer size"
-sed -i -e '$a"net.core.rmem_max = 4194304"' /etc/sysctl.conf
-sed -i -e '$a"net.core.wmem_max = 1048576"' /etc/sysctl.conf
