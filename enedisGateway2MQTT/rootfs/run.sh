8386cf654a229552697de150bf3d87c7f0a4787e
Alexandre <alexandre.pary@gmail.com>
2021-11-01 22:24:20 +0100

    message

--- enedisGateway2MQTT/rootfs/run.sh
+++ enedisGateway2MQTT/rootfs/run.sh
@@ -42,18 +42,18 @@ fi
 #################
 # Create config #
 #################
-echo "Setting variables"
-echo "................."
-
+echo " "
+bashio::log.info "Setting variables"
+echo " "
 for VARIABLES in "ACCESS_TOKEN" "PDL" "MQTT_HOST" "MQTT_PORT" "MQTT_PREFIX" "MQTT_CLIENT_ID" "MQTT_USERNAME" "MQTT_PASSWORD" "RETAIN" "QOS" "GET_CONSUMPTION" "GET_PRODUCTION" "HA_AUTODISCOVERY" "HA_AUTODISCOVERY_PREFIX" "CONSUMPTION_PRICE_BASE" "CONSUMPTION_PRICE_HC" "CONSUMPTION_PRICE_HP" "CARD_MYENEDIS"; do
     if bashio::config.has_value $VARIABLES; then
         export $VARIABLES=$(bashio::config $VARIABLES)
         echo "$VARIABLES set to $(bashio::config $VARIABLES)"
     fi
 done
-
+echo " "
 bashio::log.info "Starting the app"
-
+echo " "
 ##############
 # Launch App #
 ##############
