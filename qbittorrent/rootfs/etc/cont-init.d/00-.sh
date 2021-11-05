cf150aae28b82030462981bdea2626f4db50c7dd
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-05 22:11:32 +0100

    Allow dockerfile error

---
+++ qbittorrent/rootfs/etc/cont-init.d/00-.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [ ! -f "/usr/bin/bashio" ]; then
+    echo "Bashio does not exist, executing script"
+
+    ################
+    # Install apps #
+    ################
+    apt-get clean \
+    && apt-get update \
+    && apt-get install -y \
+    jq \
+    curl \
+    cifs-utils \
+    keyutils \
+    samba \
+    smbclient \
+    nginx \
+    coreutils \
+    openvpn \
+    && apt-get clean \
+    \    
+    ##################
+    # Install tempio #
+    ##################
+    && curl -L -f -s -o /usr/bin/tempio \
+    "https://github.com/home-assistant/tempio/releases/download/${TEMPIO_VERSION}/tempio_${BUILD_ARCH}" \
+    && chmod a+x /usr/bin/tempio \
+    \
+    ##############
+    # STATISTICS #
+    ##############
+    && curl -L -f -s -o /tmp/tmp \
+    "https://github.com/alexbelgium/downloads-tracker/archive/refs/tags/Qbittorrent.zip" \
+    \
+    ##################
+    # Install bashio #
+    ##################
+    && mkdir -p /tmp/bashio \
+    && curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" \
+    | tar -xzf - --strip 1 -C /tmp/bashio \
+    && mv /tmp/bashio/lib /usr/lib/bashio \
+    && ln -s /usr/lib/bashio/bashio /usr/bin/bashio \
+    && rm -rf /tmp/bashio \
+    \
+    ########################################
+    # Correct upstream image folders links #
+    ########################################
+    \
+    # Allow UID and GID setting
+    && sed -i 's/bash/bashio/g' /etc/cont-init.d/10-adduser \
+    && sed -i 's/{PUID:-911}/(bashio::config "PUID")/g' /etc/cont-init.d/10-adduser \
+    && sed -i 's/{PGID:-911}/(bashio::config "PGID")/g' /etc/cont-init.d/10-adduser \
+    \
+    # Correct permissions 
+    && sed -i 's=/config=/config/qBittorrent || true=g' /etc/cont-init.d/10-adduser \    
+    && sed -i 's=	/config=/config/qBittorrent || true=g' /etc/cont-init.d/30-config \
+    \
+    # Set download folder to /share
+    && sed -i 's|/downloads/|/share/qBittorrent/|g' /defaults/qBittorrent.conf \
+    \
+    # Remove fixed folders, allows connection to webUI
+    && sed '11,13d' /defaults/qBittorrent.conf \ 
+    && echo 'WebUI\HostHeaderValidation=false' >> /defaults/qBittorrent.conf \
+    && echo 'WebUI\LocalHostAuth=false' >> /defaults/qBittorrent.conf
+
+fi
