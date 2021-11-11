cdb964ef68843e25b74fc1f75b6190f3cb8b09c4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-11 22:50:50 +0100

    Update entrypoint.sh

--- enedisgateway2mqtt/rootfs/entrypoint.sh
+++ enedisgateway2mqtt/rootfs/entrypoint.sh
@@ -8,6 +8,6 @@ for SCRIPTS in /scripts/*; do
   chown $(id -u):$(id -g) $SCRIPTS
   chmod a+x $SCRIPTS
   sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' $Scripts || true
-  /.$SCRIPTS &&
+  ./$SCRIPTS &&
   true || true # Prevents script crash on failure
 done
