1b58f594a44a40bfa0626494e2fd026f2db7ce0e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-11 22:30:58 +0100

    Create 00-.sh

---
+++ enedisgateway2mqtt/rootfs/00-.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ ! -f "/usr/bin/bashio" ]; then
+    echo "Bashio does not exist, executing script"
+
+    ################
+    # Install apps #
+    ################
+    apt-get clean &&
+        apt-get update &&
+        apt-get install -y --no-install-recommends \
+            jq \
+            curl
+
+    ###################
+    # Install bashio #
+    ##################
+    BASHIO_VERSION=0.13.1
+    mkdir -p /tmp/bashio
+    curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" |
+        tar -xzf - --strip 1 -C /tmp/bashio
+    mv /tmp/bashio/lib /usr/lib/bashio
+    ln -s /usr/lib/bashio/bashio /usr/bin/bashio
+    rm -rf /tmp/bashio
+
+    echo "Bashio installed"
+fi
