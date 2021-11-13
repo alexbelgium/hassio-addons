ba82638dcd8d9b513a303e3b4390b89566c28435
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-13 07:43:29 +0100

    Update entrypoint.sh

--- cloudcommander/rootfs/entrypoint.sh
+++ cloudcommander/rootfs/entrypoint.sh
@@ -1,64 +1,13 @@
-#!/usr/bin/env bashio
+#!/bin/bash
 
-###########
-# SCRIPTS #
-###########
-
-for SCRIPTS in "/00-banner.sh" "/92-local_mounts.sh" "/92-smb_mounts.sh"; do
+#for SCRIPTS in "/00-.sh" "/00-banner.sh" "/99-run.sh"; do
+cd /scripts
+for SCRIPTS in *; do
+  [ -e "$SCRIPTS" ] || continue 
   echo $SCRIPTS
   chown $(id -u):$(id -g) $SCRIPTS
   chmod a+x $SCRIPTS
-  sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' $SCRIPTS
-  /.$SCRIPTS &&
-  true # Prevents script crash on failure
+  sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' $SCRIPTS || true
+  ./$SCRIPTS &&
+  true || true # Prevents script crash on failure
 done
-
-#################
-# NGINX SETTING #
-#################
-
-declare port
-declare certfile
-declare ingress_interface
-declare ingress_port
-declare keyfile
-
-CLOUDCMD_PREFIX=$(bashio::addon.ingress_entry)
-export CLOUDCMD_PREFIX
-
-declare ADDON_PROTOCOL=http
-if bashio::config.true 'ssl'; then
-  ADDON_PROTOCOL=https
-fi
-
-port=$(bashio::addon.port 80)
-ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-sed -i "s|%%protocol%%|${ADDON_PROTOCOL}|g" /etc/nginx/servers/ingress.conf
-sed -i "s|%%port%%|${ingress_port}|g" /etc/nginx/servers/ingress.conf
-sed -i "s|%%interface%%|${ingress_interface}|g" /etc/nginx/servers/ingress.conf
-sed -i "s|%%subpath%%|${CLOUDCMD_PREFIX}/|g" /etc/nginx/servers/ingress.conf
-mkdir -p /var/log/nginx && touch /var/log/nginx/error.log
-
-###############
-# LAUNCH APPS #
-###############
-
-if bashio::config.has_value 'CUSTOM_OPTIONS'; then
-  CUSTOMOPTIONS=" $(bashio::config 'CUSTOM_OPTIONS')"
-else
-  CUSTOMOPTIONS=""
-fi
-
-if bashio::config.has_value 'DROPBOX_TOKEN'; then
-  DROPBOX_TOKEN="--dropbox --dropbox-token $(bashio::config 'DROPBOX_TOKEN')"
-else
-  DROPBOX_TOKEN=""
-fi
-
-bashio::log.info "Starting..."
-
-./usr/src/app/bin/cloudcmd.mjs '"'$DROPBOX_TOKEN$CUSTOMOPTIONS'"' &
-bashio::net.wait_for 8000 localhost 900 || true
-bashio::log.info "Started !"
-exec nginx
