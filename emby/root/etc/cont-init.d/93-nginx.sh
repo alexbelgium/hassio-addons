1ae28bd0d0e41ed0d61548016a661b7ac39675b2
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-13 17:52:57 +0100

    Update 93-nginx.sh

--- emby/root/etc/cont-init.d/93-nginx.sh
+++ emby/root/etc/cont-init.d/93-nginx.sh
@@ -18,4 +18,6 @@ if bashio::config.true 'ssl'; then
     sed -i "s|%%certfile%%|/ssl/$certfile|g" /etc/nginx/templates/ssl.gtpl
     sed -i "s|%%certkey%%|/ssl/$keyfile|g" /etc/nginx/templates/ssl.gtpl
     mv /etc/nginx/templates/ssl.gtpl /etc/nginx/servers/ssl.conf
+else
+    rm -r /etc/services.d/nginx
 fi
