e4ce1f75895122438c2e8bdc434125efd6e794a8
Alexandre <alexandre.pary@gmail.com>
2021-11-13 17:41:48 +0100

    emby ssl

---
+++ emby/root/etc/cont-init.d/93-nginx.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv bashio
+
+###############
+# SSL SETTING #
+###############
+
+declare admin_port
+declare protocol=http
+
+# Generate Ingress configuration
+if bashio::config.true 'ssl'; then
+    bashio::config.require.ssl
+    protocol=https
+    certfile=$(bashio::config 'certfile')
+    keyfile=$(bashio::config 'keyfile')
+    address=$(bashio::addon.ip_address)
+    sed -i "s|%%interface%%|$address|g" /etc/nginx/templates/ssl.gtpl
+    sed -i "s|%%certfile%%|/ssl/$certfile|g" /etc/nginx/templates/ssl.gtpl
+    sed -i "s|%%certkey%%|/ssl/$keyfile|g" /etc/nginx/templates/ssl.gtpl
+    mv /etc/nginx/templates/ssl.gtpl /etc/nginx/servers/ssl.conf
+fi
---
+++ emby/root/etc/nginx/templates/ssl.gtpl
@@ -0,0 +1,23 @@
+server {
+  listen %%interface%%:8096 ssl;
+  server_name emby;
+  include /etc/nginx/includes/server_params.conf;
+  include /etc/nginx/includes/proxy_params.conf;
+  include /etc/nginx/includes/ssl_params.conf;
+  ssl_certificate %%certfile%%
+  ssl_certificate_key %%certkey%%
+
+location ^~ / {
+    include /config/nginx/proxy.conf;
+    include /config/nginx/resolver.conf;
+    proxy_pass http://backend;
+}
+
+location ^~ /embywebsocket {
+    include /config/nginx/proxy.conf;
+    include /config/nginx/resolver.conf;
+    proxy_pass http://backend;
+}
+
+
+}
