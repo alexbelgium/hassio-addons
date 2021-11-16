7c535cb929172ea832aef630a8983e5e6173855d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-16 18:21:57 +0100

    Update 99-run.sh

--- enedisgateway2mqtt_test/rootfs/scripts/99-run.sh
+++ enedisgateway2mqtt_test/rootfs/scripts/99-run.sh
@@ -18,7 +18,7 @@ else
 fi
 
 # Check if yaml is valid
-yamllint -d relaxed --no-warnings $CONFIGSOURCE &>ERROR
+yamllint -d relaxed --no-warnings $CONFIGSOURCE &>ERROR || true
 if [ $? = 0 ]; then
     echo "Config file is a valid yaml"
 else
