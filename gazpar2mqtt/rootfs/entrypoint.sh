f8e3d0b6d785ee6f0a372ea56a9d2a2f9fb575a1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-18 16:55:38 +0100

    Update entrypoint.sh

--- gazpar2mqtt/rootfs/entrypoint.sh
+++ gazpar2mqtt/rootfs/entrypoint.sh
@@ -9,13 +9,17 @@ echo "Starting..."
 # For all keys in options.json
 JSONSOURCE="/data/options.json"
 
+
 # Export keys as env variables
 echo "All addon options were exported as variables"
 mapfile -t arr < <(jq -r 'keys[]' ${JSONSOURCE})
 for KEYS in ${arr[@]}; do
         # export key
+        VALUE=$(jq .$KEYS ${JSONSOURCE})
+        export ${KEYS}=${VALUE:1:-1}
         export $(echo "${KEYS}=$(jq .$KEYS ${JSONSOURCE})")
 done 
+
 
 ################
 # Set timezone #
