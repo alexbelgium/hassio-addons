f83be65293e743d1f7d99ba438f8ced51c731074
Alexandre <alexandre.pary@gmail.com>
2021-11-19 11:49:15 +0100

    Correct entrypoint

--- webtrees/rootfs/entrypoint.sh
+++ webtrees/rootfs/entrypoint.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
-echo "Starting"
+echo "Starting..."
 
-if [ ! -f /data/config.json ]; then
-  echo "building userdir"
-  freqtrade create-userdir --userdir /data
-  echo "building initial config"
-  freqtrade new-config --config /data/config.json
+###################################
+# Export all addon options as env #
+###################################
+
+# For all keys in options.json
+JSONSOURCE="/data/options.json"
+
+# Export keys as env variables
+echo "All addon options were exported as variables"
+mapfile -t arr < <(jq -r 'keys[]' ${JSONSOURCE})
+for KEYS in ${arr[@]}; do
+  # export key
+  VALUE=$(jq .$KEYS ${JSONSOURCE})
+  export ${KEYS}=${VALUE//[\"\']/} &>/dev/null
+done
+
+################
+# Set timezone #
+################
+if [ ! -z "TZ" ] && [ -f /etc/localtime ]; then
+  if [ -f /usr/share/zoneinfo/$TZ ]; then
+    echo "Timezone set from $(cat /etc/timezone) to $TZ"
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ >/etc/timezone
+  else
+    echo "WARNING : Timezone $TZ is invalid, it will be kept to default value of $(cat /etc/timezone)"
+  fi
 fi
 
-pause 36000000000
+####################
+# Starting scripts #
+####################
 
-echo "Starting app"
-freqtrade trade --logfile /data/logs/freqtrade.log --db-url sqlite://///data/tradesv3.sqlite --config /data/config.json --strategy SampleStrategy
+for SCRIPTS in scripts/*; do
+  [ -e "$SCRIPTS" ] || continue
+  echo "$SCRIPTS: executing"
+  chown $(id -u):$(id -g) $SCRIPTS
+  chmod a+x $SCRIPTS
+  sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' $SCRIPTS || true
+  ./$SCRIPTS || echo "$SCRIPTS: exiting $?"
+done
