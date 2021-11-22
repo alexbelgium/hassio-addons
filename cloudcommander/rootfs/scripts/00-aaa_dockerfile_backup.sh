28eca160a4e5bf65dc34015104afaac116c2d025
Alexandre <alexandre.pary@gmail.com>
2021-11-22 13:12:10 +0100

    Suppress messages

--- cloudcommander/rootfs/scripts/00-aaa_dockerfile_backup.sh
+++ cloudcommander/rootfs/scripts/00-aaa_dockerfile_backup.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-# If dockerfile failed install manually 
+# If dockerfile failed install manually
 if [ ! -f "/usr/bin/bashio" ]; then
     echo "Bashio does not exist, executing script"
+    (
+        ################
+        # Install apps #
+        ################
+        apt-get clean &&
+            apt-get update &&
+            apt-get install -y --no-install-recommends \
+                jq \
+                cifs-utils \ 
+        keyutils \
+            samba \
+            smbclient \
+            nginx &&
+            apt-get clean
 
-    ################
-    # Install apps #
-    ################
-    apt-get clean \
-    && apt-get update \
-    && apt-get install -y --no-install-recommends \
-    jq \
-    cifs-utils \    
-    keyutils \
-    samba \
-    smbclient \
-    nginx \
-    && apt-get clean
-
-    ###################
-    # Install bashio #
-    ##################
-    BASHIO_VERSION=0.13.1
-    mkdir -p /tmp/bashio
-    curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" |
-        tar -xzf - --strip 1 -C /tmp/bashio
-    mv /tmp/bashio/lib /usr/lib/bashio
-    ln -s /usr/lib/bashio/bashio /usr/bin/bashio
-    rm -rf /tmp/bashio
-
+        ###################
+        # Install bashio #
+        ##################
+        BASHIO_VERSION=0.13.1
+        mkdir -p /tmp/bashio
+        curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" |
+            tar -xzf - --strip 1 -C /tmp/bashio
+        mv /tmp/bashio/lib /usr/lib/bashio
+        ln -s /usr/lib/bashio/bashio /usr/bin/bashio
+        rm -rf /tmp/bashio
+    ) >/dev/null
     echo "Bashio installed"
 fi
--- filebrowser/rootfs/scripts/00-aaa_dockerfile_backup.sh
+++ filebrowser/rootfs/scripts/00-aaa_dockerfile_backup.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
-# If dockerfile failed install manually 
+# If dockerfile failed install manually
 if [ ! -f "/usr/bin/bashio" ]; then
     echo "Bashio does not exist, executing script"
+    (
+        ################
+        # Install apps #
+        ################
+        apk add --no-cache \
+            curl \
+            jq \
+            bash \
+            cifs-utils \
+            keyutils \
+            samba \
+            samba-client \
+            bind-tools \
+            nginx
 
-    ################
-    # Install apps #
-    ################
-    apk add --no-cache \
-        curl \
-        jq \
-        bash \
-        cifs-utils \
-        keyutils \
-        samba \
-        samba-client \
-        bind-tools \
-        nginx
+        ###################
+        # Install bashio #
+        ##################
+        BASHIO_VERSION=0.13.1
+        mkdir -p /tmp/bashio
+        curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" |
+            tar -xzf - --strip 1 -C /tmp/bashio
+        mv /tmp/bashio/lib /usr/lib/bashio
+        ln -s /usr/lib/bashio/bashio /usr/bin/bashio
+        rm -rf /tmp/bashio
 
-    ###################
-    # Install bashio #
-    ##################
-    BASHIO_VERSION=0.13.1
-    mkdir -p /tmp/bashio
-    curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" |
-        tar -xzf - --strip 1 -C /tmp/bashio
-    mv /tmp/bashio/lib /usr/lib/bashio
-    ln -s /usr/lib/bashio/bashio /usr/bin/bashio
-    rm -rf /tmp/bashio
+        ########################################
+        # Correct upstream image folders links #
+        ########################################
+        mkdir -p -m 777 /config/filebrowser || true
 
-    ########################################
-    # Correct upstream image folders links #
-    ########################################
-    mkdir -p -m 777 /config/filebrowser || true
-
+    ) >/dev/null
     echo "Bashio installed"
 fi
