45cc2596ec2aa2b5b3fbb796845a7c92f3b43736
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-11-30 09:23:12 +0100

    Update 32-nginx_ingress.sh

--- ubooquity/rootfs/etc/cont-init.d/32-nginx_ingress.sh
+++ ubooquity/rootfs/etc/cont-init.d/32-nginx_ingress.sh
@@ -12,8 +12,12 @@ declare keyfile
 # General values
 port=$(bashio::addon.ingress_port)
 if [ $port > 1 ]; then 
+  # Adapt nginx
   sed -i "s|%%port%%|$port|g" /etc/nginx/servers/ingress.conf
   sed -i "s|%%interface%%|$(bashio::addon.ip_address)|g" /etc/nginx/servers/ingress.conf
+  # Removebaseurl
+  jq '.reverseProxyPrefix = ""' /config/ubooquity/preferences.json | sponge /config/ubooquity/preferences.json
+  # Log
   bashio::log.info "Ingress enabled"
 else
   rm /etc/nginx/servers/ingress.conf
