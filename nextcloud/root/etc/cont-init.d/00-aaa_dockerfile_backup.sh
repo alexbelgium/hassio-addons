a58c8be010845d566100dc3eb816fc06d476f98b
Alexandre <alexandre.pary@gmail.com>
2021-12-01 22:48:53 +0100

    remove yq

--- nextcloud/root/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ nextcloud/root/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -19,7 +19,6 @@ if [ ! -f "/usr/bin/bashio" ]; then
                     apt-get update &&
                     apt-get install -y --no-install-recommends \
                 jq \
-                yq \
                 curl \
                 cifs-utils \
                 keyutils \
