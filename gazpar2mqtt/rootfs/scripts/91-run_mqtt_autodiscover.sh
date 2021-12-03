97d0a5b1f6c3cd18f5b569c38ab6f1154d494f3b
Alexandre <alexandre.pary@gmail.com>
2021-12-03 21:37:58 +0100

    log

--- gazpar2mqtt/rootfs/scripts/91-run_mqtt_autodiscover.sh
+++ gazpar2mqtt/rootfs/scripts/91-run_mqtt_autodiscover.sh
@@ -19,7 +19,7 @@ if bashio::config.true 'mqtt_autodiscover'; then
     for variables in "MQTT_HOST=$MQTT_HOST" "MQTT_PORT=$MQTT_PORT" "MQTT_SSL=$MQTT_SSL" "MQTT_USERNAME=$MQTT_USERNAME" "MQTT_PASSWORD=$MQTT_PASSWORD"; do
         sed -i "1a export $variables" /etc/services.d/*/*run*
         sed -i "1a export $variables" /scripts/*run*
+        # Log
+        bashio::log.blue "... $variables"
     done
-    # Log
-    bashio::log.info "Received user: '$MQTT_USERNAME' for MQTT at '$MQTT_HOST:$MQTT_PORT'!"
 fi
