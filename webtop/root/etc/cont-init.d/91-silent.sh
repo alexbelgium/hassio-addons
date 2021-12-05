f5148a430b08b86fe64e5da9fb455dd5b0d03e45
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-05 19:22:41 +0100

    Create 91-silent.sh

---
+++ webtop/root/etc/cont-init.d/91-silent.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bashio
+
+###############
+# SILENT MODE #
+###############
+
+if bashio::config.true 'silent'; then
+APPEND=' > /dev/null'
+sed -i '$s|$|'"$APPEND"'|' /etc/services.d/*/run
+bashio::log.info 'Silent mode activated, all logs from emby server are hidden. Disable this option if you need to troubleshoot the addon.'
+fi
