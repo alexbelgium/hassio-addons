eda08015329779693b924d750ea666840271e26c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-05 19:22:05 +0100

    Update 91-silent.sh

--- emby/root/etc/cont-init.d/91-silent.sh
+++ emby/root/etc/cont-init.d/91-silent.sh
@@ -6,6 +6,6 @@
 
 if bashio::config.true 'silent'; then
 APPEND=' > /dev/null'
-sed -i '$s|$|'"$APPEND"'|' /etc/services.d/emby/run
-bashio::log.info 'Silent mode activated, all logs from emby server are hidden. Disable this option if you need to troubleshoot emby.'
+sed -i '$s|$|'"$APPEND"'|' /etc/services.d/*/run
+bashio::log.info 'Silent mode activated, all logs from emby server are hidden. Disable this option if you need to troubleshoot the addon.'
 fi
