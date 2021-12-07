21081441c7fefbdac7c6ed8a2e5db8e70c94be56
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-07 15:37:45 +0100

    Update 99-run.sh

--- arpspoof/rootfs/scripts/99-run.sh
+++ arpspoof/rootfs/scripts/99-run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bashio
 
+INTERFACE_NAME=$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')
+bashio::log.blue "INTERFACE_NAME=$INTERFACE_NAME"
+
 bashio::log.info "Starting..."
 /usr/bin/python3 /opt/arpspoof/arpspoof.py
