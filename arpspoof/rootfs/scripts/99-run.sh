651437d82701420d8797faeb72a8c9a2c194af8f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-07 16:19:29 +0100

    Update 99-run.sh

--- arpspoof/rootfs/scripts/99-run.sh
+++ arpspoof/rootfs/scripts/99-run.sh
@@ -2,7 +2,7 @@
 
 # Autodefine if not defined
 if [ -z INTERFACE_NAME ]; then
-INTERFACE_NAME=$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')
+export INTERFACE_NAME=$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')
 bashio::log.blue "Autodetection : INTERFACE_NAME=$INTERFACE_NAME"
 fi
 
