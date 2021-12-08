ab8d44d57ab9a566f4faacc03a778f4c55cb76d3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-08 13:12:36 +0100

    Update 00-aaa_dockerfile_backup.sh

--- emby/root/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ emby/root/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -3,24 +3,13 @@
 if [ ! -f "/usr/bin/bashio" ]; then
     echo "Bashio does not exist, executing script"
     (
-        ################
-        # Install apps #
-        ################
-        cd /
-        curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/automatic_packages.sh" --output automatic_packages.sh
-        chmod 777 automatic_packages.sh
-        $(./automatic_packages.sh)
-
-        ###################
-        # Install bashio #
-        ##################
-        
-        mkdir -p /tmp/bashio
-        curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" |
-            tar -xzf - --strip 1 -C /tmp/bashio
-        mv /tmp/bashio/lib /usr/lib/bashio
-        ln -s /usr/lib/bashio/bashio /usr/bin/bashio
-        rm -rf /tmp/bashio
+        #######################
+        # Automatic installer #
+        #######################
+        curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/automatic_packages.sh" --output /automatic_packages.sh
+        chmod 777 /automatic_packages.sh
+        eval /./automatic_packages.sh
+        rm /automatic_packages.sh
 
     ) >/dev/null
 
