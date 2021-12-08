afbd5aa080f98cfad879a1fadb41d116e3312918
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-08 13:23:29 +0100

    Update 00-aaa_dockerfile_backup.sh

--- emby/root/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ emby/root/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # If dockerfile failed install manually
 if [ ! -f "/usr/bin/bashio" ]; then
-    echo "Bashio does not exist, executing script"
+    echo "Executing script"
     (
         #######################
         # Automatic installer #
         #######################
+        $(curl --help &>/dev/null) && (apt-get install -y --no-install-recommends curl || apk add --no-cache curl)
         curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/automatic_packages.sh" --output /automatic_packages.sh
         chmod 777 /automatic_packages.sh
         eval /./automatic_packages.sh
