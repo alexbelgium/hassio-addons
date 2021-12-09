a4ea067c04a058b06c761f4ffdad4155ac974aa4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-09 18:06:37 +0100

    Update automatic_packages.sh

--- zzz_templates/automatic_packages.sh
+++ zzz_templates/automatic_packages.sh
@@ -14,10 +14,12 @@ PACKMANAGER="apk"
 
 if [[ "$(apk -h 2>/dev/null)" ]]; then
 # If apk based
+echo "apk based" 
 PACKMANAGER="apk"
 PACKAGES="apk add --no-cache $PACKAGES"
 else
 # If apt-get based
+echo "apt based" 
 PACKMANAGER="apt"
 PACKAGES="apt-get clean \
     && apt-get update \
@@ -38,6 +40,7 @@ PACKAGES="$PACKAGES jq curl"
 
 # In etc
 if ls /etc/nginx 1> /dev/null 2>&1; then
+    echo "nginx found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES nginx"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES nginx"
     mv /etc/nginx /etc/nginx2
@@ -47,41 +50,49 @@ fi
 for files in "/scripts" "/etc/cont-init.d"; do
 
     if ls $files/*smb* 1> /dev/null 2>&1; then
+    echo "smb found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES cifs-utils keyutils samba samba-client"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES cifs-utils keyutils samba smbclient"
     fi
 
     if ls $files/*vpn* 1> /dev/null 2>&1; then
+     echo "vpn found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES coreutils openvpn"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES coreutils openvpn"
     fi
 
     if ls $files/*global_var* 1> /dev/null 2>&1; then
+    echo "global_var found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES jq"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES jq"
     fi
 
     if ls $files/*yaml* 1> /dev/null 2>&1; then
+    echo "yaml found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES yamllint"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES yamllint"
     fi
 
     if [[ $(grep -rnw "$files/" -e 'git') ]]; then
+    echo "git found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES git"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES git"
     fi
 
     if [[ $(grep -rnw "$files/" -e 'sponge') ]]; then
+    echo "sponge found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES moreutils"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES moreutils"
     fi
 
     if [[ $(grep -rnw "$files/" -e 'sqlite') ]]; then
+    echo "sqlite found" 
     [ $PACKMANAGER = "apk" ] && PACKAGES="$PACKAGES sqlite"
     [ $PACKMANAGER = "apt" ] && PACKAGES="$PACKAGES sqlite3"
     fi
 
     if [[ $(grep -rnw "$files/" -e 'pip') ]]; then
+    echo "pip found" 
     [ $PACKMANAGER = "apk" ] && [[ $(pip -V) ]] || PACKAGES="$PACKAGES py3-pip" 
     [ $PACKMANAGER = "apt" ] && [[ $(pip -V) ]] || PACKAGES="$PACKAGES python-pip" 
     fi
