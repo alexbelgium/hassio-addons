50af184bc5b228483b12770bbb4d4958b24bec70
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-09 22:59:41 +0100

    Update 00-aaa_dockerfile_backup.sh

--- bitwarden/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ bitwarden/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -8,6 +8,7 @@ if [ -f "/ENV" ]; then
         #######################
         # Automatic installer #
         #######################
+        $(ls /bin/bash &>/dev/null) || (apt-get install -y --no-install-recommends bash || apk add --no-cache bash) && \
         $(curl --help &>/dev/null) || (apt-get install -y --no-install-recommends curl &>/dev/null || apk add --no-cache curl) && \
         curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/automatic_packages.sh" --output /automatic_packages.sh  && \
         chmod 777 /automatic_packages.sh && \
