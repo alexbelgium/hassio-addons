d2a521944f29a97e05eaca4ca410b59e405c8220
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-09 20:37:55 +0100

    Update 00-aaa_dockerfile_backup.sh

--- addons_updater/rootfs/scripts/00-aaa_dockerfile_backup.sh
+++ addons_updater/rootfs/scripts/00-aaa_dockerfile_backup.sh
@@ -6,10 +6,10 @@ if [ ! -f "/usr/bin/bashio" ]; then
         #######################
         # Automatic installer #
         #######################
-        $(curl --help &>/dev/null) && (apt-get install -y --no-install-recommends curl &>/dev/null || apk add --no-cache curl)
-        curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/automatic_packages.sh" --output /automatic_packages.sh
-        chmod 777 /automatic_packages.sh
-        eval /./automatic_packages.sh
+        $(curl --help &>/dev/null) || (apt-get install -y --no-install-recommends curl &>/dev/null || apk add --no-cache curl) && \
+        curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/automatic_packages.sh" --output /automatic_packages.sh  && \
+        chmod 777 /automatic_packages.sh && \
+        eval /./automatic_packages.sh "${PACKAGES:-}" && \
         rm /automatic_packages.sh
 
     ) >/dev/null
