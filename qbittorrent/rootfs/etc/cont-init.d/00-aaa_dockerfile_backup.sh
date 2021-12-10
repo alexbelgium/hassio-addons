9687a41b3b178dd7b37b82dcaf8971dc5643a15b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-10 20:34:21 +0100

    Update 00-aaa_dockerfile_backup.sh

--- qbittorrent/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ qbittorrent/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 # If dockerfile failed install manually
-if [ ! -f "/usr/bin/bashio" ]; then
-    echo "Bashio does not exist, executing script"
+if [ -e "/ENVFILE" ]; then
+    echo "Executing script"
+    PACKAGES=$(< /ENVFILE)
     (
-        ################
-        # Install apps #
-        ################
-        PACKAGES="${PACKAGES:="curl"}"
-        
-        apt-get clean \
-        && apt-get update \
-        && apt-get install -y --no-install-recommends ${PACKAGES} 2>/dev/null \
-        || apk add --no-cache ${PACKAGES}
-
-        ###################
-        # Install bashio #
-        ##################
-        mkdir -p /tmp/bashio
-        curl -L -f -s "https://github.com/hassio-addons/bashio/archive/v${BASHIO_VERSION}.tar.gz" |
-            tar -xzf - --strip 1 -C /tmp/bashio
-        mv /tmp/bashio/lib /usr/lib/bashio
-        ln -s /usr/lib/bashio/bashio /usr/bin/bashio
-        rm -rf /tmp/bashio
+        #######################
+        # Automatic installer #
+        #######################
+        $(ls /bin/bash &>/dev/null) || (apt-get install -y --no-install-recommends bash || apk add --no-cache bash) && \
+        $(curl --help &>/dev/null) || (apt-get install -y --no-install-recommends curl &>/dev/null || apk add --no-cache curl) && \
+        curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/automatic_packages.sh" --output /automatic_packages.sh  && \
+        chmod 777 /automatic_packages.sh && \
+        eval /./automatic_packages.sh "$PACKAGES" && \
+        rm /automatic_packages.sh
 
     ) >/dev/null
 
