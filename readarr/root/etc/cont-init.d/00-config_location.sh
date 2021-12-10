355653576fb507bdf88ee4d192d1e8387a49a4ca
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-10 14:29:39 +0100

    Update 00-config_location.sh

--- readarr/root/etc/cont-init.d/00-config_location.sh
+++ readarr/root/etc/cont-init.d/00-config_location.sh
@@ -4,5 +4,7 @@ CONFIG_LOCATION=$(bashio::config 'config_location')
 bashio::log.info "Config stored in $CONFIG_LOCATION"
 
 mkdir -p $CONFIG_LOCATION
+chown -R abc:abc $CONFIG_LOCATION
+
 sed -i "s|/config/readarrg|$CONFIG_LOCATION|g" /etc/services.d/readarr/run
 sed -i "s|/config/readarr|$CONFIG_LOCATION|g" /etc/cont-init.d/30-config
