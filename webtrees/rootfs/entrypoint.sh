5a002f940d4852572c7f0d7c54f3e73f7205d272
Alexandre <alexandre.pary@gmail.com>
2021-12-18 17:55:08 +0100

    keep original location

--- webtrees/rootfs/entrypoint.sh
+++ webtrees/rootfs/entrypoint.sh
@@ -6,12 +6,11 @@ echo "Starting..."
 # Starting scripts #
 ####################
 
-cd /
-for SCRIPTS in scripts/*; do
+for SCRIPTS in /scripts/*; do
   [ -e "$SCRIPTS" ] || continue
   echo "$SCRIPTS: executing"
   chown $(id -u):$(id -g) $SCRIPTS
   chmod a+x $SCRIPTS
   sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' $SCRIPTS || true
-  ./$SCRIPTS || echo "$SCRIPTS: exiting $?"
+  /./$SCRIPTS || echo "$SCRIPTS: exiting $?"
 done
