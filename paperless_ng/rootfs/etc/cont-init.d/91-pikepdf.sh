f64de72c47779a0412dfba8bccbb9364966cb644
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-22 03:18:15 +0100

    Update 91-pikepdf.sh

--- paperless_ng/rootfs/etc/cont-init.d/91-pikepdf.sh
+++ paperless_ng/rootfs/etc/cont-init.d/91-pikepdf.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "Installing pikepdf..." 
-(apt-get update
-apt-get install -yqq libxml2-dev libxslt-dev python-dev libjpeg-dev zlib1g-dev python3-dev build-essential
-pip install pikepdf --force-reinstall) >/dev/null
+pip install pikepdf --force-reinstall >/dev/null
 echo "... success!" 
