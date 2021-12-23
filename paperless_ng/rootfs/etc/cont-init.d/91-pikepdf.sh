a21a423ce364b98c3cb022baeb877b08108153f6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-23 08:12:51 +0100

    Update 91-pikepdf.sh

--- paperless_ng/rootfs/etc/cont-init.d/91-pikepdf.sh
+++ paperless_ng/rootfs/etc/cont-init.d/91-pikepdf.sh
@@ -7,8 +7,7 @@ export TERM="xterm-256color"
 apt-get update
 apt-get install -yq libxml2-dev libxslt-dev python-dev
 apt-get install -yq libjpeg-dev zlib1g-dev
-apt-get install -yq python3-dev build-essential -y && \
-pip install -yq pikepdf==2.16.1 --force-reinstall
- 
-pip install pikepdf --force-reinstall) >/dev/null
+apt-get install -yq python3-dev build-essential
+pip install pikepdf --force-reinstall
+) >/dev/null
 echo "... success!" 
