0346768b6c8a483fc322cd393931b8bd60f01679
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-25 07:50:38 +0100

    Update 93-multiocr.sh

--- paperless_ng/rootfs/etc/cont-init.d/93-multiocr.sh
+++ paperless_ng/rootfs/etc/cont-init.d/93-multiocr.sh
@@ -4,16 +4,17 @@ OCRLANG=$(bashio::config 'OCRLANG')
 if [ -n "$OCRLANG" ]; then
   apt-get update &>/dev/null
   echo "OCRLANG variable is set, processing the language packages"
-  for i in $(echo "$OCRLANG" | tr "," " " "+"); do
+  for i in ${OCRLANG//,/ }; do
     if apt-cache show tesseract-ocr-"${i}" > /dev/null 2>&1; then
       echo "installing tesseract-ocr-${i}"
       apt-get install -y tesseract-ocr-"${i}"
 
             # Downloading trainer data
             # Downloading trainer data
+        cd "$(find / -name "tessdata")"
         #    cd /usr/share/tessdata
-        #    sudo rm -r $LANG.traineddata &>/dev/null || true
-        #    wget https://github.com/tesseract-ocr/tessdata/raw/main/$LANG.traineddata &>/dev/null
+        rm -r $LANG.traineddata &>/dev/null || true
+        wget https://github.com/tesseract-ocr/tessdata/raw/main/$LANG.traineddata &>/dev/null
     else
       echo "package tesseract-ocr-${i} not found in the repository, skipping"
     fi
