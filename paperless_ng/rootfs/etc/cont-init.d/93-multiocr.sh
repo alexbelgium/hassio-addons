b7c0556a00c2f01b76982a458a67d73d1dba02d8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-25 23:34:32 +0100

    Update 93-multiocr.sh

--- paperless_ng/rootfs/etc/cont-init.d/93-multiocr.sh
+++ paperless_ng/rootfs/etc/cont-init.d/93-multiocr.sh
@@ -6,11 +6,10 @@ if [ -n "$OCRLANG" ]; then
   echo "OCRLANG variable is set, processing the language packages"
   for i in ${OCRLANG//,/ }; do
     if apt-cache show tesseract-ocr-"${i}" > /dev/null 2>&1; then
-      echo "installing tesseract-ocr-${i}"
-      apt-get install -y tesseract-ocr-"${i}"
+      apt-get install -yq tesseract-ocr-"${i}"
+      bashio::log.info "OCR Language installed : $i" || bashio::log.fatal "Couldn't install OCR lang $i. Please check its format is conform"
     else
       echo "package tesseract-ocr-${i} not found in the repository, skipping"
     fi
-    bashio::log.info "OCR Language installed : $i" || bashio::log.fatal "Couldn't install OCR lang $i. Please check its format is conform"
   done
 fi
