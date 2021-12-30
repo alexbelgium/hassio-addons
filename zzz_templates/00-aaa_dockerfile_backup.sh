1bfa9e0267d22a61247cd2809089096f9df77b15
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-30 13:29:35 +0100

    Update 00-aaa_dockerfile_backup.sh

--- zzz_templates/00-aaa_dockerfile_backup.sh
+++ zzz_templates/00-aaa_dockerfile_backup.sh
@@ -29,10 +29,10 @@ if [ -e "/MODULESFILE" ]; then
             if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl); fi &&
             mkdir -p /tmpscripts /scripts /etc/cont-init.d \
             && for scripts in "$PACKAGES"; do \
-            && curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/$PACKAGES" -o /tmpscripts/$PACKAGES \
+            && curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/$PACKAGES" -o /tmpscripts/$scripts \
             done \
-            && /bin/cp -rf /tmpscripts/* /scripts \
-            && /bin/cp -rf /tmpscripts/* /etc/cont-init.d \
+            && /bin/cp -rf /tmpscripts/* /scripts/ \
+            && /bin/cp -rf /tmpscripts/* /etc/cont-init.d/ \
             && chmod -R 777 /scripts \
             && chmod -R 777 /etc/cont-init.d \
             rm -rf /tmpscripts
