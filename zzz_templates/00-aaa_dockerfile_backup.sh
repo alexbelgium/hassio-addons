21f704e16753249c0f393df284d101ae52541e46
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2021-12-30 13:26:27 +0100

    Update 00-aaa_dockerfile_backup.sh

--- zzz_templates/00-aaa_dockerfile_backup.sh
+++ zzz_templates/00-aaa_dockerfile_backup.sh
@@ -17,3 +17,25 @@ if [ -e "/ENVFILE" ]; then
     ) >/dev/null
 
 fi
+
+if [ -e "/MODULESFILE" ]; then
+    echo "Executing modules script"
+    PACKAGES=$(</MODULESFILE)
+    (
+        ##############################
+        # Automatic modules download #
+        ##############################
+        if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash); fi &&
+            if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl); fi &&
+            mkdir -p /tmpscripts /scripts /etc/cont-init.d \
+            && for scripts in "$PACKAGES"; do \
+            && curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/zzz_templates/$PACKAGES" -o /tmpscripts/$PACKAGES \
+            done \
+            && /bin/cp -rf /tmpscripts/* /scripts \
+            && /bin/cp -rf /tmpscripts/* /etc/cont-init.d \
+            && chmod -R 777 /scripts \
+            && chmod -R 777 /etc/cont-init.d \
+            rm -rf /tmpscripts
+    ) >/dev/null
+
+fi
