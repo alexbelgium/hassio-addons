03ffdd0d4e61a236d5e06edff160cb2b1ec48996
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-05 10:07:28 +0100

    Create 20-folders.sh

---
+++ papermerge/rootfs/etc/cont-init.d/20-folders.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bashio
+
+# Create directory
+mkdir -p /config/addons_config/papermerge/config
+
+# Copy previous config if existing
+if [ -d /data/config ]; then
+  echo "Moving to new location /config/addons_config/papermerge"
+  mv /data/config/* /config/addons_config/papermerge/config/
+  rm -r /data/config
+fi
+
+# Make symlinks
+ln -snf -T /config/addons_config/papermerge /data/config
+
+# Make sure permissions are right
+chown -R $(id -u):$(id -g) /config/addons_config/papermerge
