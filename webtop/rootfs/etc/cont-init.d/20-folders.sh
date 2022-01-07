275e6c4ae7f0b6c8342416ca44b91b79bada1b32
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-07 08:06:43 +0100

    Update 20-folders.sh

--- webtop/rootfs/etc/cont-init.d/20-folders.sh
+++ webtop/rootfs/etc/cont-init.d/20-folders.sh
@@ -2,7 +2,8 @@
 
 mkdir -p /share/webtop-alpine
 chown abc:abc /share/webtop-alpine
-if [ ! -f /config/configuration.html ]; then
-rm -r /config
-fi
+if [ ! -f /config/configuration.yaml ]; then
+  rm -r /config
 ln -s /share/webtop-alpine /config
+echo "Data folder set to /share/webtop-alpine"
+fi
