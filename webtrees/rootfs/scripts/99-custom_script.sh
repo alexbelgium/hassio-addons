00c123a008d845a0e64006e55f6487073cc7a8b7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-07 16:32:25 +0100

    Create 99-custom_script.sh

---
+++ webtrees/rootfs/scripts/99-custom_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CONFIGSOURCE=$(bashio::config "CONFIG_LOCATION")
+CONFIGSOURCE="$(dirname "${CONFIGSOURCE}")"
+
+if [ -f "$CONFIGSOURCE"/script.sh ]; then
+"$CONFIGSOURCE"./script.sh
+fi
