bc46d94ce7370a92a6fa3dfefcaae75edc3bd11d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-08 16:52:30 +0100

    Update 20-folders.sh

--- webtop/rootfs/etc/cont-init.d/20-folders.sh
+++ webtop/rootfs/etc/cont-init.d/20-folders.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
-for FOLDERS in "Desktop" "thinclient_drives" ".config"; do
-mkdir -p /share/webtop-alpine/$FOLDERS
-if [ -d /config/$FOLDERS ]; then
-  cp /config/$FOLDERS/* /share/webtop-alpine/$FOLDERS
-  rm -r /config/$FOLDERS
-fi
-ln -s /share/webtop-alpine/$FOLDERS /config
-done
+HOME=${HOME:-/share/webtop}
+mkdir -p $HOME
+chown -R abc:abc $HOME
+bashio::log.info "Your files are located in $HOME"
 
-echo "Data folder set to /share/webtop-alpine"
-chown -R abc:abc /share/webtop-alpine
+#for FOLDERS in "Desktop" "thinclient_drives" ".config"; do
+#mkdir -p /share/webtop-alpine/$FOLDERS
+#if [ -d /config/$FOLDERS ]; then
+#  cp /config/$FOLDERS/* /share/webtop-alpine/$FOLDERS
+#  rm -r /config/$FOLDERS
+#fi
+#ln -s /share/webtop-alpine/$FOLDERS /config
+#done
