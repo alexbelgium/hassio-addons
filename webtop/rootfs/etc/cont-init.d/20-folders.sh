a2aa3e20a5d71104fee2fe299bc2e2e302ef0344
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-08 17:54:26 +0100

    Update 20-folders.sh

--- webtop/rootfs/etc/cont-init.d/20-folders.sh
+++ webtop/rootfs/etc/cont-init.d/20-folders.sh
@@ -6,11 +6,11 @@ mkdir -p $HOME
 chown -R abc:abc $HOME
 
 # Create symlinks
-for FOLDERS in ".config" ".local" "Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos" "thinclient_drives"; do
-mkdir -p $HOME/$FOLDERS
-if [ -d /config/$FOLDERS ]; then
-  cp /config/$FOLDERS/* $HOME/$FOLDERS
-  rm -r /config/$FOLDERS
-fi
-ln -s $HOME/$FOLDERS /config
-done
+#for FOLDERS in ".config" ".local" "Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos" "thinclient_drives"; do
+#mkdir -p $HOME/$FOLDERS
+#if [ -d /config/$FOLDERS ]; then
+#  cp /config/$FOLDERS/* $HOME/$FOLDERS
+#  rm -r /config/$FOLDERS
+#fi
+#ln -s $HOME/$FOLDERS /config
+#done
