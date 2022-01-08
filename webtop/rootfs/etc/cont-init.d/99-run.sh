662959c0a362c807c225de7996849370c222926c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-08 21:17:02 +0100

    Update 99-run.sh

--- webtop/rootfs/etc/cont-init.d/99-run.sh
+++ webtop/rootfs/etc/cont-init.d/99-run.sh
@@ -5,8 +5,3 @@ if [ -f /usr/lib/dbus-1.0/dbus-daemon-launch-helper ]; then
 chmod u+s /usr/lib/dbus-1.0/dbus-daemon-launch-helper
 service dbus restart
 fi
-
-# Enable acceleration
-mkdir -p /share/webtop/boot
-touch /share/webtop/boot/config.txt
-ln -s /share/webtop/boot/config.txt /boot/
