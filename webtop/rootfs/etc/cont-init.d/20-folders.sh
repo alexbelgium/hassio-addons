0edeb4df41b44b4fe460722e1827acbd1f27d76d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-08 17:52:39 +0100

    Update 20-folders.sh

--- webtop/rootfs/etc/cont-init.d/20-folders.sh
+++ webtop/rootfs/etc/cont-init.d/20-folders.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
+# Define home
 HOME="/share/webtop"
 mkdir -p $HOME
 chown -R abc:abc $HOME
-cp -rn /config/* $HOME/
-bashio::log.info "Your files are located in $HOME"
 
-#for FOLDERS in "Desktop" "thinclient_drives" ".config"; do
-#mkdir -p /share/webtop-alpine/$FOLDERS
-#if [ -d /config/$FOLDERS ]; then
-#  cp /config/$FOLDERS/* /share/webtop-alpine/$FOLDERS
-#  rm -r /config/$FOLDERS
-#fi
-#ln -s /share/webtop-alpine/$FOLDERS /config
-#done
+# Create symlinks
+for FOLDERS in ".config" ".local" "Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos" "thinclient_drives"; do
+mkdir -p $HOME/$FOLDERS
+if [ -d /config/$FOLDERS ]; then
+  cp /config/$FOLDERS/* $HOME/$FOLDERS
+  rm -r /config/$FOLDERS
+fi
+ln -s $HOME/$FOLDERS /config
+done
