ed4a1bea970bf76359002a976a8ccb9954dc7324
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-08 20:43:14 +0100

    Update 99-run.sh

--- webtop/rootfs/etc/cont-init.d/99-run.sh
+++ webtop/rootfs/etc/cont-init.d/99-run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
+# Fix mate software center
+if [ -f /usr/lib/dbus-1.0/dbus-daemon-launch-helper ]; then
+chmod u+s /usr/lib/dbus-1.0/dbus-daemon-launch-helper
 service dbus restart
+fi
