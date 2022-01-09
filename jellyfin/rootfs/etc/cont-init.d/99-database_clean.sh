9c5521c407a7dcd09cc25a8ce630458927e3ab3b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-09 09:36:10 +0100

    Delete 99-database_clean.sh

--- jellyfin/rootfs/etc/cont-init.d/99-database_clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Set variable
-db=/config/addons_config/jellyfin/library.db
-
-#Modify base
-if [ -f $db ]; then
-sqlite3 -quote ${db} "UPDATE TypedBaseItems SET data = replace( data, '/config/jellyfin/', 'config/addons_config/jellyfin/' ) WHERE data LIKE '/config/jellyfin/%'; UPDATE TypedBaseItems SET path = replace( path, '/config/jellyfin/', 'config/addons_config/jellyfin/' ) WHERE path LIKE '/config/jellyfin/%';" 
-fi
