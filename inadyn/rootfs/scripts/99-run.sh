ae8c3274074e0b144d594ad87e1b9eba75bb2ac4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-10 21:30:44 +0100

    Update 99-run.sh

--- inadyn/rootfs/scripts/99-run.sh
+++ inadyn/rootfs/scripts/99-run.sh
@@ -5,6 +5,9 @@
 #################
 mustache-cli /data/options.json /templates/inadyn.mustache >/etc/inadyn.conf
 
+# Check it
+/usr/sbin/inadyn --check-config
+
 ##############
 # Launch App #
 ##############
