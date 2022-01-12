6ffdecdbc56122ed3a61e256fa6c2dc784d8ef38
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-12 08:46:08 +0100

    Update 99-run.sh

--- webtop/rootfs/etc/cont-init.d/99-run.sh
+++ webtop/rootfs/etc/cont-init.d/99-run.sh
@@ -13,10 +13,6 @@ chmod u+s /usr/lib/dbus-1.0/dbus-daemon-launch-helper
 service dbus restart
 fi
 
-# Spotify source
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/spotify-archive-keyring.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
 # Install specific apps
 if bashio::config.has_value 'additional_apps'; then
     bashio::log.info "Installing additional apps :" 
