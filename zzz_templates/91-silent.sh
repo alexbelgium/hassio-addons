ecde46236e9e9828824608c59735ebc8af4b4ab8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-16 13:24:54 +0100

    Update 91-silent.sh

--- zzz_templates/91-silent.sh
+++ zzz_templates/91-silent.sh
@@ -7,6 +7,6 @@
 if bashio::config.true 'silent'; then
 APPEND=' > /dev/null'
 [ -f /etc/services.d/*/run ] && sed -i '$s|$|'"$APPEND"'|' /etc/services.d/*/run
-[ -f /etc/cont-init.d/*/*run* ] && sed -i '$s|$|'"$APPEND"'|' /etc/cont-init.d/*/run
+[ -f /etc/cont-init.d/*/*run* ] && sed -i '$s|$|'"$APPEND"'|' /etc/cont-init.d/*/*run*
 bashio::log.info 'Silent mode activated, all logs from emby server are hidden. Disable this option if you need to troubleshoot the addon.'
 fi
