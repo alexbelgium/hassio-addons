31f868e937621b18c0a9a172a82a20ec3ee55210
Alexandre <alexandre.pary@gmail.com>
2022-01-17 13:26:36 +0100

    Adapt shebang if no s6 supervision

--- enedisgateway2mqtt_dev/rootfs/entrypoint.sh
+++ enedisgateway2mqtt_dev/rootfs/entrypoint.sh
@@ -11,6 +11,6 @@ for SCRIPTS in /etc/cont-init.d/*; do
   chown "$(id -u)":"$(id -g)" "$SCRIPTS"
   chmod a+x "$SCRIPTS"
   # Change shebang if no s6 supervision
-  if [ -d /etc/s6 ]; then sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' "$SCRIPTS"; fi
+  if [ ! -d /etc/s6 ]; then sed -i 's|/usr/bin/with-contenv bashio|/usr/bin/env bashio|g' "$SCRIPTS"; fi
   /./"$SCRIPTS" || echo "$SCRIPTS: exiting $?"
 done
