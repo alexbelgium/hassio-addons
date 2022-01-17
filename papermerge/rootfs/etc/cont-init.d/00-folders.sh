c2486872afbb12dd725da47e955fc93ec50490a7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-17 18:23:27 +0100

    Update and rename 20-folders.sh to 00-folders.sh

--- papermerge/rootfs/etc/cont-init.d/00-folders.sh
+++ papermerge/rootfs/etc/cont-init.d/00-folders.sh
@@ -2,19 +2,18 @@
 
 CONFIGLOCATION=$(bashio::config "CONFIG_LOCATION")
 
+sed -i "s| /data/config| $CONFIGLOCATION|g" /etc/cont-init.d/* \
+sed -i "s| /data/config| $CONFIGLOCATION|g" /defaults/* || true
+
 # Create directory
 mkdir -p $CONFIGLOCATION/config
 
 # Copy previous config if existing
-unlink /data/config 2>/dev/null || true
 if [ -d /data/config ]; then
   echo "Moving to new location $CONFIGLOCATION"
   mv /data/config/* $CONFIGLOCATION/config/
   rm -r /data/config
 fi
-
-# Make symlinks
-ln -s $CONFIGLOCATION/config /data/config
 
 # Make sure permissions are right
 chown -R $(id -u):$(id -g) $CONFIGLOCATION
