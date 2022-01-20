c7b06cbd7dbe61f6d6739e27a7600899e4789ca4
Alexandre <alexandre.pary@gmail.com>
2022-01-20 09:26:30 +0100

    correct

--- gazpar2mqtt/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ gazpar2mqtt/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -9,12 +9,13 @@ if [ -e "/MODULESFILE" ]; then
     MODULES=$(</MODULESFILE)
     MODULES="${MODULES:-00-banner.sh}"
 
-    ( if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash); fi && \
-            if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl); fi && \
-            mkdir -p /etc/cont-init.d && \
-            for scripts in "$MODULES"; do curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts"; done && \
-            chmod -R 755 /etc/cont-init.d && \
-            || printf '%s\n' "${MODULES}" >/MODULESFILE ) >/dev/null
+    (
+        if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash); fi && \
+        if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl); fi && \
+        mkdir -p /etc/cont-init.d && \
+        for scripts in "$MODULES"; do curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts"; done && \
+        chmod -R 755 /etc/cont-init.d || printf '%s\n' "${MODULES}" >/MODULESFILE
+    ) >/dev/null
 
 fi
 
@@ -26,9 +27,9 @@ if [ -e "/ENVFILE" ]; then
     PACKAGES=$(</ENVFILE)
 
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi && \
-        if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi && \
-        curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh && \
-        chmod 777 /automatic_packages.sh && \
-        eval /./automatic_packages.sh "$PACKAGES" && \
-        rm /automatic_packages.sh
+    if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi && \
+    curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh && \
+    chmod 777 /automatic_packages.sh && \
+    eval /./automatic_packages.sh "$PACKAGES" && \
+    rm /automatic_packages.sh
 fi
