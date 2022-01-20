acb295338db52028f6d69045fb084762645a2f7c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-20 22:06:41 +0100

    https://github.com/alexbelgium/hassio-addons/issues/176

---
+++ nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -d /data/config/www/nextcloud/apps/pdfannotate ]; then
+  CURRENT=$PWD
+  cd /data/config/www/nextcloud/apps
+  git clone https://gitlab.com/nextcloud-other/nextcloud-annotate pdfannotate
+  cd $PWD
+  apk add --no-cache ghostscript >/dev/null
+  echo "Nextcloud annotate app added to Nextcloud app store"
+fi
