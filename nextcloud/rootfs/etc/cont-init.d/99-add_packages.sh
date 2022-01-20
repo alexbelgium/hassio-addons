216b4a4956e6048d4ecad2a5f38512c2d2fe4b48
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-20 22:10:36 +0100

    Update 99-add_packages.sh

--- nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
+++ nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -d /data/config/www/nextcloud/apps/pdfannotate ]; then
-  CURRENT=$PWD
+  CURRENT="$PWD"
   cd /data/config/www/nextcloud/apps
   git clone https://gitlab.com/nextcloud-other/nextcloud-annotate pdfannotate
-  cd $PWD
+  cd "$CURRENT"
   apk add --no-cache ghostscript >/dev/null
   echo "Nextcloud annotate app added to Nextcloud app store"
 fi
