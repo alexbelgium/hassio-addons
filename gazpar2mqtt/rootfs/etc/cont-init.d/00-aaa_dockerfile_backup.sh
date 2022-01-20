20f5fd27a06fd34f5047f6e61fd8a2ccb991c481
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-20 20:03:15 +0100

    Update 00-aaa_dockerfile_backup.sh

--- gazpar2mqtt/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ gazpar2mqtt/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -6,14 +6,14 @@
 # Automatic modules download #
 ##############################
 if [ -e "/MODULESFILE" ]; then
-    echo "Executing modules script"
     MODULES=$(</MODULESFILE)
     MODULES="${MODULES:-00-banner.sh}"
+    echo "Installing modules : $MODULES" 
 
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi && \
     if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi && \
     mkdir -p /etc/cont-init.d && \
-    for scripts in $MODULES; do curl -L -f -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts" || echo "script failed to install $scripts"; done && \
+    for scripts in $MODULES; do curl -L -f -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/$scripts || echo "script failed to install $scripts"; done && \
     chmod -R 777 /etc/cont-init.d || printf '%s\n' "${MODULES}" >/MODULESFILE
 fi
 
@@ -21,8 +21,8 @@ fi
 # Automatic installer #
 #######################
 if [ -e "/ENVFILE" ]; then
-    echo "Executing script"
     PACKAGES=$(</ENVFILE)
+    echo "Installing apps : $PACKAGES" 
 
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi && \
     if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi && \
