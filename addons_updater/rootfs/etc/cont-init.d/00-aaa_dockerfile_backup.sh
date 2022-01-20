1d3fb52eb9ce5fe6805b7aecac34110ddb11b731
Alexandre <alexandre.pary@gmail.com>
2022-01-20 09:47:01 +0100

    Improved dockerfilebackup code

--- addons_updater/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ addons_updater/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # If dockerfile failed install manually
 
 ##############################
@@ -16,18 +17,17 @@ if [ -e "/MODULESFILE" ]; then
     chmod -R 755 /etc/cont-init.d || printf '%s\n' "${MODULES}" >/MODULESFILE
 fi
 
+#######################
+# Automatic installer #
+#######################
 if [ -e "/ENVFILE" ]; then
     echo "Executing script"
     PACKAGES=$(</ENVFILE)
 
-    #######################
-    # Automatic installer #
-    #######################
-    if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi &&
-        if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi &&
-        curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh &&
-        chmod 777 /automatic_packages.sh &&
-        eval /./automatic_packages.sh "$PACKAGES" &&
-        rm /automatic_packages.sh
-
+    if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi && \
+    if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi && \
+    curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh && \
+    chmod 777 /automatic_packages.sh && \
+    eval /./automatic_packages.sh "$PACKAGES" && \
+    rm /automatic_packages.sh
 fi
--- paperless_ng/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ paperless_ng/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # If dockerfile failed install manually
 
 ##############################
