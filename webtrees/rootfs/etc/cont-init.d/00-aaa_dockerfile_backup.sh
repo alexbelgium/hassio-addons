1247a6eca1d6da6c034738aa78c19950b69d26ab
Alexandre <alexandre.pary@gmail.com>
2022-01-20 20:32:29 +0100

    Correct Dockerfile

--- webtrees/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ webtrees/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -28,6 +28,6 @@ if [ -e "/ENVFILE" ]; then
     && if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi \
     && curl -L -f -s "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh \
     && chmod 777 /automatic_packages.sh \
-    && eval /./automatic_packages.sh "$PACKAGES" \
+    && eval /./automatic_packages.sh "${PACKAGES:-}" \
     rm /automatic_packages.sh
 fi
