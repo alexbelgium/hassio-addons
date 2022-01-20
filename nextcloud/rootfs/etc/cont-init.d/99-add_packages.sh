c7cc475c2c4506d65be859467d18ce55e4103ab5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-20 23:43:36 +0100

    Update 99-add_packages.sh

--- nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
+++ nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d /data/config/www/nextcloud/apps/pdfannotate ]; then
   CURRENT="$PWD"
-  cd /data/config/www/nextcloud/apps
+  cd /data/config/www/nextcloud/apps || exit
   git clone https://gitlab.com/nextcloud-other/nextcloud-annotate pdfannotate
   cd "$CURRENT" || exit
   apk add --no-cache ghostscript >/dev/null
