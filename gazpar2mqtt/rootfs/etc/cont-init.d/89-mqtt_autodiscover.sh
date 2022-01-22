4e451b2cf82d772c1c8095d4988e2c8940e67371
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-22 07:49:11 +0100

    Hadolint

--- gazpar2mqtt/rootfs/etc/cont-init.d/89-mqtt_autodiscover.sh
+++ gazpar2mqtt/rootfs/etc/cont-init.d/89-mqtt_autodiscover.sh
@@ -11,16 +11,16 @@ if bashio::config.true 'mqtt_autodiscover'; then
     if ! bashio::services.available "mqtt"; then bashio::exit.nok "No internal MQTT service found. Please install Mosquitto broker"; fi
     # Get variables
     bashio::log.info "... MQTT service found, fetching server detail (you can enter those manually in your config file) ..."
-    # spellcheck disable=SC2155
-    export MQTT_HOST=$(bashio::services mqtt "host")
-    # spellcheck disable=SC2155
-    export MQTT_PORT=$(bashio::services mqtt "port")
-    # spellcheck disable=SC2155
-    export MQTT_SSL=$(bashio::services mqtt "ssl")
-    # spellcheck disable=SC2155
-    export MQTT_USERNAME=$(bashio::services mqtt "username")
-    # spellcheck disable=SC2155
-    export MQTT_PASSWORD=$(bashio::services mqtt "password")
+    MQTT_HOST=$(bashio::services mqtt "host")
+    export MQTT_HOST 
+    MQTT_PORT=$(bashio::services mqtt "port")
+    export MQTT_PORT 
+    MQTT_SSL=$(bashio::services mqtt "ssl")
+    export MQTT_SSL 
+    MQTT_USERNAME=$(bashio::services mqtt "username")
+    export MQTT_USERNAME
+    MQTT_PASSWORD=$(bashio::services mqtt "password")
+    export MQTT_PASSWORD 
     # Export variables
     for variables in "MQTT_HOST=$MQTT_HOST" "MQTT_PORT=$MQTT_PORT" "MQTT_SSL=$MQTT_SSL" "MQTT_USERNAME=$MQTT_USERNAME" "MQTT_PASSWORD=$MQTT_PASSWORD"; do
         sed -i "1a export $variables" /etc/services.d/*/*run* 2>/dev/null || true
