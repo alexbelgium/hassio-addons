66a161f499e897ace43eb10c3e2560bc44533bb4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-27 06:42:54 +0100

    Update 99-run.sh

--- photoprism/rootfs/etc/cont-init.d/99-run.sh
+++ photoprism/rootfs/etc/cont-init.d/99-run.sh
@@ -41,7 +41,7 @@ mariadb_addon)
     bashio::log.warning "Please ensure this is included in your backups"
     bashio::log.warning "Uninstalling the MariaDB addon will remove any data"
 
-    mysql -h $PHOTOPRISM_DATABASE_SERVER -u $PHOTOPRISM_DATABASE_USER -ppassword $PHOTOPRISM_DATABASE_PASSWORD -e"CREATE DATABASE IF NOT EXISTS $PHOTOPRISM_DATABASE_NAME; CHARACTER SET = 'utf8mb4'; COLLATE = 'utf8mb4_unicode_ci'" || true
+    mysql -h $(bashio::services 'mysql' 'host') -P $(bashio::services 'mysql' 'port') -u $PHOTOPRISM_DATABASE_USER -p $PHOTOPRISM_DATABASE_PASSWORD -e"CREATE DATABASE IF NOT EXISTS $PHOTOPRISM_DATABASE_NAME; CHARACTER SET = 'utf8mb4'; COLLATE = 'utf8mb4_unicode_ci'" || true
     ;;
 esac
 
