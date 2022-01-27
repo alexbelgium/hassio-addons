04a1c5f18922140b65b4b8228fcd18358680e842
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-01-27 05:37:25 +0100

    Update 90-config_yaml.sh

--- .templates/90-config_yaml.sh
+++ .templates/90-config_yaml.sh
@@ -52,14 +52,14 @@ function parse_yaml {
     -e "s| #.*$||g" \
     -e "s|#.*$||g" \
     -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-    -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
+    -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" $1 |
         awk -F$fs '{
       indent = length($1)/2;
       vname[indent] = $2;
       for (i in vname) {if (i > indent) {delete vname[i]}}
       if (length($3) > 0) {
          vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, "$2", "$3");
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
       }
      }'
 }
