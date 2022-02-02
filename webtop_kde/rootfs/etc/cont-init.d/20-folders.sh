f633006c193c111a41d2d981f8809eea2849f69e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-02 10:58:29 +0100

    Update 20-folders.sh

--- webtop_kde/rootfs/etc/cont-init.d/20-folders.sh
+++ webtop_kde/rootfs/etc/cont-init.d/20-folders.sh
@@ -2,16 +2,7 @@
 # shellcheck shell=bash
 
 # Define home
-HOME="/share/webtop"
+HOME="/share/webtop_kde"
 mkdir -p $HOME
 chown -R abc:abc $HOME
-
-# Create symlinks
-#for FOLDERS in ".config" ".local" "Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos" "thinclient_drives"; do
-#mkdir -p $HOME/$FOLDERS
-#if [ -d /config/$FOLDERS ]; then
-#  cp /config/$FOLDERS/* $HOME/$FOLDERS
-#  rm -r /config/$FOLDERS
-#fi
-#ln -s $HOME/$FOLDERS /config
-#done
+chmod -R 755 $HOME
