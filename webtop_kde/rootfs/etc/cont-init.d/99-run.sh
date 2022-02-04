5ab608a9c7b475b9b7b80de537801babc261b639
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-04 15:42:39 +0100

    Update 99-run.sh

--- webtop_kde/rootfs/etc/cont-init.d/99-run.sh
+++ webtop_kde/rootfs/etc/cont-init.d/99-run.sh
@@ -16,11 +16,12 @@ fi
 # Install specific apps
 if bashio::config.has_value 'additional_apps'; then
   bashio::log.info "Installing additional apps :"
-  # Install apps
+  # hadolint ignore=SC2005
   for APP in $(echo "$(bashio::config 'additional_apps')" | tr "," " "); do
     bashio::log.green "... $APP"
     # Test install with both apt-get and snap
-    apt-get install -yqq $APP &>/dev/null || apk add --no-cache $APP &>/dev/null &&
+    # hadolint ignore=SC2015
+    apt-get install -yqq "$APP" &>/dev/null || apk add --no-cache "$APP" &>/dev/null &&
       bashio::log.green "... done" ||
       bashio::log.red "... not successful, please check package name"
   done
