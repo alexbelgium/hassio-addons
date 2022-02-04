e138047523bc506a47e67524cdb5c38c7b45b373
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-04 15:37:25 +0100

    Update 99-run.sh

--- webtop_kde/rootfs/etc/cont-init.d/99-run.sh
+++ webtop_kde/rootfs/etc/cont-init.d/99-run.sh
@@ -13,13 +13,6 @@ if [ -f /usr/lib/dbus-1.0/dbus-daemon-launch-helper ]; then
   service dbus restart
 fi
 
-# Add custom repositories
-echo "Adding custom repository : "
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/releases" >> /etc/apk/repositories
-
 # Install specific apps
 if bashio::config.has_value 'additional_apps'; then
   bashio::log.info "Installing additional apps :"
