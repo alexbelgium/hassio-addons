9bb031be6d7cf61a89049f59ef82ffb1f3fabed8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-05 20:15:07 +0100

    Update 20-folders.sh

--- fireflyiii_data_importer/rootfs/etc/cont-init.d/20-folders.sh
+++ fireflyiii_data_importer/rootfs/etc/cont-init.d/20-folders.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bashio
 
 CONFIGSOURCE=$(bashio::config "CONFIG_LOCATION")
+CONFIGSOURCE=$(dirname ${CONFIGSOURCE})
 
 # Create directory
-mkdir -p "$(dirname ${CONFIGSOURCE})" || true
-mkdir -p "$(dirname ${CONFIGSOURCE}/import_files)" || true
-mkdir -p "$(dirname ${CONFIGSOURCE}/configurations)" || true
+mkdir -p "${CONFIGSOURCE}" || true
+mkdir -p "${CONFIGSOURCE}/import_files)" || true
+mkdir -p "${CONFIGSOURCE}/configurations)" || true
 
 # Make sure permissions are right
-chown -R $(id -u):$(id -g) "$(dirname "${CONFIGSOURCE}")"
+chown -R $(id -u):$(id -g) "${CONFIGSOURCE}"
