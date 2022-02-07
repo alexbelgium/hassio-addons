c0b7bbd0145f8fdec5c6b730183aa34fa5966f8e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-07 12:03:43 +0100

    Update 20-folders.sh

--- fireflyiii_data_importer/rootfs/etc/cont-init.d/20-folders.sh
+++ fireflyiii_data_importer/rootfs/etc/cont-init.d/20-folders.sh
@@ -5,8 +5,8 @@ CONFIGSOURCE=$(dirname $CONFIGSOURCE)
 
 # Create directory
 mkdir -p "$CONFIGSOURCE" || true
-mkdir -p "$CONFIGSOURCE/import_files)" || true
-mkdir -p "$CONFIGSOURCE/configurations)" || true
+mkdir -p "$CONFIGSOURCE/import_files" || true
+mkdir -p "$CONFIGSOURCE/configurations" || true
 
 # Make sure permissions are right
 chown -R $(id -u):$(id -g) "$CONFIGSOURCE"
