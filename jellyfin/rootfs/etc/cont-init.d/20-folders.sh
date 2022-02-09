2e9f31055c875e9d55cbea9de9df8d70f59a1568
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-09 20:21:00 +0100

    Update 20-folders.sh

--- jellyfin/rootfs/etc/cont-init.d/20-folders.sh
+++ jellyfin/rootfs/etc/cont-init.d/20-folders.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
+
+LOCATION=$(bashio::config 'data_location')
 
 if [ ! -d /jellyfin ]; then
   echo "Creating /jellyfin"
@@ -6,79 +8,64 @@ if [ ! -d /jellyfin ]; then
   chown -R abc:abc /jellyfin
 fi
 
-if [ ! -d /share/storage/tv ]; then
-  echo "Creating /share/storage/tv"
-  mkdir -p /share/storage/tv
-  chown -R abc:abc /share/storage/tv
+if [ ! -d $LOCATION/tv ]; then
+  echo "Creating $LOCATION/tv"
+  mkdir -p $LOCATION/tv
+  chown -R abc:abc $LOCATION/tv
 fi
 
-if [ ! -d /share/storage/movies ]; then
-  echo "Creating /share/storage/movies"
-  mkdir -p /share/storage/movies
-  chown -R abc:abc /share/storage/movies
+if [ ! -d $LOCATION/movies ]; then
+  echo "Creating $LOCATION/movies"
+  mkdir -p $LOCATION/movies
+  chown -R abc:abc $LOCATION/movies
 fi
 
-if [ ! -d /share/jellyfin ]; then
-  echo "Creating /share/jellyfin"
-  mkdir -p /share/jellyfin
-  chown -R abc:abc /share/jellyfin
+if [ ! -d $LOCATION ]; then
+  echo "Creating $LOCATION"
+  mkdir -p $LOCATION
+  chown -R abc:abc $LOCATION
 fi
 
 # links
 
 if [ ! -d /jellyfin/cache ]; then
   echo "Creating link for /jellyfin/cache"
-  mkdir -p /share/jellyfin/cache
-  chown -R abc:abc /share/jellyfin/cache
-  ln -s /share/jellyfin/cache /jellyfin/cache
-fi
-
-
-if [ -d /config/jellyfin ] && [ ! -d /config/addons_config/jellyfin ]; then
-  echo "Moving to new location /config/addons_config/jellyfin"
-  mkdir -p /config/addons_config/jellyfin
-  chown -R abc:abc /config/addons_config/jellyfin
-  mv /config/jellyfin/* /config/addons_config/jellyfin/
-  rm /config/jellyfin
-fi
-
-if [ ! -d /config/addons_config/jellyfin ]; then
-  echo "Creating /config/addons_config/jellyfin"
-  mkdir -p /config/addons_config/jellyfin
-  chown -R abc:abc /config/addons_config/jellyfin
+  mkdir -p $LOCATION/cache
+  chown -R abc:abc $LOCATION/cache
+  ln -s $LOCATION/cache /jellyfin/cache
 fi
 
 if [ ! -d /jellyfin/data ]; then
   echo "Creating link for /jellyfin/data"
-  mkdir -p /share/jellyfin/data
-  chown -R abc:abc /share/jellyfin/data
-  ln -s /share/jellyfin/data /jellyfin/data
+  mkdir -p $LOCATION/data
+  chown -R abc:abc $LOCATION/data
+  ln -s $LOCATION/data /jellyfin/data
 fi
 
 if [ ! -d /jellyfin/logs ]; then
   echo "Creating link for /jellyfin/logs"
-  mkdir -p /share/jellyfin/logs
-  chown -R abc:abc /share/jellyfin/logs
-  ln -s /share/jellyfin/logs /jellyfin/logs
+  mkdir -p $LOCATION/logs
+  chown -R abc:abc $LOCATION/logs
+  ln -s $LOCATION/logs /jellyfin/logs
 fi
 
 if [ ! -d /jellyfin/metadata ]; then
   echo "Creating link for /jellyfin/metadata"
-  mkdir -p /share/jellyfin/metadata
-  chown -R abc:abc /share/jellyfin/metadata
-  ln -s /share/jellyfin/metadata /jellyfin/metadata
+  mkdir -p $LOCATION/metadata
+  chown -R abc:abc $LOCATION/metadata
+  ln -s $LOCATION/metadata /jellyfin/metadata
 fi
 
 if [ ! -d /jellyfin/plugins ]; then
   echo "Creating link for /jellyfin/plugins"
-  mkdir -p /share/jellyfin/plugins
-  chown -R abc:abc /share/jellyfin/plugins
-  ln -s /share/jellyfin/plugins /jellyfin/plugins
+  mkdir -p $LOCATION/plugins
+  chown -R abc:abc $LOCATION/plugins
+  ln -s $LOCATION/plugins /jellyfin/plugins
 fi
 
 if [ ! -d /jellyfin/root ]; then
   echo "Creating link for /jellyfin/root"
-  mkdir -p /share/jellyfin/root
-  chown -R abc:abc /share/jellyfin/root
-  ln -s /share/jellyfin/root /jellyfin/root
+  mkdir -p $LOCATION/root
+  chown -R abc:abc $LOCATION/root
+  ln -s $LOCATION/root /jellyfin/root
 fi
