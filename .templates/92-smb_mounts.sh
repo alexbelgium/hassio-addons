bd47a6114934a2a670d3f0d0c9d742d2ea8e4774
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-17 19:31:42 +0100

    Update 92-smb_mounts.sh

--- .templates/92-smb_mounts.sh
+++ .templates/92-smb_mounts.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
+# shellcheck disable=
 
 #########################
 # MOUNT SMB SHARES v1.6 #
@@ -24,12 +25,14 @@ if bashio::config.has_value 'networkdisks'; then
   fi
 
   # Mounting disks
+  # shellcheck disable=SC2086
   for disk in ${MOREDISKS//,/ }; do # Separate comma separated values
 
     # Clean name of network share
+    # shellcheck disable=SC2116
     disk=$(echo $disk | sed "s,/$,,") # Remove / at end of name
-    diskname=${disk//\\//}            #replace \ with /
-    diskname=${diskname##*/}          # Get only last part of the name
+    diskname="${disk//\\//}"            #replace \ with /
+    diskname="${diskname##*/}"          # Get only last part of the name
 
     # Data validation
     if [[ ! $disk =~ ^.*+[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+[/]+.*+$ ]]; then
@@ -38,26 +41,26 @@ if bashio::config.has_value 'networkdisks'; then
     fi
 
     # Prepare mount point
-    mkdir -p /mnt/$diskname
-    chown -R root:root /mnt/$diskname
+    mkdir -p /mnt/"$diskname"
+    chown -R root:root /mnt/"$diskname"
 
     #Tries to mount with default options
-    mount -t cifs -o rw,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$DOMAIN $disk /mnt/$diskname 2>ERRORCODE && MOUNTED=true || MOUNTED=false
+    mount -t cifs -o rw,username="$CIFS_USERNAME",password="${CIFS_PASSWORD}$DOMAIN" "$disk" /mnt/"$diskname" 2>ERRORCODE && MOUNTED=true || MOUNTED=false
 
     # if Fail test different smb and sec versions
-    if [ $MOUNTED = false ]; then
+    if [ "$MOUNTED" = false ]; then
       for SMBVERS in ",vers=3" ",vers=1.0" ",vers=2.1" ",vers=3.0" ",nodfs" ",uid=0,gid=0,forceuid,forcegid" ",noforceuid,noforcegid" ",${DOMAIN:-WORKGROUP}"; do
-        mount -t cifs -o rw,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$SMBVERS $disk /mnt/$diskname 2>/dev/null && MOUNTED=true && break || MOUNTED=false
+        mount -t cifs -o "rw,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$SMBVERS" "$disk" /mnt/"$diskname" 2>/dev/null && MOUNTED=true && break || MOUNTED=false
         for SECVERS in ",sec=ntlmi" ",sec=ntlmv2" ",sec=ntlmv2i" ",sec=ntlmssp" ",sec=ntlmsspi" ",sec=ntlm" ",sec=krb5i" ",sec=krb5" ",iocharset=utf8"; do
-          mount -t cifs -o rw,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$SMBVERS$SECVERS $disk /mnt/$disk name 2>/dev/null && MOUNTED=true && break 2 && break || MOUNTED=false
+          mount -t cifs -o "rw,file_mode=0777,dir_mode=0777,username=$CIFS_USERNAME,password=${CIFS_PASSWORD}$SMBVERS$SECVERS" "$disk" /mnt/"$disk" name 2>/dev/null && MOUNTED=true && break 2 && break || MOUNTED=false
         done
       done
     fi
 
     # Messages
-    if [ $MOUNTED = true ] && [ "mountpoint -q /mnt/$diskname" ]; then
+    if [ "$MOUNTED" = true ] && [ "mountpoint -q /mnt/$diskname" ]; then
       #Test write permissions
-      touch /mnt/$diskname/testaze && rm /mnt/$diskname/testaze &&
+      touch "/mnt/$diskname/testaze" && rm "/mnt/$diskname/testaze" &&
         bashio::log.info "... $disk successfully mounted to /mnt/$diskname with options $SMBVERS$SECVERS" ||
         bashio::log.fatal "Disk is mounted, however unable to write in the shared disk. Please check UID/GID for permissions, and if the share is rw"
 
@@ -69,15 +72,15 @@ if bashio::config.has_value 'networkdisks'; then
       # Provide debugging info
       smbclient -V &>/dev/null || apt-get install smbclient || apk add --no-cache samba-client
       #smbclient $disk -U $CIFS_USERNAME%$CIFS_PASSWORD  || true
-      smbclient -L $disk -U $CIFS_USERNAME%$CIFS_PASSWORD || true
+      smbclient -L $disk -U "$CIFS_USERNAME%$CIFS_PASSWORD" || true
 
       # Error code
       bashio::log.fatal "Error read : $(<ERRORCODE)"
       rm ERRORCODE
 
       # clean folder
-      umount /mnt/$diskname 2>/dev/null || true
-      rmdir /mnt/$diskname || true
+      umount "/mnt/$diskname" 2>/dev/null || true
+      rmdir "/mnt/$diskname" || true
     fi
 
   done
