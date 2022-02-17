62cf6f2a846ee3a51d3d3d31a389fe770e56109c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-17 19:53:20 +0100

    Update 92-local_mounts.sh

--- .templates/92-local_mounts.sh
+++ .templates/92-local_mounts.sh
@@ -25,8 +25,10 @@ if bashio::config.has_value 'localdisks'; then
     mkdir -p /mnt/"$disk"
     chown -R "$(id -u)":"$(id -g)" /mnt/"$disk"
     # Legacy mounting : mount to share if still exists (avoid breaking changes)
+    # shellcheck disable=SC2015
     [ -d /share/"$disk" ] && mount "$devpath"/"$disk" /share/"$disk" || true
     # Mount
+    # shellcheck disable=SC2015
     mount "$devpath"/"$disk" /mnt/"$disk" && bashio::log.info "Success! $disk mounted to /mnt/$disk" || (bashio::log.fatal "Unable to mount local drives! Please check the name." && rmdir /mnt/$disk)
 
   done
