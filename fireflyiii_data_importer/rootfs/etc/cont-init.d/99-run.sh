ed9845962b03544cabc82582e43a013c5120e966
Alexandre <alexandre.pary@gmail.com>
2022-02-18 12:56:00 +0100

    lint

--- fireflyiii_data_importer/rootfs/etc/cont-init.d/99-run.sh
+++ fireflyiii_data_importer/rootfs/etc/cont-init.d/99-run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bashio
 
 CONFIGSOURCE=$(bashio::config "CONFIG_LOCATION")
-CONFIGSOURCE="$(dirname $CONFIGSOURCE)"
+CONFIGSOURCE="$(dirname "$CONFIGSOURCE")"
 
 #################
 # CONFIG IMPORT #
 #################
 
-if [ $(ls -A "$CONFIGSOURCE/configurations") ]; then
+if [ "$(ls -A "$CONFIGSOURCE/configurations")" ]; then
     bashio::log.info "Configurations were found in $CONFIGSOURCE/configurations, they will be loaded."
     JSON_CONFIGURATION_DIR="$CONFIGSOURCE/configurations"
     export JSON_CONFIGURATION_DIR
@@ -18,38 +18,38 @@ fi
 ################
 
 if bashio::config.has_value 'Updates'; then
-    
-    if [ "$(ls -A ${CONFIGSOURCE}/import_files)" ]; then
+
+    if [ "$(ls -A "${CONFIGSOURCE}"/import_files)" ]; then
         # Align update with options
         echo ""
         FREQUENCY=$(bashio::config 'Updates')
         bashio::log.info "$FREQUENCY updates"
         echo ""
-        
+
         # Sets cron // do not delete this message
         cp /templates/cronupdate /etc/cron."${FREQUENCY}"/
         chmod 777 /etc/cron."${FREQUENCY}"/cronupdate
-        
+
         # Sets cron to run with www-data user
         # sed -i 's|root|www-data|g' /etc/crontab
-        
+
         # Starts cron
         service cron start
-        
+
         # Export variables
         IMPORT_DIR_WHITELIST="${CONFIGSOURCE}/import_files"
         export IMPORT_DIR_WHITELIST
-        
+
         bashio::log.info "Automatic updates were requested. The files in ${CONFIGSOURCE}/import_files will be imported $FREQUENCY."
-        
+
     else
         bashio::log.fatal "Automatic updates were requested, but there are no configuration files in ${CONFIGSOURCE}/import_files. There will therefore be be no automatic updates."
     fi
-    
+
 else
-    
+
     bashio::log.info "Automatic updates not set in addon config. If you add configuration files in ${CONFIGSOURCE}/import_files, they won't be automatically updated."
-    
+
 fi
 
 ##############
--- grav/rootfs/etc/cont-init.d/00-ha-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# shellcheck shell=bash
-
-for k in $(bashio::jq "${__BASHIO_ADDON_CONFIG}" 'keys | .[]'); do
-    printf "$(bashio::config $k)" >/var/run/s6/container_environment/$k
-done
