b1cee48de736d06d9157701cc2fe3aa9ea7bad80
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-18 09:56:08 +0100

    Update 00-folders.sh

--- papermerge/rootfs/etc/cont-init.d/00-folders.sh
+++ papermerge/rootfs/etc/cont-init.d/00-folders.sh
@@ -7,14 +7,14 @@ sed -i "s| /data/config| $CONFIGLOCATION|g" /etc/cont-init.d/*
 sed -i "s| /data/config| $CONFIGLOCATION|g" /defaults/* || true
 
 # Create directory
-mkdir -p $CONFIGLOCATION/config
+mkdir -p "$CONFIGLOCATION"/config
 
 # Copy previous config if existing
 if [ -d /data/config ]; then
   echo "Moving to new location $CONFIGLOCATION"
-  mv /data/config/* $CONFIGLOCATION/config/
+  mv /data/config/* "$CONFIGLOCATION"/config/
   rm -r /data/config
 fi
 
 # Make sure permissions are right
-chown -R "$(id -u):$(id -g)" $CONFIGLOCATION
+chown -R "$(id -u):$(id -g)" "$CONFIGLOCATION"
