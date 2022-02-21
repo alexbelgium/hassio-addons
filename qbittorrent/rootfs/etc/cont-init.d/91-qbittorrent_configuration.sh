402746929094ebe3c87b5b07a3595ff59dc772b2
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-02-21 18:49:43 +0100

    Fix initial build

--- qbittorrent/rootfs/etc/cont-init.d/91-qbittorrent_configuration.sh
+++ qbittorrent/rootfs/etc/cont-init.d/91-qbittorrent_configuration.sh
@@ -35,11 +35,11 @@ if bashio::config.has_value 'SavePath'; then
 
   # Replace save path
   CURRENTSAVEPATH=$(sed -n '/Downloads\\SavePath/p' qBittorrent.conf)
-  sed -i "s|${CURRENTSAVEPATH#*=}|$DOWNLOADS|g" qBittorrent.conf
+  sed -i "s|${CURRENTSAVEPATH#*=}|$DOWNLOADS|g" qBittorrent.conf 2>/dev/null || true
 
   # Replace session save path
   CURRENTSAVEPATH=$(sed -n '/Session\\DefaultSavePath/p' qBittorrent.conf)
-  sed -i "s|${CURRENTSAVEPATH#*=}|$DOWNLOADS|g" qBittorrent.conf
+  sed -i "s|${CURRENTSAVEPATH#*=}|$DOWNLOADS|g" qBittorrent.conf 2>/dev/null || true
   
   # Info
   bashio::log.info "Downloads can be found in $DOWNLOADS"
