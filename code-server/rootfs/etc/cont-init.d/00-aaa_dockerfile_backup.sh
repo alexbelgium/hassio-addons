bcce6f385376aa63731ac2cda0e9aa835b43e341
Alexandre <alexandre.pary@gmail.com>
2022-02-25 10:29:55 +0100

    show modules being downloaded

--- code-server/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ code-server/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -13,7 +13,7 @@ if [ -e "/MODULESFILE" ]; then
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi \
     && if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi \
     && mkdir -p /etc/cont-init.d \
-    && for scripts in $MODULES; do curl -L -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts" || (echo "script failed to install $scripts" && exit 1); done \
+    && for scripts in $MODULES; do echo "$scripts" && curl -L -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts" || (echo "script failed to install $scripts" && exit 1); done \
     && chmod -R 755 /etc/cont-init.d
 fi
 
