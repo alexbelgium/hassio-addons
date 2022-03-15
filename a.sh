dd938f11b389df1529217b257d4534143c2ffa7d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-15 11:02:57 +0100

    Delete a.sh

--- a.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-alias jq=/c/Tmp/jq-win64.exe
-
-for files in */*.json; do
-/./c/Tmp/jq-win64.exe --sort-keys . $files > config2.json && cat config2.json > $files && rm config2.json
-done
