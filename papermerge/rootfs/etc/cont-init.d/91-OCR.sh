fcbef09e8a541f11f56a808a273ea66237c7b25b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-21 15:38:06 +0100

    Adapt for custom config location
    https://github.com/alexbelgium/hassio-addons/issues/263

--- papermerge/rootfs/etc/cont-init.d/91-OCR.sh
+++ papermerge/rootfs/etc/cont-init.d/91-OCR.sh
@@ -3,18 +3,26 @@
 
 # Allow OCR setting
 OCRLANG="$(bashio::config "ocrlang")"
+
+#Define location place
+CONFIGLOCATION="$(bashio::config "CONFIG_LOCATION")"
+#If it's a file, get the folder
+if [ ! -d "$CONFIGLOCATION" ]; then
+    CONFIGLOCATION="$(dirname $CONFIGLOCATION)"
+fi
+
 languageCount=$(echo "$OCRLANG" | tr -cd ',' | wc -c)
 languageCount=$((languageCount+1))
 bashio::log.info "Configuring ${languageCount} languages"
 
 if [ -n "$OCRLANG" ]; then
-    lineStart=$(sed -n '/OCR_LANGUAGES/=' /data/config/papermerge.conf.py)
+    lineStart=$(sed -n '/OCR_LANGUAGES/=' "$CONFIGLOCATION"/papermerge.conf.py)
     bashio::log.info "OCRLANG variable is set, processing the language packages"
-    lineEnd=$(sed -n '/}/=' /data/config/papermerge.conf.py)
-    sed -i "${lineStart},${lineEnd}d" /data/config/papermerge.conf.py
+    lineEnd=$(sed -n '/}/=' "$CONFIGLOCATION"/papermerge.conf.py)
+    sed -i "${lineStart},${lineEnd}d" "$CONFIGLOCATION"/papermerge.conf.py
 
     bashio::log.info "Writing new configuration"
-    echo "OCRLANG = {" >> /data/config/papermerge.conf.py
+    echo "OCRLANG = {" >> "$CONFIGLOCATION"/papermerge.conf.py
 
     languages=$(echo "$OCRLANG" | tr "," "\n")
 
@@ -30,18 +38,18 @@ if [ -n "$OCRLANG" ]; then
             bashio::log.info "${language} identified as ${languageFullName}"
             i=$((i+1))
             if [[ $i -eq $languageCount ]]; then
-                echo "  \"$language\" : \"$languageFullName\"" >> /data/config/papermerge.conf.py
+                echo "  \"$language\" : \"$languageFullName\"" >> "$CONFIGLOCATION"/papermerge.conf.py
             elif [[ $i -eq 1 ]]; then
-                echo "  \"$language\" : \"$languageFullName\"," >> /data/config/papermerge.conf.py
+                echo "  \"$language\" : \"$languageFullName\"," >> "$CONFIGLOCATION"/papermerge.conf.py
                 bashio::log.info "Setting default language to ${language}"
-                sed -i "s/^OCR_DEFAULT_LANGUAGE = \"eng\"/OCR_DEFAULT_LANGUAGE = \"${language}\"/g" /data/config/papermerge.conf.py
+                sed -i "s/^OCR_DEFAULT_LANGUAGE = \"eng\"/OCR_DEFAULT_LANGUAGE = \"${language}\"/g" "$CONFIGLOCATION"/papermerge.conf.py
             else
-                echo "  \"$language\" : \"$languageFullName\"," >> /data/config/papermerge.conf.py
+                echo "  \"$language\" : \"$languageFullName\"," >> "$CONFIGLOCATION"/papermerge.conf.py
             fi
             bashio::log.info "... ${language} installed"
         else
             bashio::log.info "Package tesseract-ocr-${language} not found in the repository, skipping"
         fi
     done
-    echo "}" >> /data/config/papermerge.conf.py
+    echo "}" >> "$CONFIGLOCATION"/papermerge.conf.py
 fi
