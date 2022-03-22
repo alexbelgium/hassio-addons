30d44e2444094cda198038bc64782f528a80b265
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-03-22 11:14:51 +0100

    Update 00-folders.sh

--- papermerge/rootfs/etc/cont-init.d/00-folders.sh
+++ papermerge/rootfs/etc/cont-init.d/00-folders.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-CONFIGLOCATION=$(bashio::config "CONFIG_LOCATION")
+# Config location
+CONFIGLOCATION="$(bashio::config 'CONFIG_LOCATION')"
 
+# Text
+bashio::log.info "Setting config location to $CONFIGLOCATION"
 
-if [ ! -d "$CONFIGLOCATION" ]; then
-    CONFIGLOCATION="$(dirname "$CONFIGLOCATION")"
-fi
-
+# Adapt files
 sed -i "s|/data/config|$CONFIGLOCATION|g" /etc/cont-init.d/*
-sed -i "s|/data/config|$CONFIGLOCATION|g" /defaults/*
 
 # Create directory
 mkdir -p "$CONFIGLOCATION"/config
