bd95bd48d79359d569bad3895d621f16916c3725
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-04-24 20:54:36 +0200

    Update 99-run.sh

--- addons_updater/rootfs/etc/cont-init.d/99-run.sh
+++ addons_updater/rootfs/etc/cont-init.d/99-run.sh
@@ -56,11 +56,11 @@ for addons in $(bashio::config "addon|keys"); do
 
     #Define the folder addon
     LOGINFO="... $SLUG : checking slug exists in repo" && if [ "$VERBOSE" = true ]; then bashio::log.info "$LOGINFO"; fi
-    cd /data/"${BASENAME}"/"${SLUG}" || ( bashio::log.error "$SLUG addon not found in this repository. Exiting." && continue )
+    cd /data/"${BASENAME}"/"${SLUG}" || { bashio::log.error "$SLUG addon not found in this repository. Exiting."; continue; }
 
     #Find current version
     LOGINFO="... $SLUG : get current version" && if [ "$VERBOSE" = true ]; then bashio::log.info "$LOGINFO"; fi
-    CURRENT=$(jq .upstream config.json) || ( bashio::log.error "$SLUG addon upstream tag not found in config.json. Exiting." && continue )
+    CURRENT=$(jq .upstream config.json) || { bashio::log.error "$SLUG addon upstream tag not found in config.json. Exiting."; continue; }
 
     if [ "$SOURCE" = "dockerhub" ]; then
         # Use dockerhub as upstream
