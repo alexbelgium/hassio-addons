f2a88762fed039c8e4b92869d48000de01f4a0e3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-04-26 20:06:47 +0200

    Update 99-run.sh

--- addons_updater/rootfs/etc/cont-init.d/99-run.sh
+++ addons_updater/rootfs/etc/cont-init.d/99-run.sh
@@ -31,8 +31,8 @@ fi
 LOGINFO="... parse addons" && if [ "$VERBOSE" = true ]; then bashio::log.info "$LOGINFO"; fi
 
 # Go through all folders, add to filters if not existing
-for f in $( find -- /data/* -maxdepth 0 -type d | sort -r ); do
-if [ -f "$f"/updater.json ]; then
+cd /data/hassio-addons
+for f in $( dirname "$(find -- */updater.json -maxdepth 0 -type d | sort -r )" ); do
     SLUG=$f
     REPOSITORY=$(jq -r .repository /data/"$f"/updater.json)
     UPSTREAM=$(jq -r .upstream /data/"$f"/updater.json)
@@ -176,7 +176,6 @@ if [ -f "$f"/updater.json ]; then
     else
         bashio::log.green "... $SLUG is up-to-date ${CURRENT}"
     fi
-fi
 done || true # Continue even if issue
 
 bashio::log.info "Addons update completed"
