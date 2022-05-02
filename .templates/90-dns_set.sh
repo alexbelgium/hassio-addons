8270653ada75c778bf40d97a70368fcae7f38083
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-02 20:45:32 +0200

    -n instead of ! -z

--- .templates/90-dns_set.sh
+++ .templates/90-dns_set.sh
@@ -27,7 +27,7 @@ if bashio::config.has_value 'DNS_server'; then
     done
 
     # Only add DNS if there are DNS set
-    if [[ ! -z "$DNS" ]]; then
+    if [[ -n "$DNS" ]]; then
         # Write resolv.conf
         # shellcheck disable=SC2059
         printf "${DNS}" >/etc/resolv.conf
