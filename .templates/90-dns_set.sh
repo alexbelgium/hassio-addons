f8cde01e12b3e3140230c9e2c014c582de7e757e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-02 20:40:10 +0200

    Only add DNS if there are DNS set

--- .templates/90-dns_set.sh
+++ .templates/90-dns_set.sh
@@ -16,6 +16,7 @@ if bashio::config.has_value 'DNS_server'; then
     # Get DNS servers
     # shellcheck disable=SC2086
     for server in ${DNSSERVER//,/ }; do # Separate comma separated values
+        # Only add DNS if successful
         if ping -c 1 "$server" &> /dev/null
         then
             DNS="${DNS}nameserver $server\n"
@@ -25,11 +26,17 @@ if bashio::config.has_value 'DNS_server'; then
         fi
     done
 
-    # Write resolv.conf
-    # shellcheck disable=SC2059
-    printf "${DNS}" >/etc/resolv.conf
-    chmod 644 /etc/resolv.conf
-    bashio::log.info "DNS SERVERS set to $DNSLIST"
+    # Only add DNS if there are DNS set
+    if [[ ! -z "$DNS" ]]; then
+        # Write resolv.conf
+        # shellcheck disable=SC2059
+        printf "${DNS}" >/etc/resolv.conf
+        chmod 644 /etc/resolv.conf
+        bashio::log.info "DNS SERVERS set to $DNSLIST"
+    else
+        bashio::log.warning "No valid DNS were found. Using default router (or HA) dns servers."
+    fi
+     
 else
     bashio::log.info "DNS Servers option empty. Using default router (or HA) dns servers."
 fi
