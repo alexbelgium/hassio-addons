e26a2c3a5857c67574b4b4d95ab23cf73c25737c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-04 14:16:06 +0200

    Adding symlinks

--- zoneminder/rootfs/etc/cont-init.d/00-folders.sh
+++ zoneminder/rootfs/etc/cont-init.d/00-folders.sh
@@ -6,7 +6,18 @@
 CONFIGSOURCE="/config/addons_config/zoneminder"
 
 # Create directory
-mkdir -p "$CONFIGSOURCE" || true
+echo "... making sure $CONFIGSOURCE exists"
+mkdir -p "$CONFIGSOURCE"/{events,images,temp} || true
 
 # Make sure permissions are right
+echo "... checking permissions"
 chown -R "$(id -u):$(id -g)" "$CONFIGSOURCE"
+
+# Make symlinks
+echo "... making symlinks"
+rm -rf /var/cache/zoneminder/events
+rm -rf /var/cache/zoneminder/images
+rm -rf /var/cache/zoneminder/temp
+ln -s "$CONFIGSOURCE"/events /var/cache/zoneminder/events
+ln -s "$CONFIGSOURCE"/images /var/cache/zoneminder/images
+ln -s "$CONFIGSOURCE"/temp /var/cache/zoneminder/temp
