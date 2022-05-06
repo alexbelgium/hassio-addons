90347128a8a1a81dd863352f139b36babccc0c4e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-06 10:12:45 +0200

    removed (no shell)

--- unpackerr/rootfs/etc/cont-init.d/20-folders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d /config/addons_config/unpackerr ]; then
-    echo "Creating /config/addons_config/unpackerr"
-    mkdir -p /config/addons_config/unpackerr
-    echo "Updating files ownership"
-    chown -R "$(id -u)":"$(id -g)" /config/addons_config/unpackerr
-fi
