7744f7dfa701ed37b3da8fef4a10d0bb7997c199
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-11 09:11:58 +0200

    initial build

---
+++ guacamole/rootfs/etc/cont-init.d/00-folders.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+# cp -rn /config/* /data
+
+# Define home
+#HOME="/config/addons_config/guacamole"
+#mkdir -p "$HOME"/postgres
+
+#PUID="$(bashio::config "PUID")"
+#PGID="$(bashio::config "PGID")"
+
+#mkdir -p "$HOME"
+#chown -R "$PUID:$PGID" "$HOME"
