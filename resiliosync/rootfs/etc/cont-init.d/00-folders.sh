5068aeb3ff829b938b08c2be4e85fe0821fcea56
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-17 22:10:52 +0200

    Update 00-folders.sh

--- resiliosync/rootfs/etc/cont-init.d/00-folders.sh
+++ resiliosync/rootfs/etc/cont-init.d/00-folders.sh
@@ -4,6 +4,7 @@ echo "Checking folders"
 LOCATION="/share/resiliosync"
 mkdir -p "$LOCATION"/folders
 mkdir -p "$LOCATION"/mounted_folders
+mkdir -p /share/resiliosync_config
 
 echo "Checking permissions"
 chown -R "$(id -u):$(id -g)" "$LOCATION"
