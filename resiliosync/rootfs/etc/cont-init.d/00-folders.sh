4a0c7232c98fb81c983b5ad8eeced66e3a8383c6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-05-18 14:47:19 +0200

    Update 00-folders.sh

--- resiliosync/rootfs/etc/cont-init.d/00-folders.sh
+++ resiliosync/rootfs/etc/cont-init.d/00-folders.sh
@@ -26,6 +26,12 @@ change_folders () {
     grep -rl "$ORIGINALLOCATION" /etc/services.d | xargs sed -i "s|$ORIGINALLOCATION|$CONFIGLOCATION|g"
     sed -i "s=$ORIGINALLOCATION=$CONFIGLOCATION=g" /etc/cont-init.d/10-adduser
     sed -i "s=$ORIGINALLOCATION=$CONFIGLOCATION=g" /defaults/*
+    #if [ -f $ORIGINALLOCATION/sync.conf ]; then
+    #  sed "s|$(jq -r .storage_path "$ORIGINALLOCATION"/sync.conf)||g" $ORIGINALLOCATION/sync.conf
+    #fi
+    #if [ -f $CONFIGLOCATION/sync.conf ]; then
+    #  sed "s|||g" $CONFIGLOCATION/sync.conf
+    #fi
 
     # Create folders
     [ ! -d "$CONFIGLOCATION" ] && echo "Creating $CONFIGLOCATION" && mkdir -p "$CONFIGLOCATION"
