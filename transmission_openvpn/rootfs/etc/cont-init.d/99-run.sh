76e925cce17a4742f2719bf07ab4beacfdae9344
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-06-02 16:53:04 +0200

    Update 99-run.sh

--- transmission_openvpn/rootfs/etc/cont-init.d/99-run.sh
+++ transmission_openvpn/rootfs/etc/cont-init.d/99-run.sh
@@ -9,21 +9,24 @@ for k in $(bashio::jq "/data/options.json" 'keys | .[]'); do
     bashio::log.blue "$k"="$(bashio::config "$k")"
     export "$k"="$(bashio::config "$k")"
 done
-
 echo ""
+
+#######################
+# Run haugene scripts #
+#######################
 
 bashio::log.info "Running userscript"
 chmod +x /etc/transmission/userSetup.sh
 /./etc/transmission/userSetup.sh
-
 echo ""
 
 bashio::log.info "Starting app"
+/./etc/openvpn/start.sh & echo ""
 
-/./etc/openvpn/start.sh & echo "Starting nginx"
+#################
+# Allow ingress #
+#################
 
 bashio::net.wait_for 9091 localhost 900
-
 bashio::log.info "Ingress ready"
-
 exec nginx
