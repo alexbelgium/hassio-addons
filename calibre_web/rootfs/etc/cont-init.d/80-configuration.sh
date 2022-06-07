87725f50e652c84addaa05a8de6752e6da47f04a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-06-07 13:16:21 +0200

    Update 80-configuration.sh

--- calibre_web/rootfs/etc/cont-init.d/80-configuration.sh
+++ calibre_web/rootfs/etc/cont-init.d/80-configuration.sh
@@ -9,4 +9,9 @@ if bashio::config.has_value 'TZ'; then
     ln -snf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime && echo "$TIMEZONE" >/etc/timezone
 fi
 
+# Disable session protection 
+# https://forums.unraid.net/topic/71927-support-linuxserverio-calibre-web/page/5/#comment-1015352
+echo "**** patching calibre-web - removing session protection ****"
+sed -i "/lm.session_protection = 'strong'/d" /app/calibre-web/cps/__init__.py || true
+
 bashio::log.info "Default username:password is admin:admin123"
