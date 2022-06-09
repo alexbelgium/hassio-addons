28f90f2af77b8acb98375a0d36f78a3ab0e2a448
Alexandre <alexandre.pary@gmail.com>
2022-06-09 21:40:31 +0200

    add ca-certificates update by default

--- addons_updater/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
+++ addons_updater/rootfs/etc/cont-init.d/00-aaa_dockerfile_backup.sh
@@ -12,6 +12,7 @@ if [ -e "/MODULESFILE" ]; then
 
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi \
         && if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi \
+        && apt-get update && apt-get install -yqq --no-install-recommends ca-certificates || apk add --no-cache ca-certificates >/dev/null || true; fi \
         && mkdir -p /etc/cont-init.d \
         && for scripts in $MODULES; do echo "$scripts" && curl -f -L -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" -o /etc/cont-init.d/"$scripts" && [ "$(sed -n '/\/bin/p;q' /etc/cont-init.d/"$scripts")" != "" ] || (echo "script failed to install $scripts" && exit 1); done \
         && chmod -R 755 /etc/cont-init.d
@@ -25,11 +26,11 @@ if [ -e "/ENVFILE" ]; then
     echo "Executing dependency script with custom elements : $PACKAGES"
 
     if ! command -v bash >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null; fi \
-        && if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi \
-        && curl -f -L -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh \
-        && chmod 777 /automatic_packages.sh \
-        && eval /./automatic_packages.sh "${PACKAGES:-}" \
-        && rm /automatic_packages.sh
+    && if ! command -v curl >/dev/null 2>/dev/null; then (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null; fi \
+    && curl -f -L -s -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/automatic_packages.sh" --output /automatic_packages.sh \
+    && chmod 777 /automatic_packages.sh \
+    && eval /./automatic_packages.sh "${PACKAGES:-}" \
+    && rm /automatic_packages.sh
 fi
 
 if [ -e "/MODULESFILE" ] && [ ! -f /entrypoint.sh ]; then
