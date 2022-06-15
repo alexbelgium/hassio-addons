3bc62471743504f864ffcccd2c973521a106ac49
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-06-15 16:44:20 +0200

    Update 20-folders.sh

--- teamspeak/rootfs/etc/cont-init.d/20-folders.sh
+++ teamspeak/rootfs/etc/cont-init.d/20-folders.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-LOCATION=$(bashio::config 'data_location')
-
 # Data remanence for /teamspeak/save
 if [ -d /teamspeak/save ]; then
-  cp -rn /teamspeak/save/* "$LOCATION"/ || true
+  cp -rn /teamspeak/save/* /data
   rm -r /teamspeak/save
 fi
+mkdir /teamspeak
 ln -sf /data /teamspeak/save
+chown -R 533:533 /data
 chmod -R 777 /data
+chown -R 533:533 /teamspeak
+chmod -R 777 /teamspeak
