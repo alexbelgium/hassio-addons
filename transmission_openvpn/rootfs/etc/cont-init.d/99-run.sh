916c7d48afcccff77afe9c142747c8f6f0ce899d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-06-16 14:08:33 +0200

    Update 99-run.sh

--- transmission_openvpn/rootfs/etc/cont-init.d/99-run.sh
+++ transmission_openvpn/rootfs/etc/cont-init.d/99-run.sh
@@ -11,6 +11,39 @@ for k in $(bashio::jq "/data/options.json" 'keys | .[]'); do
 done
 echo ""
 
+###################
+# Custom provider #
+###################
+
+if bashio::config.true "OPENVPN_CUSTOM_PROVIDER"; then
+
+OVPNLOCATION="$(bashio::config "OPENVPN_CUSTOM_PROVIDER_OVPN_LOCATION")"
+OPENVPN_PROVIDER="${OVPNLOCATION##*/}"
+OPENVPN_PROVIDER="${OPENVPN_PROVIDER%.*}"
+bashio::log.info "Custom openvpn provider selected"
+
+    # Check that ovpn file exists
+    if [ ! -f bashio::config "OPENVPN_CUSTOM_PROVIDER_OVPN_LOCATION" ]; then
+        bashio::log.fatal "Ovpn file not found at location provided : $OVPNLOCATION"
+        exit 1
+    fi
+
+    # Copy ovpn file
+    echo "Copying ovpn file to proper location"
+    mkdir /openvpn/"$OPENVPN_PROVIDER"
+    cp "$OVPNLOCATION" /openvpn/"$OPENVPN_PROVIDER"/
+
+    # Use custom provider
+    echo "Exporting variable for custom provider : $OPENVPN_PROVIDER"
+    export OPENVPN_PROVIDER="$OPENVPN_PROVIDER"
+    export OPENVPN_CONFIG="$OPENVPN_PROVIDER"
+    
+else
+
+bashio::log.info "Custom openvpn provider not selected, the provider $OPENVPN_PROVIDER will be used"
+
+fi
+
 #######################
 # Run haugene scripts #
 #######################
