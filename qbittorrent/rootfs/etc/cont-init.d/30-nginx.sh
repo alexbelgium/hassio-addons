d95a59803de1f0d368e449b71d4e56c2a0b93db9
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-06-18 06:27:33 +0200

    Update 30-nginx.sh

--- qbittorrent/rootfs/etc/cont-init.d/30-nginx.sh
+++ qbittorrent/rootfs/etc/cont-init.d/30-nginx.sh
@@ -40,6 +40,7 @@ LATEST_RELEASE=$(curl -f -s --retry 5 -L https://api.github.com/repos/wdaan/vuet
     cut -d : -f 2,3 |
     tr -d \" |
 xargs)
+[ "$DEBUG" = "debug" ] && echo "url: $LATEST_RELEASE"
 
 [ "$DEBUG" = "debug" ] && echo "Before curl"
 curl -f -s -S -O -J -L "$LATEST_RELEASE"
