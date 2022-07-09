fa7c837e0af435b5e3e938034e1ea4c0136dd42f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-07-09 21:59:45 +0200

    Update run.sh

--- wger/rootfs/run.sh
+++ wger/rootfs/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bashio
 
+chmod +x /etc/cont-init.d/*
+/./etc/cont-init.d/*
+
 LOCATION=/data
 python3 manage.py migrate || true
 mkdir -p "$LOCATION"
@@ -8,9 +11,6 @@ chown -R wger "$LOCATION" || true
 chmod -R 777 "$LOCATION" || true
 rm /home/wger/db/database.sqlite &>/dev/null || true
 ln -s "$LOCATION"/database.sqlite /home/wger/db
-
-chmod +x /etc/cont-init.d/*
-/./etc/cont-init.d/*
 
 echo "Launch app"
 su -u wger bash -c /./home/wger/entrypoint.sh
