dff291963dafa5e4b3f5f6e657189c2ebe9b1c2e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-07-10 09:13:40 +0200

    Update run.sh

--- wger/rootfs/run.sh
+++ wger/rootfs/run.sh
@@ -19,21 +19,4 @@ chown -R wger:wger "/home/wger"
 chmod -R 777 "$LOCATION"
 
 echo "Launch app"
-su -l wger -c "\
-export S6_CMD_WAIT_FOR_SERVICES=1 \
-S6_CMD_WAIT_FOR_SERVICES_MAXTIME=300000 \
-S6_SERVICES_GRACETIME=300000 \
-PATH=/home/wger/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-FROM_EMAIL='wger Workout Manager <wger@example.com>' \
-DJANGO_DB_DATABASE=/data/database.sqlite \
-DEBIAN_FRONTEND=noninteractive \
-LANG=en_US.UTF-8 \
-LANGUAGE=en_US:en \
-LC_ALL=en_US.UTF-8 \
-PYTHONDONTWRITEBYTECODE=1 \
-PYTHONUNBUFFERED=1 \
-WORKDIR="/home/wger/src" && \
-cd /home/wger/src && \
-DOCKER_DIR=./extras/docker/development && \
-if [ -f ~/.bashrc ]; then source ~/.bashrc; fi && \
-/bin/sh /home/wger/entrypoint.sh"
+su wger -c "/bin/sh /home/wger/entrypoint.sh"
