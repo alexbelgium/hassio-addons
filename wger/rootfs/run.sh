f2874bcb1b5da00ec567dbc2d77a1eb99eaa0f8e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-07-10 08:25:26 +0200

    test

--- wger/rootfs/run.sh
+++ wger/rootfs/run.sh
@@ -23,4 +23,22 @@ ln -s "$LOCATION"/database.sqlite /home/wger/db
 python3 manage.py migrate || true
 
 echo "Launch app"
-su -m wger -c "cd /home/wger/src && export FROM_EMAIL='wger Workout Manager <wger@example.com>' && exec /home/wger/entrypoint.sh"
+su -m wger -c "\
+DOCKER_DIR=./extras/docker/development && \
+/bin/sh -c mkdir ~/media     && \
+pip3 install -e .     && \
+mkdir ~/static     && \
+mkdir ~/db/ && \
+if [ -f .env ]; then export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst); fi && \
+if [ -f ~/.bashrc ]; then source ~/.bashrc; fi && \
+cd /home/wger/src && \
+export FROM_EMAIL='wger Workout Manager <wger@example.com>' && \
+export DJANGO_DB_DATABASE=/data/database.sqlite && \
+export DEBIAN_FRONTEND=noninteractive && \
+LANG=en_US.UTF-8 && \
+LANGUAGE=en_US:en && \
+LC_ALL=en_US.UTF-8 && \
+PYTHONDONTWRITEBYTECODE=1 && \
+PYTHONUNBUFFERED=1 && \
+PATH=/home/wger/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin && \
+/bin/sh /home/wger/entrypoint.sh"
