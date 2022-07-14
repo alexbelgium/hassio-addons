23ff7a93f8d8e35772623e86004fddf9adb788ba
BerghsJelmer <43340072+BerghsJelmer@users.noreply.github.com>
2022-07-14 22:19:55 +0200

    Create 98-fix.sh

---
+++ fireflyiii_data_importer/rootfs/etc/cont-init.d/98-fix.sh
@@ -0,0 +1,1 @@
+sed -i $"s/'description'.*/\'description\'        => \'\' !== trim((string)(\$entry->getDescription() ?? \'\')) ? \$entry->getDescription() : \'(no description)\',/" /var/www/html/app/Services/Nordigen/Conversion/Routine/GenerateTransactions.php
