43f61bfc8819f563217ddcaed4b14e2c113b113c
Alexandre <alexandre.pary@gmail.com>
2022-07-21 15:18:17 +0200

    Accept cookies files

--- epicgamesfree/rootfs/etc/cont-init.d/99-run.sh
+++ epicgamesfree/rootfs/etc/cont-init.d/99-run.sh
@@ -5,19 +5,22 @@
 # Initialize #
 ##############
 
-# Copy default config.json
 HOME="/config/addons_config/epicgamesfree"
 if [ ! -f "$HOME"/config.json ]; then
+    # Copy default config.json
     cp /templates/config.json "$HOME"/config.json
     chmod 777 "$HOME"/config.json
     bashio::log.warning "A default config.json file was copied in $HOME. Please customize according to https://github.com/claabs/epicgames-freegames-node#json-configuration before restarting the addon"
     bashio::exit.nok
 fi
 
-# Make symlink
-
-if [ -f /usr/app/config/config.json ]; then rm /usr/app/config/config.json; fi
-ln -s "$HOME"/config.json /usr/app/config/config.json
+# Make symlink for cookies
+bashio::log.info "The following json files were found in $HOME and will be used in the app :"
+cd "$HOME" || true
+for i in *.json; do # Whitespace-safe but not recursive.
+    echo "... processing $i"
+    ln -s $i /usr/app/config
+done
 
 ##############
 # Launch App #
