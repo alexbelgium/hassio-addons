eb1d60e7979fdf3215d8a2968119be70b3e96387
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-08-01 15:31:36 +0200

    Update 99-add_packages.sh

--- nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
+++ nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Install specific packages
 if [ ! -d /data/config/www/nextcloud/apps/pdfannotate ]; then
     CURRENT="$PWD"
     cd /data/config/www/nextcloud/apps || exit
@@ -7,4 +8,16 @@ if [ ! -d /data/config/www/nextcloud/apps/pdfannotate ]; then
     cd "$CURRENT" || exit
     apk add --no-cache ghostscript >/dev/null
     echo "Nextcloud annotate app added to Nextcloud app store"
+fi
+
+# Install specific apps
+if bashio::config.has_value 'additional_apps'; then
+    bashio::log.info "Installing additional apps :"
+    # hadolint ignore=SC2005
+    NEWAPPS=$(bashio::config 'additional_apps')
+    for APP in ${NEWAPPS//,/ }; do
+        bashio::log.green "... $APP"
+        # shellcheck disable=SC2015
+        apk add --no-cache "$APP" >/dev/null || bashio::log.red "... not successful, please check package name"
+    done
 fi
