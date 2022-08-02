10864ba70d1b6889f3260b2dd188eaaf3f7c4d36
Brad Williams <Deckoz2302@gmail.com>
2022-08-02 16:40:44 -0400

    Update 20-folders.sh

--- jackett/rootfs/etc/cont-init.d/20-folders.sh
+++ jackett/rootfs/etc/cont-init.d/20-folders.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ ! -d /share/downloads ]; then
-    echo "Creating /share/downloads"
-    mkdir -p /share/jackett
-    chown -R jackett:jackett /share/jackett
+if [ ! -d /share/Jackett ]; then
+    echo "Creating /share/Jacket"
+    mkdir /share/Jackett
+    chown -R abc:abc /share/Jackett
 fi
 
 if [ -d /config/Jackett ] && [ ! -d /config/addons_config/Jackett ]; then
     echo "Moving to new location /config/addons_config/Jackett"
     mkdir /config/addons_config/Jackett
-    chown -R jackett:jackett /config/addons_config/Jackett
+    chown -R abc:abc /config/addons_config/Jackett
     mv /config/Jackett/* /config/addons_config/Jackett/
     rm -r /config/Jackett
     rm -r /config/jackett
@@ -18,5 +18,5 @@ fi
 if [ ! -d /config/addons_config/Jackett ]; then
     echo "Creating /config/addons_config/Jackett"
     mkdir /config/addons_config/Jackett
-    chown -R jackett:jackett /config/addons_config/Jackett
+    chown -R abc:abc /config/addons_config/Jackett
 fi
