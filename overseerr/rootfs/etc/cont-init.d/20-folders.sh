ed14b4f84aa3ed5bfaea56dab8c5fd8e65562e78
Brad Williams <Deckoz2302@gmail.com>
2022-08-02 17:21:17 -0400

    Update 20-folders.sh
    adding necessary yarn folder for overseeer, since removing -p from mkdir which no longer changes the ownership of /config when PGID and PUID are set.

--- overseerr/rootfs/etc/cont-init.d/20-folders.sh
+++ overseerr/rootfs/etc/cont-init.d/20-folders.sh
@@ -5,4 +5,10 @@ if [ ! -d /config/addons_config/overseerr ]; then
     mkdir /config/addons_config/overseerr
 fi
 
+if [ ! -d /config/.cache/yarn ]; then
+    echo "Creating /config/.cache/yarn"
+    mkdir /config/.cache/yarn
+fi
+
 chown -R abc:abc /config/addons_config/overseerr
+chown -R abc:abc /config/.cache/yarn
