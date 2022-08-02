9e4204e966b0fd0c75e07029321ef0a234197de3
Brad Williams <Deckoz2302@gmail.com>
2022-08-02 15:24:27 -0400

    Update 20-folders.sh
    Current Script changes ownership of /config and /config/addons_config

--- jackett/rootfs/etc/cont-init.d/20-folders.sh
+++ jackett/rootfs/etc/cont-init.d/20-folders.sh
@@ -2,14 +2,14 @@
 
 if [ ! -d /share/downloads ]; then
     echo "Creating /share/downloads"
-    mkdir -p /share/downloads
-    chown -R abc:abc /share/downloads
+    mkdir -p /share/jackett
+    chown -R jackett:jackett /share/jackett
 fi
 
 if [ -d /config/Jackett ] && [ ! -d /config/addons_config/Jackett ]; then
     echo "Moving to new location /config/addons_config/Jackett"
-    mkdir -p /config/addons_config/Jackett
-    chown -R abc:abc /config/addons_config/Jackett
+    mkdir /config/addons_config/Jackett
+    chown -R jackett:jackett /config/addons_config/Jackett
     mv /config/Jackett/* /config/addons_config/Jackett/
     rm -r /config/Jackett
     rm -r /config/jackett
@@ -17,6 +17,6 @@ fi
 
 if [ ! -d /config/addons_config/Jackett ]; then
     echo "Creating /config/addons_config/Jackett"
-    mkdir -p /config/addons_config/Jackett
-    chown -R abc:abc /config/addons_config/Jackett
+    mkdir /config/addons_config/Jackett
+    chown -R jackett:jackett /config/addons_config/Jackett
 fi
