58f2a59f6e317a00459cd272909b58935a933872
Brad Williams <Deckoz2302@gmail.com>
2022-08-02 17:14:55 -0400

    Update 20-folders.sh
    Current script is changing /config and /config/addons_config ownership when PGID and PUID are set

--- overseerr/rootfs/etc/cont-init.d/20-folders.sh
+++ overseerr/rootfs/etc/cont-init.d/20-folders.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d /config/addons_config/overseerr ]; then
     echo "Creating /config/addons_config/overseerr"
-    mkdir -p /config/addons_config/overseerr
+    mkdir /config/addons_config/overseerr
 fi
 
 chown -R abc:abc /config/addons_config/overseerr
