f36812b8b30a88d7f40b9814c85d43c7bb2b76e5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-09-04 17:14:58 +0200

    Update up-qbittorrent.sh

--- qbittorrent/rootfs/etc/openvpn/up-qbittorrent.sh
+++ qbittorrent/rootfs/etc/openvpn/up-qbittorrent.sh
@@ -3,5 +3,5 @@
 
 WEBUI_PORT=${WEBUI_PORT:-8080}
 
-s6-notifyoncheck -d -n 12 -c "nc -z 127.0.0.1 ${WEBUI_PORT}" \ 
+s6-notifyoncheck -d -n 12 -c "nc -z 127.0.0.1 ${WEBUI_PORT}" \
         s6-setuidgid abc /usr/bin/qbittorrent-nox --webui-port="${WEBUI_PORT}"
