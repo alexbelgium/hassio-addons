96766b75e602c2a8017ed4dda758d91a94629152
Alexandre <alexandre.pary@gmail.com>
2022-09-10 16:40:15 +0200

    Clean

--- qbittorrent/rootfs/etc/cont-init.d/00-folders.sh
+++ qbittorrent/rootfs/etc/cont-init.d/00-folders.sh
@@ -2,6 +2,3 @@
 
 mkdir -p /config/qBittorrent
 
-# Avoid linuxserver anti tamper issues
-chown root:root /config/custom-cont-init.d* &>/dev/null || true
-chown root:root /config/custom-services* &>/dev/null || true
