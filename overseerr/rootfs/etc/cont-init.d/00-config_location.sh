a3a9c862040719d6e167f6455f866312f65a3762
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-09-15 17:02:36 +0200

    Update 00-config_location.sh

--- overseerr/rootfs/etc/cont-init.d/00-config_location.sh
+++ overseerr/rootfs/etc/cont-init.d/00-config_location.sh
@@ -7,6 +7,6 @@ bashio::log.info "Config stored in $CONFIG_LOCATION"
 mkdir -p "$CONFIG_LOCATION"
 chown -R abc:abc "$CONFIG_LOCATION"
 
-for file in $(grep -Esril '/config[ /]|/config$' /etc/logrotate.d /defaults /etc/cont-init.d /etc/services.d /etc/s6-overlay/s6-rc.d); do 
-  sed -i "s=/config/addons_config/overseerr=$CONFIGLOCATION=g" "$file"
+for file in $(grep -Esril "/config/addons_config/overseerr" /etc/logrotate.d /defaults /etc/cont-init.d /etc/services.d /etc/s6-overlay/s6-rc.d); do 
+  sed -i "s=/config/addons_config/overseerr=$CONFIG_LOCATION=g" "$file"
 done
