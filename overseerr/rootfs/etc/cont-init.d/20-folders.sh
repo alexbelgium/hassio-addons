a9642eee242fc534bb5b38b4b521b5e9de202658
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-09-15 10:19:04 +0200

    Update 20-folders.sh

--- overseerr/rootfs/etc/cont-init.d/20-folders.sh
+++ overseerr/rootfs/etc/cont-init.d/20-folders.sh
@@ -5,4 +5,9 @@ if [ ! -d /config/addons_config/overseerr ]; then
     mkdir -p /config/addons_config/overseerr
 fi
 
+if [ ! -d /config/addons_config/addons_config/overseerr ]; then
+    echo "Migrating data to /config/addons_config/overseerr"
+    mv /config/addons_config/addons_config/overseerr /config/addons_config/overseerr
+fi
+
 chown -R abc:abc /config/addons_config/overseerr
