ca1498ea6f3ed1972fb9c4de9c0cc5a42c803b3b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-09-22 10:31:21 +0200

    Lint

--- overseerr/rootfs/etc/cont-init.d/00-config_location.sh
+++ overseerr/rootfs/etc/cont-init.d/00-config_location.sh
@@ -8,6 +8,7 @@ mkdir -p "$CONFIG_LOCATION"
 chown -R abc:abc "$CONFIG_LOCATION"
 chmod -R 755 "$CONFIG_LOCATION"
 
+# shellcheck ignore=SC2013
 for file in $(grep -Esril "/config/addons_config/overseerr" /etc/logrotate.d /defaults /etc/cont-init.d /etc/services.d /etc/s6-overlay/s6-rc.d); do
     sed -i "s=/config/addons_config/overseerr=$CONFIG_LOCATION=g" "$file"
 done
