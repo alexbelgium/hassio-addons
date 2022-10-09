28f677c2a042dcf08f2f8ef6116eec412dde6ce9
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-10-09 11:55:24 +0200

    Update 99-config.sh

--- unpackerr/rootfs/etc/cont-init.d/99-config.sh
+++ unpackerr/rootfs/etc/cont-init.d/99-config.sh
@@ -10,7 +10,7 @@ if bashio::config.has_value "watch_path"; then
   sed -i "/[[folder]]/c [[folder]]" /config/unpackerr.conf
   # Set downloads path
   sed -i "s|_path|_pth|g" /config/unpackerr.conf
-  sed -i "/path =/c path = $(bashio::config 'watch_path')" /config/unpackerr.conf
+  sed -i "/path =/c path = \"$(bashio::config 'watch_path')\"" /config/unpackerr.conf
   sed -i "s|_pth|_path|g" /config/unpackerr.conf
   # Make path
   mkdir -p "$(bashio::config 'watch_path')"
@@ -23,7 +23,7 @@ if bashio::config.has_value "extraction_path"; then
   # Enables folders
   sed -i "/[[folder]]/c [[folder]]" /config/unpackerr.conf
   # Set extraction path
-  sed -i "/extract_path =/c extract_path = $(bashio::config 'extraction_path')" /config/unpackerr.conf
+  sed -i "/extract_path =/c extract_path = \"$(bashio::config 'extraction_path')\"" /config/unpackerr.conf
   # Make path
   mkdir -p "$(bashio::config 'extraction_path')"
   # Set permission
