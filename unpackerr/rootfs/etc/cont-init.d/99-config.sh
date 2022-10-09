71d5e1ef678f2a5c9b5f8b2d55a707ea93170e4a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-10-09 10:43:16 +0200

    Automatic folders definition
    https://github.com/alexbelgium/hassio-addons/issues/502

---
+++ unpackerr/rootfs/etc/cont-init.d/99-config.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bashio
+
+# Set user
+if bashio::config_has.value 'PUID'; then PUID = "$(bashio::config 'PUID')"; fi
+if bashio::config_has.value 'PGID'; then PGID = "$(bashio::config 'PGID')"; fi
+
+# Enable watch folder
+if bashio::config_has.value "watch_path"; then
+  # Enables folders
+  sed -i "/[[folder]]/c [[folder]]" /config/unpackerr.conf
+  # Set downloads path
+  sed -i "s|_path|_pth|g" /config/unpackerr.conf
+  sed -i "/path =/c path = $(bashio::config 'watch_path')" /config/unpackerr.conf
+  sed -i "s|_pth|_path|g" /config/unpackerr.conf
+  # Make path
+  mkdir -p "$(bashio::config 'watch_path')"
+  # Set permission
+  chown -R $PUID:$PGID "$(bashio::config 'watch_path')"
+fi
+
+# Enable extraction folder
+if bashio::config_has.value "extraction_path"; then
+  # Enables folders
+  sed -i "/[[folder]]/c [[folder]]" /config/unpackerr.conf
+  # Set extraction path
+  sed -i "/extract_path =/c extract_path = $(bashio::config 'extraction_path')" /config/unpackerr.conf
+  # Make path
+  mkdir -p "$(bashio::config 'extraction_path')"
+  # Set permission
+  chown -R $PUID:$PGID "$(bashio::config 'extraction_path')"
+fi
