5e94d295f89acb97c9e8d8579a933ed3a9326996
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-10-23 22:59:12 +0100

    Test update

--- omada/rootfs/etc/cont-init.d/00-folders.sh
+++ omada/rootfs/etc/cont-init.d/00-folders.sh
@@ -17,21 +17,24 @@ if [ ! -f "$CONFIGSOURCE" ]; then
 fi
 
 # Ensure structure is correct
-mkdir -p "$CONFIGSOURCE"/db "$CONFIGSOURCE"/html "$CONFIGSOURCE"/pdf
 cp -rnf /opt/tplink/EAPController/data/* "$CONFIGSOURCE"
+
+echo "Creating symlink"
+rm -r /opt/tplink/EAPController/data/*
+
+mkdir -p "$CONFIGSOURCE"/pdf
+mkdir -p "$CONFIGSOURCE"/omada/html
+mkdir -p "$CONFIGSOURCE"/db
+mkdir -p "$CONFIGSOURCE"/map
+mkdir -p "$CONFIGSOURCE"/portal
+
+ln -s "$CONFIGSOURCE"/pdf /opt/tplink/EAPController/data
+ln -s "$CONFIGSOURCE"/omada/html /opt/tplink/EAPController/data
+ln -s "$CONFIGSOURCE"/db /opt/tplink/EAPController/data
+ln -s "$CONFIGSOURCE"/map /opt/tplink/EAPController/data
+ln -s "$CONFIGSOURCE"/portal /opt/tplink/EAPController/data
 
 # Make sure permissions are right
 echo "Updating permissions"
+chmod -R 777 "$CONFIGSOURCE"
 chown -R "508:508" "$CONFIGSOURCE"
-
-# Delete previous directories
-echo "Removing previous directories"
-rm -r /opt/tplink/EAPController/data/html
-rm -r /opt/tplink/EAPController/data/pdf
-rm -r /opt/tplink/EAPController/data/db
-
-# Create symlink
-echo "Creating symlink"
-ln -s /config/addons_config/omada/pdf /opt/tplink/EAPController/data/pdf
-ln -s /config/addons_config/omada/html /opt/tplink/EAPController/data/html
-ln -s /config/addons_config/omada/db /opt/tplink/EAPController/data/db
