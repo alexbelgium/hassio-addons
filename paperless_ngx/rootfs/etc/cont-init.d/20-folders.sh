a07a03cc293923db71c322c4ec3909eadce8b3f8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-02 11:08:19 +0100

    Switch to paperless user

--- paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
@@ -8,4 +8,5 @@ if [ ! -d /config/addons_config/$slug ]; then
     mkdir -p /config/addons_config/$slug
 fi
 
-chmod 777 /config/addons_config/$slug
+chmod -R 755 /config/addons_config/$slug
+chown -R paperless:paperless /config/addons_config/$slug
