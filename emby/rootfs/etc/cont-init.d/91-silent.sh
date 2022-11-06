63dd9c3d10c5f500369b5fe389b5b8b200fbfdca
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-06 13:38:56 +0100

    Update 91-silent.sh

--- emby/rootfs/etc/cont-init.d/91-silent.sh
+++ emby/rootfs/etc/cont-init.d/91-silent.sh
@@ -7,6 +7,6 @@
 
 if bashio::config.true 'silent'; then
     APPEND=' >/dev/null'
-    sed -i '$s|$|'"$APPEND"'|' /etc/s6-overlay/s6-rc.d/*/run
+    sed -i '$s|$|'"$APPEND"'|' /etc/s6-overlay/s6-rc.d/svc-emby/run
     bashio::log.info 'Silent mode activated, all logs from emby server are hidden. Disable this option if you need to troubleshoot the addon.'
 fi
