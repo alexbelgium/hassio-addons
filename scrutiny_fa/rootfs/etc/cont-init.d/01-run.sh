07b67711e9969c702608a707f4642ed6abc202a8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-06 21:22:44 +0100

    Adapt services

--- scrutiny_fa/rootfs/etc/cont-init.d/01-run.sh
+++ scrutiny_fa/rootfs/etc/cont-init.d/01-run.sh
@@ -71,5 +71,7 @@ if [[ "$(bashio::config "Mode")" == Collector ]]; then
   bashio::log.warning "Collector only mode. WebUI and Influxdb will be disabled"
   rm -r /etc/services.d/influxdb
   rm -r /etc/services.d/scrutiny
+  sed -i "/wait/d" /etc/services.d/collector-once/run
+  sed -i "/scrutiny api not ready/d" /etc/services.d/collector-once/run
 fi
 
