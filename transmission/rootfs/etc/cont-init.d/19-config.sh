d369900e8b20b5f684ba374b889d7b1dedda1136
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-08 23:43:06 +0100

    Create 19-config.sh

--- transmission/rootfs/etc/cont-init.d/19-config.sh
+++ transmission/rootfs/etc/cont-init.d/19-config.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-#rm -rf /etc/cont-init.d/20-config || true
-
 declare CONFIG
 #declare incomplete_bool
 declare download_dir
@@ -38,7 +36,7 @@ fi
 
 if bashio::config.has_value 'customUI'; then
     CUSTOMUI=$(bashio::config 'customUI')
-    [ "$CUSTOMUI" != "standard" ] && sed -i "1a export TRANSMISSION_WEB_HOME=\"/$CUSTOMUI/\"" /etc/services.d/transmission/run
+    [ "$CUSTOMUI" != "standard" ] && sed -i "1a export TRANSMISSION_WEB_HOME=\"/$CUSTOMUI/\"" /etc/s6-overlay/s6-rc.d/svc-transmission/run
 
     # Enable transmission-web-control return to default UI
     if [ ! -f "/transmission-web-control/index.original.html" ]; then
