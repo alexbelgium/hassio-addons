7bba805ab510d82afef17e09bcded3c9405f313c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-08 17:01:41 +0100

    Enable config.yaml
    https://github.com/alexbelgium/hassio-addons/issues/528

--- paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
@@ -10,3 +10,8 @@ fi
 
 chmod -R 755 /config/addons_config/$slug
 chown -R paperless:paperless /config/addons_config/$slug
+
+# Enable variables acceptation with the specific entrypoint
+if [ -f /etc/cont-init.d/90-config_yaml.sh ]; then
+    sed -i "/# Export the variable/a sed -i \"1a export \$line\" /sbin/docker-entrypoint.sh 2>/dev/null" /etc/cont-init.d/90-config_yaml.sh
+fi
