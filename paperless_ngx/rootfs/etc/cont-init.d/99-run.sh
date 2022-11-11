e4c5a07f88a1b62168b18d28e80062e4085894bd
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-11 11:28:20 +0100

    Update 99-run.sh

--- paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
@@ -6,19 +6,12 @@
 #################
 exec redis-server & bashio::log.info "Starting redis"
 
-################
-# Starting app #
-################
-/./usr/local/bin/paperless_cmd.sh & bashio::log.info "Starting app"
-
 ###############################
 # Create user if not existing #
 ###############################
 # Origin : https://github.com/linuxserver/docker-paperless-ngx/blob/main/root/etc/cont-init.d/99-migrations
 
-bashio::net.wait_for 8000 localhost 900
-
-bashio::log.info "Creating default user"
+echo "# Creating user admin
 cat << EOF | python3 manage.py shell
 from django.contrib.auth import get_user_model
 
@@ -26,11 +19,11 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 if len(UserModel.objects.all()) == 1:
-    print("Creating new user")
+    print('Creating new user')
     user = UserModel.objects.create_user('admin', password='admin')
     user.is_superuser = True
     user.is_staff = True
     user.save()
-EOF
+EOF" >> /sbin/docker-entrypoint.sh
 
 bashio::log.info "Initial username and password are admin. Please change in the administration panel of the webUI after login."
