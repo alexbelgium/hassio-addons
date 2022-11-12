bf50bd37c48a1b35ba410816b5a2ea08df338ee5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-12 22:35:22 +0100

    Update 20-folders.sh

--- paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
@@ -11,3 +11,7 @@ fi
 chmod -R 755 /config/addons_config/$slug
 chown -R paperless:paperless /config/addons_config/$slug
 
+if [ -f /etc/cont-init.d/90-config_yaml.sh ]; then
+     sed -i "/# Export the variable/a sed -i \"/file_env()/a export \$line\" /sbin/docker-entrypoint.sh" /etc/cont-init.d/90-config_yaml.sh
+     echo "config.yaml enabled"
+fi
