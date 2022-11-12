0af53e132b56319929329888ceef453b15039d80
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-12 20:12:23 +0100

    Update 20-folders.sh

--- paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/20-folders.sh
@@ -13,6 +13,7 @@ chown -R paperless:paperless /config/addons_config/$slug
 
 # Enable variables acceptation with the specific entrypoint
 if [ -f /etc/cont-init.d/90-config_yaml.sh ]; then
-    sed -i "/# Export the variable/a sed -i \"1a export \$line\" /sbin/docker-entrypoint.sh 2>/dev/null" /etc/cont-init.d/90-config_yaml.sh
-    sed -i "/# Export the variable/a sed -i \"1a export \$line\" /usr/local/bin/paperless_cmd.sh 2>/dev/null" /etc/cont-init.d/90-config_yaml.sh
+    sed -i "/# Export the variable/a sed -i \"1a export \$line\" /sbin/docker-entrypoint.sh" /etc/cont-init.d/90-config_yaml.sh
+    sed -i "/# Export the variable/a sed -i \"1a export \$line\" /usr/local/bin/paperless_cmd.sh" /etc/cont-init.d/90-config_yaml.sh
+    echo "config.yaml enabled"
 fi
