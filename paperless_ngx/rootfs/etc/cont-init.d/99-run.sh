f7c641e9f5296d0f350454643b3c7683e0a3b28b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-12 23:25:45 +0100

    Update 99-run.sh

--- paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
@@ -7,5 +7,3 @@
 exec redis-server & bashio::log.info "Starting redis"
 
 bashio::log.info "Initial username and password are admin. Please change in the administration panel of the webUI after login."
-
-/./sbin/docker-entrypoint.sh
