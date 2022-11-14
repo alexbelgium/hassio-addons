3b77543aebfe53ce8be747b33246a04daa238475
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-14 04:33:18 +0100

    Update 99-run.sh

--- paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
+bashio::log.info "Defining variables"
+
+if bashio::config.has_value "PUID"; then export USERMAP_UID=$(bashio::config "PUID"); fi
+if bashio::config.has_value "PGID"; then export USERMAP_GID=$(bashio::config "PGID"); fi
+if bashio::config.has_value "TZ"; then export PAPERLESS_TIME_ZONE=$(bashio::config "TZ"); fi
+if bashio::config.has_value "OCRLANG"; then export PAPERLESS_OCR_LANGUAGES=$(bashio::config "OCRLANG"); fi
+if bashio::config.has_value "PAPERLESS_OCR_MODE"; then export PAPERLESS_OCR_MODE=$(bashio::config "PAPERLESS_OCR_MODE"); fi
+
 #################
 # Staring redis #
 #################
 exec redis-server & bashio::log.info "Starting redis"
 
 bashio::log.info "Initial username and password are admin. Please change in the administration panel of the webUI after login."
+
+/./sbin/docker-entrypoint.sh /usr/local/bin/paperless_cmd.sh
