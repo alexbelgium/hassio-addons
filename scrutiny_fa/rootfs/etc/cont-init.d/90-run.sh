9729ab5f3ff0284cf747e8249a74e6213f10fffb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-18 10:59:40 +0100

    Use collector api

--- scrutiny_fa/rootfs/etc/cont-init.d/90-run.sh
+++ scrutiny_fa/rootfs/etc/cont-init.d/90-run.sh
@@ -17,6 +17,8 @@ if [[ "$(bashio::config "Mode")" == Collector ]]; then
     # Check collector
     if bashio::config.has_value "COLLECTOR_API_ENDPOINT"; then
       echo "export COLLECTOR_API_ENDPOINT=$(bashio::config "COLLECTOR_API_ENDPOINT")" >> /env.sh
+      sed -i "1a export COLLECTOR_API_ENDPOINT=$(bashio::config "COLLECTOR_API_ENDPOINT")" /etc/services.d/collector-once/run
+      bashio::log.info "Using 'COLLECTOR_API_ENDPOINT' $(bashio::config "COLLECTOR_API_ENDPOINT")"
     else
       bashio::exit.nok "Mode is set to 'Collector', but 'COLLECTOR_API_ENDPOINT' is not defined"
     fi
