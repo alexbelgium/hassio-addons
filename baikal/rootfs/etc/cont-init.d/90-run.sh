81648c6da49d9758a3e61cfa72380873e9665540
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-11-18 21:48:06 +0100

    Update 90-run.sh

--- baikal/rootfs/etc/cont-init.d/90-run.sh
+++ baikal/rootfs/etc/cont-init.d/90-run.sh
@@ -3,6 +3,9 @@
 # Copy data
 cp -rnf /var/www/baikal/* /data/
 
+# Fix permissions
+chown -R nginx:nginx /data
+
 # Start app
 /./etc/init.d/php8.1-fpm start && \
 chown -R nginx:nginx /data/Specific && \
