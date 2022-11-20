246aff7df861e421c12e6c8f221bdbfbefd4e4b3
Alexandre <alexandre.pary@gmail.com>
2022-11-20 15:29:18 +0100

    Align with changes by @scavara
    Co-Authored-By: scavara <1292351+scavara@users.noreply.github.com>

--- scrutiny/rootfs/etc/cont-init.d/01-configuration.sh
+++ scrutiny/rootfs/etc/cont-init.d/01-configuration.sh
@@ -56,7 +56,6 @@ case "$FREQUENCY" in
         sed -i "1a export COLLECTOR_CRON_SCHEDULE=\"0 0 * * 0\"" /etc/cont-init.d/50-cron-config
         ;;
 esac
-
 ############################
 # SMARTCTL COMMAND OPTIONS #
 ############################
