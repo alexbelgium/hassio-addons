c132f50b85aeb9a38134044c11d0541eb6bb6e5b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-12-07 22:13:11 +0100

    Correct savepath

--- qbittorrent/rootfs/etc/cont-init.d/91-qbittorrent_configuration.sh
+++ qbittorrent/rootfs/etc/cont-init.d/91-qbittorrent_configuration.sh
@@ -39,12 +39,12 @@ DOWNLOADS=$(bashio::config 'SavePath')
 if bashio::config.has_value 'SavePath'; then
 
     # Replace save path
-    CURRENTSAVEPATH=$(sed -n '/Downloads\\SavePath/p' qBittorrent.conf)
+    CURRENTSAVEPATH=$(sed -n '/Downloads\SavePath/p' qBittorrent.conf)
     sed -i "s|${CURRENTSAVEPATH#*=}|$DOWNLOADS|g" qBittorrent.conf || \
         sed -i "${LINE}a Downloads\SavePath=$DOWNLOADS" qBittorrent.conf
 
     # Replace session save path
-    CURRENTSAVEPATH=$(sed -n '/Session\\DefaultSavePath/p' qBittorrent.conf)
+    CURRENTSAVEPATH=$(sed -n '/Session\DefaultSavePath/p' qBittorrent.conf)
     sed -i "s|${CURRENTSAVEPATH#*=}|$DOWNLOADS|g" qBittorrent.conf || true
 
     # Info
