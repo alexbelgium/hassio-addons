9a63ee90da703be6342fa0029cde82332c7d4f4b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-12-12 13:41:24 +0100

    Convert to lowercase @bruvv
    https://github.com/alexbelgium/hassio-addons/issues/598#issuecomment-1346422331

--- paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
@@ -11,7 +11,8 @@ bashio::log.info "Defining variables"
 if bashio::config.has_value "PUID"; then export USERMAP_UID=$(bashio::config "PUID"); fi
 if bashio::config.has_value "PGID"; then export USERMAP_GID=$(bashio::config "PGID"); fi
 if bashio::config.has_value "TZ"; then export PAPERLESS_TIME_ZONE=$(bashio::config "TZ"); fi
-if bashio::config.has_value "OCRLANG"; then export PAPERLESS_OCR_LANGUAGES=$(bashio::config "OCRLANG"); fi
+PAPERLESS_OCR_LANGUAGES="$(bashio::config "OCRLANG")"
+if bashio::config.has_value "OCRLANG"; then export ${PAPERLESS_OCR_LANGUAGES,,}; fi
 if bashio::config.has_value "PAPERLESS_OCR_MODE"; then export PAPERLESS_OCR_MODE=$(bashio::config "PAPERLESS_OCR_MODE"); fi
 
 export PAPERLESS_ADMIN_PASSWORD="admin"
