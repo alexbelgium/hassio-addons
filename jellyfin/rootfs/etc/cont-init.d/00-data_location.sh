827b601939bc4fd4a631afb9026fc899c11806dd
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-12-17 01:08:06 +0100

    Update 00-data_location.sh

--- jellyfin/rootfs/etc/cont-init.d/00-data_location.sh
+++ jellyfin/rootfs/etc/cont-init.d/00-data_location.sh
@@ -11,7 +11,7 @@ if [[ "$LOCATION" = "null" || -z "$LOCATION" ]]; then LOCATION=/config/addons_co
 
 # Set data location
 bashio::log.info "Setting data location to $LOCATION"
-for file in $(grep -sril "/config" /etc /defaults); do sed -i "s=/config=$LOCATION=g" $file; done
+for file in $(grep -Esril "/config[ '\"/]|/config\$" /etc /defaults); do sed -Ei "s=(/config)+(/| |$|\"|\')=$LOCATION\2=g" $file; done
 
 echo "Creating $LOCATION"
 mkdir -p "$LOCATION"
