f4fa9cdac386f4e68bb1b2ad77a84fcb4ad247fe
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-12-19 21:29:21 +0100

    Update 90-data_location.sh

--- jellyfin/rootfs/etc/cont-init.d/90-data_location.sh
+++ jellyfin/rootfs/etc/cont-init.d/90-data_location.sh
@@ -11,7 +11,7 @@ if [[ "$LOCATION" = "null" || -z "$LOCATION" ]]; then LOCATION=/config/addons_co
 
 # Set data location
 bashio::log.info "Setting data location to $LOCATION"
-# shellscript disable=SC2013
+# shellcheck disable=SC2013
 for file in $(grep -sril "/config" /etc /defaults); do sed -i "s=/config=$LOCATION=g" $file; done
 # Modify new location for database
 sed -i "s|%%LOCATION%%|$LOCATION|g" /etc/cont-init.d/99-database_clean.sh
