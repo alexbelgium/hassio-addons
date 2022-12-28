e01a4a91d62e6d007df596947d82f81f9c10830f
Alexandre <alexandre.pary@gmail.com>
2022-12-28 22:21:42 +0100

    Allow url

--- paperless_ngx/rootfs/etc/cont-init.d/31-nginx.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/31-nginx.sh
@@ -6,6 +6,13 @@ if bashio::config.true 'ssl'; then
     keyfile=$(bashio::config 'keyfile')
     sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
     sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
+
+    if bashio::config.has_value "PAPERLESS_URL"; then
+        bashio::log.warning "Ssl enabled, your site will be available at $(bashio::config "PAPERLESS_URL"):$(bashio::addon.port 8000)"
+    else
+        bashio::log.fatal "PAPERLESS_URL not set, you won't be able to access your site (CSRF error)"
+    fi
+
 else
     sed -i "s|default_server ssl|default_server|g" /etc/nginx/servers/direct.conf
     sed -i "/ssl/d" /etc/nginx/servers/direct.conf
--- paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/99-run.sh
@@ -11,6 +11,7 @@ bashio::log.info "Defining variables"
 if bashio::config.has_value "PUID"; then export USERMAP_UID=$(bashio::config "PUID"); fi
 if bashio::config.has_value "PGID"; then export USERMAP_GID=$(bashio::config "PGID"); fi
 if bashio::config.has_value "TZ"; then export PAPERLESS_TIME_ZONE=$(bashio::config "TZ"); fi
+if bashio::config.has_value "PAPERLESS_URL"; then export PAPERLESS_URL=$(bashio::config "PAPERLESS_URL"); fi
 if bashio::config.has_value "OCRLANG"; then
     PAPERLESS_OCR_LANGUAGES="$(bashio::config "OCRLANG")"
     export PAPERLESS_OCR_LANGUAGES=${PAPERLESS_OCR_LANGUAGES,,}
