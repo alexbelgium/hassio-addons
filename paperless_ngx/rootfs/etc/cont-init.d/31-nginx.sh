08fbda17f312ecaa897656f8bb7c7527200aea28
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2022-12-28 22:46:06 +0100

    Update 31-nginx.sh

--- paperless_ngx/rootfs/etc/cont-init.d/31-nginx.sh
+++ paperless_ngx/rootfs/etc/cont-init.d/31-nginx.sh
@@ -16,7 +16,7 @@ if bashio::config.true 'ssl'; then
 
     # Configure URL
     if bashio::config.has_value "PAPERLESS_URL"; then
-        bashio::log.warning "Ssl enabled, your site will be available at $(bashio::config "PAPERLESS_URL"):$(bashio::addon.port 8443). Don't forget to enable the https alternative port in the addon options."
+        bashio::log.warning "Ssl enabled, your site will be available at $(bashio::config "PAPERLESS_URL"). Don't forget to enable the https alternative port in the addon options."
     else
         bashio::log.fatal "PAPERLESS_URL not set, you won't be able to access your site (CSRF error)"
     fi
