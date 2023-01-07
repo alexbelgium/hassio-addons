5007ae6ced8d445cca8e0463a9866cebb4cbed47
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-01-07 18:56:02 +0100

    Create logs.sh
    https://github.com/alexbelgium/hassio-addons/issues/632

---
+++ teamspeak/rootfs/etc/services.d/logs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+if [ ! -f /touched ]; then
+  until [[ -n $(cat /data/*_1.log 2>/dev/null) ]]; do
+    sleep 1
+  done
+  bashio::log.info "Here are the admin informations:"
+  cat /data/*_1.log
+  touch /touched
+  rm /etc/services.d/logs.sh
+fi
