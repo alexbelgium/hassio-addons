b9ce90ee747bd8f19b946bbf53dece9f0b811f39
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-01-12 10:31:21 +0100

    Immich addition

---
+++ immich/rootfs/etc/cont-init.d/20-folders.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+#################
+# DATA_LOCATION #
+#################
+
+PUID="$(bashio::config 'PUID')"
+PGID="$(bashio::config 'PGID')"
+
+bashio::log.info "Setting data location"
+DATA_LOCATION="$(bashio::config 'data_location')"
+
+echo "... check $DATA_LOCATION folder exists"
+mkdir -p "$DATA_LOCATION"
+
+echo "... setting permissions"
+chown -R "$PUID":"$PGID" "$DATA_LOCATION"
+
+echo "... correcting official script"
+for file in $(grep -sril '/photos' /etc); do sed -i "s|/photos|$DATA_LOCATION|g" "$file"; done
