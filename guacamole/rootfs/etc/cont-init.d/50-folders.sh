e07a2c30dd1d00af054eb2ec1ae20e1ff9b3521e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-01-23 20:39:40 +0100

    Update 50-folders.sh

--- guacamole/rootfs/etc/cont-init.d/50-folders.sh
+++ guacamole/rootfs/etc/cont-init.d/50-folders.sh
@@ -3,6 +3,6 @@
 
 mkdir -p /config/addons_config/guacamole/fonts
 mkdir -p /config/addons_config/guacamole/postgres
-chmod -R 777 /config/addons_config
+chmod -R 777 /config
 chown -R postgres:postgres /config/addons_config/guacamole/postgres
 chmod -R 0700 /config/addons_config/guacamole/postgres
