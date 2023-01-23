e8aed1f621d529c9436dfeffd6301d4aa6982ae7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-01-23 20:39:05 +0100

    Update and rename 30-folders.sh to 50-folders.sh

--- guacamole/rootfs/etc/cont-init.d/50-folders.sh
+++ guacamole/rootfs/etc/cont-init.d/50-folders.sh
@@ -3,5 +3,6 @@
 
 mkdir -p /config/addons_config/guacamole/fonts
 mkdir -p /config/addons_config/guacamole/postgres
-chmod -R 777 /config/addons_config/guacamole
 chmod -R 777 /config/addons_config
+chown -R postgres:postgres /config/addons_config/guacamole/postgres
+chmod -R 0700 /config/addons_config/guacamole/postgres
