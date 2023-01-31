a51db7ffb1c05249cc7bd360347769b7849efe98
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-01-31 07:45:47 +0100

    Clarify config message
    https://github.com/alexbelgium/hassio-addons/issues/655

--- epicgamesfree/rootfs/etc/cont-init.d/99-run.sh
+++ epicgamesfree/rootfs/etc/cont-init.d/99-run.sh
@@ -10,8 +10,11 @@ if [ ! -f "$HOME"/config.json ]; then
     # Copy default config.json
     cp /templates/config.json "$HOME"/config.json
     chmod 777 "$HOME"/config.json
-    bashio::log.warning "A default config.json file was copied in $HOME. Please customize according to https://github.com/claabs/epicgames-freegames-node#json-configuration before restarting the addon"
+    bashio::log.warning "A default config.json file was copied in $HOME. Please customize according to https://github.com/claabs/epicgames-freegames-node#json-configuration and restart the add-on"
+    sleep 5
     bashio::exit.nok
+else
+    bashio::log.warning "The config.json file found in $HOME will be used. Please customize according to https://github.com/claabs/epicgames-freegames-node#json-configuration and restart the add-on"
 fi
 
 # Permissions
