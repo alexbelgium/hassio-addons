b4df6dc60cc78207366decd55b267d0e16a44020
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-02-06 22:40:36 +0100

    Update 90-disable_ingress.sh

--- .templates/90-disable_ingress.sh
+++ .templates/90-disable_ingress.sh
@@ -8,6 +8,7 @@ if bashio::config.true "ingress_disabled"; then
     bashio::log.warning "Ingress is disabled. You'll need to connect using ip:port"
 
     # Adapt ingress.conf
+    sed -i "/root/d" /etc/nginx/servers/ingress.conf
     sed -i "/proxy_pass/i root /etc;" /etc/nginx/servers/ingress.conf
     sed -i "/proxy_pass/i try_files '' /ingress.html =404;" /etc/nginx/servers/ingress.conf
     sed -i "/proxy_pass/d" /etc/nginx/servers/ingress.conf
