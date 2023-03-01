a246d6a65cca2ea67cb061dd7e62e8c92ea183cd
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-03-01 21:19:52 +0100

    update s6

--- portainer/rootfs/etc/cont-init.d/portainer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# shellcheck shell=bash
-# ==============================================================================
-# Home Assistant Community Add-on: Portainer
-# Runs some initializations for Portainer
-# ==============================================================================
-bashio::require.unprotected
