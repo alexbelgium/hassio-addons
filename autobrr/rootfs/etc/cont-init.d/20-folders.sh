fde295b84bb322c2cc9b12dbffd1affac00f20ec
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-03-17 09:34:39 +0100

    Initial build https://github.com/alexbelgium/hassio-addons/issues/581

---
+++ autobrr/rootfs/etc/cont-init.d/20-folders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d /data/autobrr ]; then
+    echo "Creating /data/autobrr"
+    mkdir -p /data/autobrr
+    chown -R "$(bashio::config "PUID"):$(bashio::config "PGID")" /data/autobrr
+fi
+
+if [ ! -d /config/addons_config/autobrr ]; then
+    echo "Creating /config/addons_config/autobrr"
+    mkdir -p /config/addons_config/autobrr
+    chown -R "$(bashio::config "PUID"):$(bashio::config "PGID")" /config/addons_config/autobrr
+fi
