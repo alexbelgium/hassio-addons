8f6d4059a9347080156d331270a820ef27ae4876
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-03-17 11:01:26 +0100

    Update 91-run.sh

--- autobrr/rootfs/etc/cont-init.d/91-run.sh
+++ autobrr/rootfs/etc/cont-init.d/91-run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-nginx & bashio::log.info "Starting nginx"
-
-
+bashio::log.info "Starting nginx"
+nginx & true
 
 bashio::log.info "Starting app"
 exec /usr/local/bin/autobrr --config /config/addons_config/autobrr
