83647d1b1b6af4d0f4b57d6eccb18284eb0fe8c2
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-03-17 19:55:07 +0100

    Update 91-run.sh

--- autobrr/rootfs/etc/cont-init.d/91-run.sh
+++ autobrr/rootfs/etc/cont-init.d/91-run.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-bashio::log.info "Starting nginx"
-nginx & true
+###############
+# Start nginx #
+###############
+# Set variables
+slug=autobrr
+port=7474
+CONFIG_LOCATION=/config/addons_config/"$slug"/config.toml
+
+# Set UrlBase
+if ! grep -q "<UrlBase>/hassioautobrr</UrlBase>" "$CONFIG_LOCATION" && ! bashio::config.true "ingress_disabled"; then
+  bashio::log.warning "BaseUrl not set properly, restarting"
+  sed -i "/baseUrl/d" /config/addons_config/autobrr/config.toml
+  sed -i "/# Base url/a baseUrl = \"/hassioautobrr/\"" /config/addons_config/autobrr/config.toml
+  bashio::addon.restart
+fi
+
+bashio::log.info "Starting NGinx..."
+exec nginx
+
+#############
+# Start app #
+#############
 
 bashio::log.info "Starting app"
 exec /usr/local/bin/autobrr --config /config/addons_config/autobrr
