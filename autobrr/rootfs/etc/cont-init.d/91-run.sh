50c437d8a32daf2586fcd1eeca59861437459edc
Alexandre <alexandre.pary@gmail.com>
2023-03-17 20:26:18 +0100

    update

--- autobrr/rootfs/etc/cont-init.d/91-run.sh
+++ autobrr/rootfs/etc/cont-init.d/91-run.sh
@@ -6,15 +6,15 @@
 ###############
 
 # Set UrlBase
-if ! grep -q "hassioautobrr" /config/addons_config/autobrr/config.toml; then
-  bashio::log.warning "BaseUrl not set properly, restarting"
-  sed -i "/baseUrl/d" /config/addons_config/autobrr/config.toml
-  sed -i "/# Base url/a baseUrl = \"\/hassioautobrr\/\"" /config/addons_config/autobrr/config.toml
-  bashio::addon.restart
-fi
+#if ! grep -q "hassioautobrr" /config/addons_config/autobrr/config.toml; then
+#  bashio::log.warning "BaseUrl not set properly, restarting"
+#  sed -i "/baseUrl/d" /config/addons_config/autobrr/config.toml
+#  sed -i "/# Base url/a baseUrl = \"\/hassioautobrr\/\"" /config/addons_config/autobrr/config.toml
+#  bashio::addon.restart
+#fi
 
-bashio::log.info "Starting NGinx..."
-nginx & true
+#bashio::log.info "Starting NGinx..."
+#nginx & true
 
 #############
 # Start app #
