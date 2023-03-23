632c9786633cc1ed9ec5590bd001b39325587d5a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-03-23 16:39:33 +0100

    Update 02-init_steps.sh

--- nextcloud/rootfs/etc/cont-init.d/02-init_steps.sh
+++ nextcloud/rootfs/etc/cont-init.d/02-init_steps.sh
@@ -47,7 +47,7 @@ if [[ "$(occ --version)" == *"Composer autoloader not found"* ]]; then
         bashio::log.fatal "Version installed is : $CURRENTVERSION and version bundled is : $ADDONVERSION, need to redownload files"
         bashio::log.fatal "... download nextcloud version"
         rm /app/nextcloud.tar.bz2
-        curl -o /app/nextcloud.tar.bz2 -L https://download.nextcloud.com/server/releases/nextcloud-${CURRENTVERSION}.tar.bz2 || \
+        curl -o /app/nextcloud.tar.bz2 -L https://download.nextcloud.com/server/releases/nextcloud-${CURRENTVERSION}.tar.bz2 --progress-bar || \
         (bashio::log.fatal "Your version doesn't exist... Please restore backup or fully uninstall addon" && exit 1)
     fi
 
