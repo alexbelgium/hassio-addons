0f9fa681bbf1430eaa99bac78442e862a4d9f189
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-03-24 10:55:21 +0100

    Update 01-folders.sh

--- nextcloud/rootfs/etc/cont-init.d/01-folders.sh
+++ nextcloud/rootfs/etc/cont-init.d/01-folders.sh
@@ -5,10 +5,12 @@ PUID=$(bashio::config "PUID")
 PGID=$(bashio::config "PGID")
 datadirectory=$(bashio::config 'data_directory')
 
-bashio::log.info "Checking permissions"
+echo "Checking permissions"
 mkdir -p /data/config
 mkdir -p "$datadirectory"
 chmod 755 -R "$datadirectory"
 chmod 755 -R /data/config
 chown -R "$PUID:$PGID" "$datadirectory"
 chown -R "$PUID:$PGID" "/data/config"
+echo "...done"
+echo " "
