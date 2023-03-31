41efaf0b6a9a0a6304b643f3ec510848b646c0c1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-03-31 08:56:57 +0200

    Update 01-folders.sh

--- nextcloud/rootfs/etc/cont-init.d/01-folders.sh
+++ nextcloud/rootfs/etc/cont-init.d/01-folders.sh
@@ -4,20 +4,23 @@
 PUID=$(bashio::config "PUID")
 PGID=$(bashio::config "PGID")
 
-# Check current version
-if [ -f /data/config/www/nextcloud/config/config.php ]; then
-    datadirectory="$(sed -n "s|.*datadirectory' => '*\(.*[^ ]\) *',.*|\1|p" /data/config/www/nextcloud/config/config.php)"
-else
-    datadirectory=/share/nextcloud
-fi
-
 echo "Updating permissions..."
 echo "... Config directory : /data"
 mkdir -p /data/config
 chmod 755 -R /data/config
 chown -R "$PUID:$PGID" "/data/config"
 
+# Check current version
+if [ -f /data/config/www/nextcloud/config/config.php ]; then
+    datadirectory="$(sed -n "s|.*datadirectory' => '*\(.*[^ ]\) *',.*|\1|p" /data/config/www/nextcloud/config/config.php)"
 echo "... Data directory detected : $datadirectory"
+
+else
+    datadirectory=/share/nextcloud
+fi
+
+
+
 mkdir -p "$datadirectory"
 chmod 755 -R "$datadirectory"
 chown -R "$PUID:$PGID" "$datadirectory"
