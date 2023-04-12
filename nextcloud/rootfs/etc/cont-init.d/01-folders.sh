f650ecbd8b1de15a68bef0ab036f45d97fca6e60
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-04-12 04:50:08 +0200

    Update 01-folders.sh

--- nextcloud/rootfs/etc/cont-init.d/01-folders.sh
+++ nextcloud/rootfs/etc/cont-init.d/01-folders.sh
@@ -42,7 +42,7 @@ for files in /defaults/config.php /data/config/www/nextcloud/config/config.php; 
     if [ -f "$files" ]; then
         sed -i "/check_data_directory_permissions/d" "$files"
         sed -i "/datadirectory/a 'check_data_directory_permissions' => false," "$files"
-        sudo -u abc php /data/config/www/nextcloud/occ config:system:set check_data_directory_permissions --value=false --type=bool 
+        sudo -u abc php /data/config/www/nextcloud/occ config:system:set check_data_directory_permissions --value=false --type=bool || true
     fi
 done
 
