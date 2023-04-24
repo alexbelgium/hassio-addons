dc3c92bb66f3bc20c7835a37cdeb9c29e63b4d1e
Alexandre <alexandre.pary@gmail.com>
2023-04-24 11:06:54 +0200

    lint

--- photoprism/rootfs/etc/cont-init.d/99-run.sh
+++ photoprism/rootfs/etc/cont-init.d/99-run.sh
@@ -127,6 +127,7 @@ fi
 bashio::log.info "Please wait 1 or 2 minutes to allow the server to load"
 bashio::log.info 'Default username : admin, default password: "please_change_password"'
 
+# shellcheck ignore=SC1091
 . /scripts/entrypoint.sh photoprism start & bashio::log.info "Starting, please wait for next green text..."
 
 bashio::net.wait_for 2341 localhost 900
