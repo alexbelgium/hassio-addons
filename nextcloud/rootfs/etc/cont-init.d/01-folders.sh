8f20b5ac91930c80e6de96224d8475a1d28af56d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-04-30 20:32:12 +0200

    Modify subdirectories of data folder
    https://github.com/alexbelgium/hassio-addons/issues/818

--- nextcloud/rootfs/etc/cont-init.d/01-folders.sh
+++ nextcloud/rootfs/etc/cont-init.d/01-folders.sh
@@ -31,8 +31,8 @@ if [[ "$datadirectory" == *"/mnt/"* ]] && [ ! -f "$datadirectory"/index.html ]; 
 fi
 
 mkdir -p "$datadirectory"
-chmod 755 -R "$datadirectory"
-chown -R "$PUID:$PGID" "$datadirectory"
+chmod 755 -R "$datadirectory"/*
+chown -R "$PUID:$PGID" "$datadirectory"/*
 
 ######################
 # Modify config.json #
