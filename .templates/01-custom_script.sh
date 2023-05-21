266cb925400bd0a8756d6dc30ab5790fb70da107
Alexandre <alexandre.pary@gmail.com>
2023-05-21 11:02:35 +0200

    update

--- .templates/01-custom_script.sh
+++ .templates/01-custom_script.sh
@@ -16,7 +16,7 @@ if [ -f /config/addons_autoscripts/"${slug}".sh ]; then
     bashio::log.info "... script found, executing"
     # Convert scripts to linux
     dos2unix /config/addons_autoscripts/"${slug}".sh || true
-    chmod +x /config/addons_autoscripts/"${slug}".sh
+    chmod +x /config/addons_autoscripts/"${slug}".sh || true
     /./config/addons_autoscripts/"${slug}".sh
 else
     bashio::log.info "... no script found"
