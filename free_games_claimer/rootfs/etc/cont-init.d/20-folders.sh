3b6eb569a45318cdd2933fcf9c176d8bd9dce1e4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-05-22 16:04:50 +0200

    first test version https://github.com/alexbelgium/hassio-addons/issues/747

---
+++ free_games_claimer/rootfs/etc/cont-init.d/20-folders.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+# Define home
+# Creating config location
+echo "Creating config location ..."
+HOME="$(bashio::config "CONFIG_LOCATION")"
+HOME="$(dirname "$HOME")"
+mkdir -p $HOME
+chmod -R 777 $HOME
+
+# Copy files to data
+echo "Copying files if needed..."
+cp -rnf /fsg/* /data/
---
+++ free_games_claimer/rootfs/etc/cont-init.d/99-run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bashio
+# shellcheck shell=bash
+
+##############
+# Initialize #
+##############
+
+HOME="$(bashio::config "CONFIG_LOCATION")"
+HOME="$(dirname "$HOME")"
+if [ ! -f "$HOME"/config.yaml ]; then
+    # Copy default config.json
+    cp /templates/config.yaml "$HOME"/config.yaml
+    chmod 777 "$HOME"/config.json
+    bashio::log.warning "A default config.yaml file was copied in $HOME. Please customize according to https://github.com/vogler/free-games-claimer/tree/main#configuration--options and restart the add-on"
+    sleep 5
+    bashio::exit.nok
+else
+    bashio::log.warning "The config.yaml file found in $HOME will be used. Please customize according to https://github.com/vogler/free-games-claimer/tree/main#configuration--options and restart the add-on"
+fi
+
+# Permissions
+chmod -R 777 "$HOME"
+
+##############
+# Launch App #
+##############
+
+cd /data || true
+
+echo " "
+bashio::log.info "Starting the app"
+echo " "
