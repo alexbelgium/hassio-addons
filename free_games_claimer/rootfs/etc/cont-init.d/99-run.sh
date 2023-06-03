dfec9aa281a8c8c182ccd6b35c2e1b443d366cb8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-06-03 20:22:32 +0200

    Update 99-run.sh

--- free_games_claimer/rootfs/etc/cont-init.d/99-run.sh
+++ free_games_claimer/rootfs/etc/cont-init.d/99-run.sh
@@ -55,4 +55,6 @@ echo " "
 
 # Add docker-entrypoint command
 # shellcheck disable=SC2086
-docker-entrypoint.sh $CMD_ARGUMENTS
+docker-entrypoint.sh $CMD_ARGUMENTS || true
+
+bashio::log.info "All actions concluded"
