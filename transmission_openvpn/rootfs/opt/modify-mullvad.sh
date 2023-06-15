3961a53edb63bc474bc594ccfa641cde547be4fa
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-06-15 15:27:51 +0200

    Allow mulvad-based custom configs
    https://github.com/alexbelgium/hassio-addons/issues/874

--- transmission_openvpn/rootfs/opt/modify-mullvad.sh
+++ transmission_openvpn/rootfs/opt/modify-mullvad.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if bashio::config.has_value "OPENVPN_CUSTOM_PROVIDER"; then
+    OVPNLOCATION="$(bashio::config "OPENVPN_CUSTOM_PROVIDER_OVPN_LOCATION")"
+    OPENVPN_PROVIDER="${OVPNLOCATION##*/}"
+    OPENVPN_PROVIDER="${OPENVPN_PROVIDER%.*}"
+    OPENVPN_PROVIDER="${OPENVPN_PROVIDER,,}"
+else
+    OPENVPN_PROVIDER="mullvad"
+fi
+
 echo "Removing IPv6 from mullvad"
 
-echo "pull-filter ignore \"route-ipv6\"" | tee -a "/etc/openvpn/mullvad/"*.ovpn
-echo "pull-filter ignore \"ifconfig-ipv6\"" | tee -a "/etc/openvpn/mullvad/"*.ovpn
-echo "pull-filter ignore \"dhcp-option DNS6\"" | tee -a "/etc/openvpn/mullvad/"*.ovpn
+echo "pull-filter ignore \"route-ipv6\"" | tee -a "/etc/openvpn/$OPENVPN_PROVIDER/"*.ovpn
+echo "pull-filter ignore \"ifconfig-ipv6\"" | tee -a "/etc/openvpn/$OPENVPN_PROVIDER/"*.ovpn
+echo "pull-filter ignore \"dhcp-option DNS6\"" | tee -a "/etc/openvpn/$OPENVPN_PROVIDER/"*.ovpn
