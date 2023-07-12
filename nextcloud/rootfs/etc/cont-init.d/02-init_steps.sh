3616edf266b381177037cb8ec031840b149c017a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-07-12 13:25:57 +0200

    Update 02-init_steps.sh

--- nextcloud/rootfs/etc/cont-init.d/02-init_steps.sh
+++ nextcloud/rootfs/etc/cont-init.d/02-init_steps.sh
@@ -34,7 +34,7 @@ if [ -f /notinstalled ]; then
 fi
 
 # Specify launcher
-LAUNCHER="sudo -u abc php /data/config/www/nextcloud/occ"
+LAUNCHER="sudo -u abc php /usr/bin/occ"
 
 # Check current version
 if [ -f /data/config/www/nextcloud/version.php ]; then
