300340fd10b99a221a842b8e1666be181acfb45b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-07-24 17:12:27 +0200

    update

--- webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
+++ webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
+# shellcheck disable=SC2046
 
 #################
 # NGINX SETTING #
@@ -10,4 +11,4 @@ sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/sites-available/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/sites-available/ingress.conf
 
 # Implement SUBFOLDER value
-sed -i "1a SUBFOLDER=$(bashio::addon.ingress_url)" $(find /etc/s6-overlay/s6-rc.d -name “run” -type f)
+sed -i "1a SUBFOLDER=$(bashio::addon.ingress_url)" $(find /etc/s6-overlay/s6-rc.d -name "run" -type f)
