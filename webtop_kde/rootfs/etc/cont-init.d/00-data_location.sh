9ff6eb36f0907e788084619204383366d26e174f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-07-24 07:57:27 +0200

    Update 00-data_location.sh

--- webtop_kde/rootfs/etc/cont-init.d/00-data_location.sh
+++ webtop_kde/rootfs/etc/cont-init.d/00-data_location.sh
@@ -33,8 +33,8 @@ fi
 # Set data location
 bashio::log.info "Setting data location to $LOCATION"
 
-sed -i "1a export HOME=$LOCATION" /etc/s6-overlay/s6-rc.d/svc-web/run || true
-sed -i "1a export FM_HOME=$LOCATION" /etc/s6-overlay/s6-rc.d/svc-web/run || true
+sed -i "1a export HOME=$LOCATION" $(find /etc/s6-overlay/s6-rc.d -name “run” -type f) || true
+sed -i "1a export FM_HOME=$LOCATION” $(find /etc/s6-overlay/s6-rc.d -name “run” -type f) || true
 sed -i "s|/share/webtop_kde|$LOCATION|g" $(find /defaults -type f) || true
 sed -i "s|/share/webtop_kde|$LOCATION|g" $(find /etc/cont-init.d -type f) || true
 sed -i "s|/share/webtop_kde|$LOCATION|g" $(find /etc/services.d -type f) || true
