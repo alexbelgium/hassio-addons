1fa8039b654c7eeee925bbc86eb570801bc8791e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-07-28 15:45:27 +0200

    Update 90-ingress.sh

--- webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
+++ webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
@@ -5,10 +5,14 @@
 #################
 # NGINX SETTING #
 #################
+
+cp /etc/nginx/http.d/default.conf /etc/nginx/http.d/ingress.conf
+
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/sites-available/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/sites-available/ingress.conf
+sed -i "s/3001/0001/g" /etc/nginx/http.d/ingress.conf
+sed -i "s/3000/${ingress_port}/g" /etc/nginx/http.d/ingress.conf
+#sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/sites-available/ingress.conf
 
 # Implement SUBFOLDER value
 sed -i "1a SUBFOLDER=$(bashio::addon.ingress_url)" $(find /etc/s6-overlay/s6-rc.d -name "run" -type f)
