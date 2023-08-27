2f4b05fdefb41d469c52abf1eaf2d832710e666b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-08-27 21:15:37 +0200

    Update 99-run.sh

--- addons_updater/rootfs/etc/cont-init.d/99-run.sh
+++ addons_updater/rootfs/etc/cont-init.d/99-run.sh
@@ -252,9 +252,9 @@ for f in */; do
     fi
 done || true # Continue even if issue
 
-# Clean dry ru
+# Clean dry run
 if bashio::config.true "dry_run"; then
-  rm -r /data
+  rm -r /data/*
 fi
 
 bashio::log.info "Addons update completed"
