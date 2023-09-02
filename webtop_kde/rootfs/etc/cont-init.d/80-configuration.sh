2460da4495d2eab9bc464280f8edff93c96e9a54
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-09-02 16:49:04 +0200

    Update 80-configuration.sh

--- webtop_kde/rootfs/etc/cont-init.d/80-configuration.sh
+++ webtop_kde/rootfs/etc/cont-init.d/80-configuration.sh
@@ -2,27 +2,7 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2015
 
-# Add Edge repositories
-if bashio::config.true 'edge_repositories'; then
-    bashio::log.info "Changing app repositories to edge"
-    { echo "https://dl-cdn.alpinelinux.org/alpine/edge/community";
-        echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing";
-    echo "https://dl-cdn.alpinelinux.org/alpine/edge/main"; } > /etc/apk/repositories
-fi
-
-# Install rpi video drivers
-if bashio::config.true 'rpi_video_drivers'; then
-    bashio::log.info "Installing Rpi graphic drivers"
-    apk add --no-cache mesa-dri-vc4 mesa-dri-swrast mesa-gbm xf86-video-fbdev >/dev/null && bashio::log.green "... done" ||
-    bashio::log.red "... not successful. Are you on a rpi?"
-fi
-
-# Fix mate software center
-if [ -f /usr/lib/dbus-1.0/dbus-daemon-launch-helper ]; then
-    echo "Allow software center"
-    chmod u+s /usr/lib/dbus-1.0/dbus-daemon-launch-helper
-    service dbus restart
-fi
+exit 0
 
 # Install specific apps
 if bashio::config.has_value 'additional_apps'; then
