1c2bac16284a6e924d5d0e222db0d8d49776d468
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-09-02 17:15:41 +0200

    Update 90-ingress.sh

--- webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
+++ webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
@@ -9,17 +9,15 @@ CPORT="${CUSTOM_PORT:-3000}"
 CHPORT="${CUSTOM_HTTPS_PORT:-3001}"
 CUSER="${CUSTOM_USER:-abc}"
 
-# modify nginx config
+# Add ingress parameters
 cp /defaults/default.conf ${NGINX_CONFIG}
 sed -i '/listen \[::\]/d' ${NGINX_CONFIG}
-
-# Add ingress parameters
 sed -i '/server {/a include /etc/nginx/includes/server_params.conf;' ${NGINX_CONFIG}
-sed -i '/server {/a include /etc/nginx/includes/proxy_params.conf;;' ${NGINX_CONFIG}
-sed -i 's|3000 default_server;|%%port%% default_server;|g' ${NGINX_CONFIG}
+sed -i '/server {/a include /etc/nginx/includes/proxy_params.conf;' ${NGINX_CONFIG}
+sed -i "s|3000|$(bashio::addon.ingress_port)|g" ${NGINX_CONFIG}
 
 # Implement SUBFOLDER value
-sed -i "1a SUBFOLDER=$(bashio::addon.ingress_url)" /etc/s6-overlay/s6-rc.d/svc-autostart/run || true
+#sed -i "1a SUBFOLDER=$(bashio::addon.ingress_url)" /etc/s6-overlay/s6-rc.d/svc-autostart/run || true
 
 # Enable ingress
 cp /etc/nginx/sites-available/ingress.conf /etc/nginx/sites-enabled
