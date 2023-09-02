2144c06fcf4cdb730fc1ba50676e56aece6327c1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-09-02 17:06:12 +0200

    nginx update

--- webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
+++ webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
@@ -2,41 +2,24 @@
 # shellcheck shell=bash
 
 # nginx Path
-NGINX_CONFIG=/etc/nginx/http.d/ingress.conf
+NGINX_CONFIG=/etc/nginx/sites-available/ingress.conf
 
 # user passed env vars
 CPORT="${CUSTOM_PORT:-3000}"
 CHPORT="${CUSTOM_HTTPS_PORT:-3001}"
 CUSER="${CUSTOM_USER:-abc}"
 
-# create self signed cert
-if [ ! -f "/config/ssl/cert.pem" ]; then
-  mkdir -p /config/ssl
-  openssl req -new -x509 \
-    -days 3650 -nodes \
-    -out /config/ssl/cert.pem \
-    -keyout /config/ssl/cert.key \
-    -subj "/C=US/ST=CA/L=Carlsbad/O=Linuxserver.io/OU=LSIO Server/CN=*"
-  chmod 600 /config/ssl/cert.key
-  chown -R abc:abc /config/ssl
-fi
-
 # modify nginx config
 cp /defaults/default.conf ${NGINX_CONFIG}
-sed -i "s/3000/$CPORT/g" ${NGINX_CONFIG}
-sed -i "s/3001/$CHPORT/g" ${NGINX_CONFIG}
-if [ ! -z ${DISABLE_IPV6+x} ]; then
-  sed -i '/listen \[::\]/d' ${NGINX_CONFIG}
-fi
-if [ ! -z ${PASSWORD+x} ]; then
-  printf "${CUSER}:$(openssl passwd -apr1 ${PASSWORD})\n" > /etc/nginx/.htpasswd
-  sed -i 's/#//g' ${NGINX_CONFIG}
-fi
+sed -i '/listen \[::\]/d' ${NGINX_CONFIG}
 
 # Add ingress parameters
 sed -i '/server {/a include /etc/nginx/includes/server_params.conf;' ${NGINX_CONFIG}
 sed -i '/server {/a include /etc/nginx/includes/proxy_params.conf;;' ${NGINX_CONFIG}
-sed -i 's|3000 default_server;|%%interface%%:%%port%% default_server;|g' ${NGINX_CONFIG}
+sed -i 's|3000 default_server;|%%port%% default_server;|g' ${NGINX_CONFIG}
 
 # Implement SUBFOLDER value
 sed -i "1a SUBFOLDER=$(bashio::addon.ingress_url)" /etc/s6-overlay/s6-rc.d/svc-autostart/run || true
+
+# Enable ingress
+cp /etc/nginx/sites-available/ingress.conf /etc/nginx/sites-enabled
