a6d01ec31b28608cdcd59077b87c8205bd09a634
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-09-02 18:23:11 +0200

    Update 90-ingress.sh

--- webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
+++ webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
@@ -12,7 +12,7 @@ CUSER="${CUSTOM_USER:-abc}"
 # Copy template
 cp /defaults/default.conf ${NGINX_CONFIG}
 # Remove ssl part
-awk -v n=4 '/server/{n--}; n > 0' ${NGINX_CONFIG}
+awk -v n=4 '/server/{n--}; n > 0' ${NGINX_CONFIG} > ${NGINX_CONFIG}
 # Remove ipv6
 sed -i '/listen \[::\]/d' ${NGINX_CONFIG}
 # Add ingress parameters
