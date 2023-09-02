5ca864c9f996d897a74fba11c292a76f0940657b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-09-02 19:07:08 +0200

    Update 90-ingress.sh

--- webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
+++ webtop_kde/rootfs/etc/cont-init.d/90-ingress.sh
@@ -5,15 +5,17 @@
 NGINX_CONFIG=/etc/nginx/sites-available/ingress.conf
 
 # Copy template
-cp /defaults/default.conf ${NGINX_CONFIG}
+cp /defaults/default.conf "${NGINX_CONFIG}"
 # Remove ssl part
-awk -i inplace -v n=4 '/server/{n--}; n > 0' ${NGINX_CONFIG}
+awk -i inplace -v n=4 '/server/{n--}; n > 0' "${NGINX_CONFIG}" > tmpfile
+mv tmpfile "${NGINX_CONFIG}"
+
 # Remove ipv6
-sed -i '/listen \[::\]/d' ${NGINX_CONFIG}
+sed -i '/listen \[::\]/d' "${NGINX_CONFIG}"
 # Add ingress parameters
-sed -i "s|3000|$(bashio::addon.ingress_port)|g" ${NGINX_CONFIG}
-sed -i '/server {/a include /etc/nginx/includes/server_params.conf;' ${NGINX_CONFIG}
-sed -i '/server {/a include /etc/nginx/includes/proxy_params.conf;' ${NGINX_CONFIG}
+sed -i "s|3000|$(bashio::addon.ingress_port)|g" "${NGINX_CONFIG}"
+sed -i '/server {/a include /etc/nginx/includes/server_params.conf;' "${NGINX_CONFIG}"
+sed -i '/server {/a include /etc/nginx/includes/proxy_params.conf;' "${NGINX_CONFIG}"
 
 # Enable ingress
 cp /etc/nginx/sites-available/ingress.conf /etc/nginx/sites-enabled
