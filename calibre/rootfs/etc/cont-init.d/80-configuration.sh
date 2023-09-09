3eaf00841e78622f090aef71740479035a81b7d5
Alexandre <alexandre.pary@gmail.com>
2023-09-09 13:09:25 +0200

    First build https://github.com/alexbelgium/hassio-addons/issues/972

--- calibre/rootfs/etc/cont-init.d/80-configuration.sh
+++ calibre/rootfs/etc/cont-init.d/80-configuration.sh
@@ -2,38 +2,52 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2015
 
+# Install specific apps
+if bashio::config.has_value 'additional_apps'; then
+    bashio::log.info "Installing additional apps :"
+    # hadolint ignore=SC2005
+    NEWAPPS=$(bashio::config 'additional_apps')
+    for packagestoinstall in ${NEWAPPS//,/ }; do
+        bashio::log.green "... $packagestoinstall"
+        if command -v "apk" &>/dev/null; then
+            apk add --no-cache "$packagestoinstall" &>/dev/null || (bashio::log.fatal "Error : $packagestoinstall not found")
+        elif command -v "apt" &>/dev/null; then
+            apt-get install -yqq --no-install-recommends "$packagestoinstall" &>/dev/null || (bashio::log.fatal "Error : $packagestoinstall not found")
+        elif command -v "pacman" &>/dev/null; then
+            pacman --noconfirm -S "$packagestoinstall" &>/dev/null || (bashio::log.fatal "Error : $packagestoinstall not found")
+        fi
+    done
+fi
+
 # Set TZ
 if bashio::config.has_value 'TZ'; then
     TIMEZONE=$(bashio::config 'TZ')
     bashio::log.info "Setting timezone to $TIMEZONE"
-    ln -snf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime && echo "$TIMEZONE" >/etc/timezone
-fi || true
-
-# Set keyboard
-if bashio::config.has_value 'KEYBOARD'; then
-    KEYBOARD=$(bashio::config 'KEYBOARD')
-    bashio::log.info "Setting keyboard to $KEYBOARD"
-    sed -i "1a export KEYBOARD=$KEYBOARD" /etc/s6-overlay/s6-rc.d/svc-web/run
-    if [ -d /var/run/s6/container_environment ]; then printf "%s" "$KEYBOARD" > /var/run/s6/container_environment/KEYBOARD; fi
-    printf "%s" "KEYBOARD=\"$KEYBOARD\"" >> ~/.bashrc
-fi || true
+    ln -snf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+    echo "$TIMEZONE" >/etc/timezone
+fi || (bashio::log.fatal "Error : $TIMEZONE not found. Here is a list of valid timezones : https://manpages.ubuntu.com/manpages/focal/man3/DateTime::TimeZone::Catalog.3pm.html")
 
 # Set cli args
 if bashio::config.has_value 'CLI_ARGS'; then
     bashio::log.info "Setting password to the value defined in options"
     CLI_ARGS=$(bashio::config 'CLI_ARGS')
     bashio::log.info "Setting arguments to $CLI_ARGS"
-    sed -i "1a export CLI_ARGS=$CLI_ARGS" /etc/s6-overlay/s6-rc.d/svc-web/run
-    if [ -d /var/run/s6/container_environment ]; then printf "%s" "$CLI_ARGS" > /var/run/s6/container_environment/CLI_ARGS; fi
-    printf "%s" "CLI_ARGS=\"$CLI_ARGS\"" >> ~/.bashrc
+    if [ -d /var/run/s6/container_environment ]; then printf "%s" "$CLI_ARGS" >/var/run/s6/container_environment/CLI_ARGS; fi
+    printf "%s" "CLI_ARGS=\"$CLI_ARGS\"" >>~/.bashrc
+fi || true
+
+# Set keyboard
+if bashio::config.has_value 'KEYBOARD'; then
+    KEYBOARD=$(bashio::config 'KEYBOARD')
+    bashio::log.info "Setting keyboard to $KEYBOARD"
+    if [ -d /var/run/s6/container_environment ]; then printf "%s" "$KEYBOARD" >/var/run/s6/container_environment/KEYBOARD; fi
+    printf "%s" "KEYBOARD=\"$KEYBOARD\"" >>~/.bashrc
 fi || true
 
 # Set password
 if bashio::config.has_value 'PASSWORD'; then
     bashio::log.info "Setting password to the value defined in options"
     PASSWORD=$(bashio::config 'PASSWORD')
-    bashio::log.info "Setting password to $PASSWORD"
-    sed -i "1a export PASSWORD=$PASSWORD" /etc/s6-overlay/s6-rc.d/svc-web/run
-    if [ -d /var/run/s6/container_environment ]; then printf "%s" "$PASSWORD" > /var/run/s6/container_environment/PASSWORD; fi
-    printf "%s" "PASSWORD=\"$PASSWORD\"" >> ~/.bashrc
+    passwd -d abc
+    echo -e "$PASSWORD\n$PASSWORD" | passwd abc
 fi || true
