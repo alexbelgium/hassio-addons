847c151c534afc41c0af16c77e8a7bfea9b7ecea
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-09-27 22:15:45 +0200

    Update 00-batch_updater.sh

--- .templates/00-batch_updater.sh
+++ .templates/00-batch_updater.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/with-contenv bashio
-# shellcheck shell=bash
-# ==============================================================================
-# Ensures all scripts are up-to-date
-# ==============================================================================
 
-if [ -d /etc/cont-init.d ]; then
-  # Check if downloads possible
-  wget -NS https://example.com/ &>/dev/null || exit 0
-
-  # Check scripts
-  cd /etc/cont-init.d || true
-  for scripts in *; do
-    # If newer file, download to directory
-    wget -NS --content-disposition "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/$scripts" &>/dev/null || true
-  done
-fi
