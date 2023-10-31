b7413f101d9d603bbd33d4c998953f5895d8d9b5
Alexandre <alexandre.pary@gmail.com>
2023-10-31 11:28:33 +0100

    Adapt for new version

--- readarr/rootfs/etc/cont-init.d/32-nginx_ingress.sh
+++ readarr/rootfs/etc/cont-init.d/32-nginx_ingress.sh
@@ -14,3 +14,23 @@ ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
+
+##################
+# CONFIG SETTING #
+##################
+
+# Values
+slug=readarr
+CONFIG_LOCATION=/config/addons_config/"$slug"/config.xml
+
+if [ -f "$CONFIG_LOCATION" ]; then
+  # Disable local auth
+  sed -i "/AuthenticationType/d" "$CONFIG_LOCATION"
+  sed -i "2a <AuthenticationType>DisabledForLocalAddresses</AuthenticationType>" "$CONFIG_LOCATION"
+
+  # Set UrlBase
+  if ! bashio::config.true "ingress_disabled"; then
+    sed -i "/UrlBase/d" "$CONFIG_LOCATION"
+    sed -i "/<Config>/a <UrlBase>$slug<\/UrlBase>" "$CONFIG_LOCATION"
+  fi
+fi
