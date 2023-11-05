0f46baffc9eccaf068c65f0d880da8322b6db1a8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-05 13:22:11 +0100

    Update 20-folders.sh

--- jellyfin/rootfs/etc/cont-init.d/20-folders.sh
+++ jellyfin/rootfs/etc/cont-init.d/20-folders.sh
@@ -38,7 +38,7 @@ for folders in "cache" "log" "data/metadata"; do
     if [ -d "$LOCATION/$folders" ]; then
         echo "Previous $folders found, migrating to /data"
         cp -r "$LOCATION/$folders/*" /data/"$folders"/
-        rm -r "${$LOCATION:?}/$folders"
+        rm -r "${LOCATION:?}/$folders"
     fi
     mkdir -p "$LOCATION/$folders"
     ln -s /data/"$folders" "$LOCATION/$folders"
