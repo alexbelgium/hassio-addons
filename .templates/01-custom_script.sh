2874e3624992e3e50d85b0c346d1aae86eeb7d79
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-18 11:52:35 +0100

    Adapt for new config logic

--- .templates/01-custom_script.sh
+++ .templates/01-custom_script.sh
@@ -7,20 +7,27 @@ bashio::log.green "Execute /config/addons_autoscripts/${slug}.sh if existing"
 bashio::log.green "---------------------------------------------------------"
 echo "Wiki here : github.com/alexbelgium/hassio-addons/wiki/Add-ons-feature-:-customisation"
 
-mkdir -p /config/addons_autoscripts
-
-# Migrate scripts
-if [ -f /config/"${slug}".sh ]; then
-    mv -f /config/"${slug}".sh /config/addons_autoscripts/"${slug}".sh
+# Check type of config folder
+if [ ! -f /config/configuration.* ]; then
+    # Migrate previous script
+    if [ -f /config/addons_autoscripts/"${slug}".sh ]; then
+        mv -f /config/addons_autoscripts/"${slug}".sh /config/"${slug}".sh
+    fi
+    # New config location
+    CONFIGLOCATION="/config"
+else
+    # Legacy config location
+    CONFIGLOCATION="/config/addons_autoscripts"
+    mkdir -p /config/addons_autoscripts
 fi
 
 # Execute scripts
-if [ -f /config/addons_autoscripts/"${slug}".sh ]; then
+if [ -f "$CONFIGLOCATION/${slug}".sh ]; then
     bashio::log.green "... script found, executing"
     # Convert scripts to linux
-    dos2unix /config/addons_autoscripts/"${slug}".sh || true
-    chmod +x /config/addons_autoscripts/"${slug}".sh || true
-    /./config/addons_autoscripts/"${slug}".sh
+    dos2unix "$CONFIGLOCATION/${slug}".sh || true
+    chmod +x "$CONFIGLOCATION/${slug}".sh || true
+    /."$CONFIGLOCATION/${slug}".sh
 else
     bashio::log.green "... no script found, exiting"
 fi
