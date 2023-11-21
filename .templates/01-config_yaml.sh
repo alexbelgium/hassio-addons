710e0407644db66d3c2243e1051b50500574c000
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-21 20:32:30 +0100

    Allow Blank spaces

--- .templates/01-config_yaml.sh
+++ .templates/01-config_yaml.sh
@@ -171,8 +171,8 @@ while IFS= read -r line; do
         mkdir -p /etc
         echo "$KEYS=$VALUE" >> /etc/environment
         # Export to scripts
-        if cat /etc/services.d/*/*run* &>/dev/null; then sed -i "1a export $line" /etc/services.d/*/*run* 2>/dev/null; fi
-        if cat /etc/cont-init.d/*run* &>/dev/null; then sed -i "1a export $line" /etc/cont-init.d/*run* 2>/dev/null; fi
+        if cat /etc/services.d/*/*run* &>/dev/null; then sed -i "1a export $KEYS=\"$VALUE\"" /etc/services.d/*/*run* 2>/dev/null; fi
+        if cat /etc/cont-init.d/*run* &>/dev/null; then sed -i "1a export $KEYS=\"$VALUE\"" /etc/cont-init.d/*run* 2>/dev/null; fi
         # For s6
         if [ -d /var/run/s6/container_environment ]; then printf "%s" "${VALUE}" > /var/run/s6/container_environment/"${KEYS}"; fi
         echo "export ${KEYS}=\"${VALUE}\"" >> ~/.bashrc
