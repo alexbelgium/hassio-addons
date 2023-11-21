6c6eb698a4889c6c494ee8d4fc603c10b7eea295
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-21 17:35:31 +0100

    Update 99-run.sh

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -8,8 +8,8 @@ if [ -d /home/node/.signalk ]; then
   rm -r /home/node/.signalk
 fi
 ln -sf /config /home/node/.signalk
-chown -r node:node /config
-chown -r node:node /home/node/.signalk
+chown -R node:node /config
+chown -R node:node /home/node/.signalk
 
 bashio::log.info "Starting application"
 sudo -u node -s /bin/sh -c "/home/node/signalk/startup.sh"
