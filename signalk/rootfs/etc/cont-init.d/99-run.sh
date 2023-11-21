16da973fcedf257ee5872437dffcdace3ce3ed01
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-21 16:42:16 +0100

    Update 99-run.sh

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -3,5 +3,13 @@
 set -e
 # hadolint ignore=SC2155
 
+# Set configuration directory
+if [ -d /home/node/.signalk ]; then
+  rm -r /home/node/.signalk
+fi
+ln -s /config /home/node/.signalk
+chown node:node /config
+chown node:node /home/node/.signalk
+
 bashio::log.info "Starting application"
 sudo -u node -s /bin/sh -c "/home/node/signalk/startup.sh"
