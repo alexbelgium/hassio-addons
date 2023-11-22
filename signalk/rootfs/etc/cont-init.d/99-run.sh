5e2e5ef27ea056e3b3dbb0305ef427a779d0d4a4
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-22 19:39:17 +0100

    Update 99-run.sh
    https://github.com/alexbelgium/hassio-addons/issues/1075

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -12,4 +12,6 @@ chown -R node:node /config
 chown -R node:node /home/node/.signalk
 
 bashio::log.info "Starting application"
-sudo -u node -s /bin/sh -c "/home/node/signalk/startup.sh"
+
+
+sudo -u node -s /bin/sh -c "/home/node/signalk/startup.sh" || bashio::addon.restart
