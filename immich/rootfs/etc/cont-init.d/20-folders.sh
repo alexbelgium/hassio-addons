9235163666189cc305b974655d3fa47794d627b9
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-23 16:24:05 +0100

    Update 20-folders.sh

--- immich/rootfs/etc/cont-init.d/20-folders.sh
+++ immich/rootfs/etc/cont-init.d/20-folders.sh
@@ -22,7 +22,6 @@ mkdir -p "$DATA_LOCATION"
 
 echo "... setting permissions"
 chown -R "$PUID":"$PGID" "$DATA_LOCATION"
-chmod -R 777 /config/redis
 
 echo "... correcting official script"
 # shellcheck disable=SC2013
