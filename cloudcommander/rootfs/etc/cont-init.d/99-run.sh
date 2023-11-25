625116ef82ed3f7be7ffacd7b7d63b72a3c2ab11
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-25 22:46:20 +0100

    Update 99-run.sh

--- cloudcommander/rootfs/etc/cont-init.d/99-run.sh
+++ cloudcommander/rootfs/etc/cont-init.d/99-run.sh
@@ -2,6 +2,39 @@
 # shellcheck shell=bash
 set -e
 
+####################
+# Migrate database #
+####################
+
+if [ -f /homeassistant/addons_config/cloudcommander ]; then
+    echo "Moving database to new location /config"
+    cp -rnf /homeassistant/addons_config/cloudcommander/* /config/
+    rm -r /homeassistant/addons_config/cloudcommander
+fi
+
+######################
+# Link addon folders #
+######################
+
+# Clean symlinks
+find /config -maxdepth 1 -type l -delete
+find /homeassistant/addons_config -maxdepth 1 -type l -delete
+
+# Remove erroneous folders
+if [ -d /homeassistant ]; then
+  if [ -d /config/addons_config ]; then
+    rm -r /config/addons_config
+  fi
+  if [ -d /config/addons_autoscripts ]; then
+    rm -r /config/addons_autoscripts
+  fi
+fi
+
+# Create symlinks
+ln -s /homeassistant/addons_config /config
+ln -s /homeassistant/addons_autoscripts /config
+find /addon_configs/ -maxdepth 1 -mindepth 1 -type d -not -name "*filebrowser*" -exec ln -s {} /config/addons_config/ \;
+
 #################
 # NGINX SETTING #
 #################
