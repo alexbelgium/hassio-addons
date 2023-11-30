71db28559820db74a73ac4c1dbae4bb1db918ebd
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-11-30 15:28:42 +0100

    Update 00-run.sh

--- omada/rootfs/etc/cont-init.d/00-run.sh
+++ omada/rootfs/etc/cont-init.d/00-run.sh
@@ -11,23 +11,47 @@ if [ -d /ssl ]; then
     chown -R 508:508 /cert
 fi
 
-# Migrate previous files
-mkdir -p /config/data
-mv /data/* /config/data
+# Create directory
+if [ ! -f "$CONFIGSOURCE" ]; then
+    echo "Creating directory"
+    mkdir -p "$CONFIGSOURCE"
+fi
 
-# Copy app files
-cp -rnf /opt/tplink/EAPController/data/* /config/data/ 2>/dev/null || true
-rm -r /opt/tplink/EAPController/data 2>/dev/null || true
-rm -r /opt/tplink/EAPController/logs 2>/dev/null || true
-mv /opt/tplink/EAPController/* /config
+# Migrate data
+if [ -d /data/db ]; then
+    mv /data/* "$CONFIGSOURCE"/
+    mv "$CONFIGSOURCE"/options.json /data/
+fi
+
+# Ensure structure is correct
+cp -rnf /opt/tplink/EAPController/data/* "$CONFIGSOURCE/"
+
+echo "Creating symlink"
+# Clean existing folder
+rm -r /opt/tplink/EAPController/data/*
+
+# Create folders if not existing
+for item in db html keystore html logs properties properties.default pdf db portal; do
+    mkdir -p "$CONFIGSOURCE/$item"
+fi
+
+# Create symlinks for all files in /data
+# shellcheck disable=SC2086
+for item in "$CONFIGSOURCE"/*; do
+    # Extract the base name of the item
+    base_name=$(basename "$item")
+    # Create a symbolic link in the initial directory
+    ln -s "$item" "/opt/tplink/EAPController/data/$base_name"
+done
 
 # Make sure permissions are right
 echo "Updating permissions"
-chmod -R 777 /config
-chown -R "508:508" /config
+chmod -R 777 "$CONFIGSOURCE"
+chown -R "508:508" "$CONFIGSOURCE"
+chown -R "508:508" "/opt/tplink/EAPController/data"
 
 echo ""
 echo ""
-echo "Recommendation : please backup your database and migrate to this addon https://github.com/jkunczik/home-assistant-omada"
+echo "Recommendation : please backup your database and migrated to this addon https://github.com/jkunczik/home-assistant-omada"
 echo ""
 echo ""
