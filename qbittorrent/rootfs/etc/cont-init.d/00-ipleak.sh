35dfd960d0618671fc5406f6077760954903c349
Adrian Fedoreanu <phedoreanu@users.noreply.github.com>
2023-12-10 12:10:01 +0100

    curl https url

--- qbittorrent/rootfs/etc/cont-init.d/00-ipleak.sh
+++ qbittorrent/rootfs/etc/cont-init.d/00-ipleak.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-curl --max-time 10 --connect-timeout 5 -s ipecho.net/plain > /currentip
+curl --max-time 10 --connect-timeout 5 -s https://ipecho.net/plain > /currentip
