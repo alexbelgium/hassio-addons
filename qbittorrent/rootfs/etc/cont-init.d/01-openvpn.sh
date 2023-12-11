0772771ada4c9ea9d1d345d76fa5518e3c88b356
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-12-11 16:31:14 +0100

    Update 01-openvpn.sh

--- qbittorrent/rootfs/etc/cont-init.d/01-openvpn.sh
+++ qbittorrent/rootfs/etc/cont-init.d/01-openvpn.sh
@@ -6,7 +6,7 @@ mkdir -p /config/openvpn
 QBT_CONFIG_FILE="/config/qBittorrent/config/qBittorrent.conf"
 
 # Correct openvpn files
-if [ bashio::config "VPN_ENABLED" == "yes" ]; then
+if [[ bashio::config "VPN_ENABLED" == "yes" ]] && [[ bashio::config "VPN_TYPE" == "openvpn" ]]; then
 
     # If there is a config file, add nopull
     if [ -n "$(ls -A /config/openvpn 2>/dev/null)" ]; then
@@ -19,6 +19,10 @@ if [ bashio::config "VPN_ENABLED" == "yes" ]; then
         fi 
         done
     fi
+
+    ip route add 10.0.0.0/8 via 172.30.32.1
+    ip route add 192.168.0.0/16 via 172.30.32.1
+    ip route add 172.16.0.0/12 via 172.30.32.1
     
 else
 
