ba088564087374ac71013f2e96e5ba1be93b3b74
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-12-14 08:35:46 +0100

    Update and rename 02-test_vpn.sh to 02-wpn.sh

---
+++ qbittorrent/rootfs/etc/cont-init.d/02-wpn.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+
+# wait until vpn is up
+sleep 5
+
+# test if vpn is up
+counter=0
+until [ "$counter" -gt 10 ]
+do
+  echo "... waiting until vpn is up"
+  ping -c 1 "1.1.1.1" &> /dev/null && exit 0 || true
+  ((counter++))
+  sleep 5
+done
+
+bashio::log.fatal "vpn failed to get up for 60 seconds"
