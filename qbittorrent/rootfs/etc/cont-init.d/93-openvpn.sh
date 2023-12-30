66ade624484af4e223a3566f89614ee6d38d4406
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-12-30 16:44:06 +0100

    Correct random config
    https://github.com/alexbelgium/hassio-addons/issues/1150

--- qbittorrent/rootfs/etc/cont-init.d/93-openvpn.sh
+++ qbittorrent/rootfs/etc/cont-init.d/93-openvpn.sh
@@ -94,7 +94,7 @@ if bashio::config.true 'openvpn_enabled'; then
         fi
 
     # If openvpn_config not set, but folder is not empty
-    elif [ ! "$(ls -A /config/openvpn/*.ovpn 2>/dev/null)" ]; then
+    elif [ ! "$(ls -A /config/openvpn/*.ovpn)" ]; then
         # Look for openvpn files
         # Wildcard search for openvpn config files and store results in array
         mapfile -t VPN_CONFIGS < <( find /config/openvpn -maxdepth 1 -name "*.ovpn" -print )
