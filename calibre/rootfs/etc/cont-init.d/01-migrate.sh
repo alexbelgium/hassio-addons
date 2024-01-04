304076cd56659da3c3f137075b064576e67f8016
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-04 15:52:24 +0100

    Create 01-migrate.sh

---
+++ calibre/rootfs/etc/cont-init.d/01-migrate.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+####################
+# Migrate database #
+####################
+
+if [ -f /homeassistant/addons_config/calibre ]; then
+    echo "Moving database to new location /config"
+    cp -rnf /homeassistant/addons_config/calibre/* /config/
+    rm -r /homeassistant/addons_config/calibre
+fi
