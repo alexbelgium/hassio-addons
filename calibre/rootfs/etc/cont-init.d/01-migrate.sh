427b59be166959237e905bfa4e91ae66d230227c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-05 21:04:50 +0100

    Update 01-migrate.sh

--- calibre/rootfs/etc/cont-init.d/01-migrate.sh
+++ calibre/rootfs/etc/cont-init.d/01-migrate.sh
@@ -13,6 +13,7 @@ if [ -d /homeassistant/addons_config/calibre ]; then
 fi
 
 # Legacy path
+if [ -d /config/addons_config/calibre ]; then rm -r /config/addons_config/calibre; fi
 mkdir -p /config/addons_config/calibre
 ln -s "/config/Calibre Library" "/config/addons_config/calibre/"
 chmod -R 777 /config/addons_config
