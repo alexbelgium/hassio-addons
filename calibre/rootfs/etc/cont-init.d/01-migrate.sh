8ac93c347e1f7dee9879cfbc6f8288f4cb906dbc
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-05 14:38:09 +0100

    Update 01-migrate.sh

--- calibre/rootfs/etc/cont-init.d/01-migrate.sh
+++ calibre/rootfs/etc/cont-init.d/01-migrate.sh
@@ -8,6 +8,6 @@ set -e
 
 if [ -d /homeassistant/addons_config/calibre ]; then
     echo "Moving database to new location /config"
-    cp -rnf /homeassistant/addons_config/calibre/* /config/
+    cp -rf /homeassistant/addons_config/calibre/* /config/
     rm -r /homeassistant/addons_config/calibre
 fi
