0e2c870d6a784141ecbe07985477c468b24ee7d6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-20 07:51:27 +0100

    Update 00-deprecated.sh

--- .templates/00-deprecated.sh
+++ .templates/00-deprecated.sh
@@ -12,7 +12,7 @@ bashio::log.yellow "# ADDON deprecated #"
 bashio::log.yellow "####################"
 echo ""
 bashio::log.yellow "This addon is now supported in the official HA community repository. You should migrate your data as soon as possible! This addon will not be supported and updates might stop in the future."
-bashio::log.yellow "You'll likely get better support as the official community is supported by the HA devs !"
+bashio::log.yellow "You'll likely get better support as the official community is supported by the HA devs ! If some features from the official add-on are missing you should raise a request on the ha community add-ons repo"
 bashio::log.yellow "Thanks for all users over the years !"
 echo ""
 
