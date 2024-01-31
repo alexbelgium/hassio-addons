ae694863a623d2596cad191c7ffa4a00324276c9
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-01-31 04:51:17 +0100

    Update 20-folders.sh

--- postgres/rootfs/etc/cont-init.d/20-folders.sh
+++ postgres/rootfs/etc/cont-init.d/20-folders.sh
@@ -2,3 +2,13 @@
 # shellcheck shell=bash
 set -e
 
+if [ -d /data/database ]; then
+    bashio::log.warning "Database migrated to /config"
+    mv /data/database /config
+fi
+
+if [ -f /homeassistant/addons_config/postgres/config.yaml ]; then
+    bashio::log.warning "Config migrated to /config"
+    mv /homeassistant/addons_config/postgres/*  /config/
+    rm -r /homeassistant/addons_config/postgres
+fi
