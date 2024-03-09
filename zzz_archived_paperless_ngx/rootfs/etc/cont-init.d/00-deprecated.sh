9512522efd0caa011b7e2c8e8fb496812b350730
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-03-09 19:22:04 +0100

    Update 00-deprecated.sh

--- zzz_archived_paperless_ngx/rootfs/etc/cont-init.d/00-deprecated.sh
+++ zzz_archived_paperless_ngx/rootfs/etc/cont-init.d/00-deprecated.sh
@@ -16,4 +16,16 @@ bashio::log.yellow "It is recommended to transfer to his version that will be mo
 bashio::log.yellow "Thanks for all users over the years !"
 echo ""
 
+echo "Migration (thanks @eikeja) : 
+- Install the new addon
+- Make a backup of the old Paperless directory. In my case "/addon_configs/db21ed7f_paperless_ng"
+- View the folder structure of the new instance, assign files from the old instance to the folders of the new instance.
+
+Start new Paperless - all data is there!
+
+Folder assignment:
+/addon_configs/db21ed7f_paperless_ng/data → /addon_configs/ca5234a0_paperless-ngx/data
+
+/addon_configs/db21ed7f_paperless_ng/media → /share/paperless"
+
 sleep 5
