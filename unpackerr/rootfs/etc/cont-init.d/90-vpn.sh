cf9e606ee24e17b5bbfcecbb9a2fad86371a0938
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-03-13 11:44:41 +0100

    Avoid exec error

--- unpackerr/rootfs/etc/cont-init.d/90-vpn.sh
+++ unpackerr/rootfs/etc/cont-init.d/90-vpn.sh
@@ -5,23 +5,23 @@ set +e
 VPN_PROVIDER="${VPN_PROVIDER:-null}"
 case "$VPN_PROVIDER" in
   "generic")
-    echo "" > etc/s6*/s6*/service-pia/run
-    echo "" > /etc/s6*/s6*/service-proton/run
+    sed -i "1a sleep infinity" etc/s6*/s6*/service-pia/run
+    sed -i "1a sleep infinity" etc/s6*/s6*/service-proton/run
   ;;
 
   "pia")
-    echo "" > /etc/s6*/s6*/service-privoxy/run
-    echo "" > /etc/s6*/s6*/service-proton/run
+    sed -i "1a sleep infinity" /etc/s6*/s6*/service-privoxy/run
+    sed -i "1a sleep infinity" /etc/s6*/s6*/service-proton/run
   ;;
 
   "proton")
-    echo "" > /etc/s6*/s6*/service-privoxy/run
-    echo "" > /etc/s6*/s6*/service-pia/run
+    sed -i "1a sleep infinity" /etc/s6*/s6*/service-privoxy/run
+    sed -i "1a sleep infinity" /etc/s6*/s6*/service-pia/run
   ;;
 
   **)
-    echo "" > /etc/s6*/s6*/service-privoxy/run
-    echo "" > /etc/s6*/s6*/service-proton/run
-    echo "" > /etc/s6*/s6*/service-pia/run
+    sed -i "1a sleep infinity" /etc/s6*/s6*/service-privoxy/run
+    sed -i "1a sleep infinity" /etc/s6*/s6*/service-proton/run
+    sed -i "1a sleep infinity" /etc/s6*/s6*/service-pia/run
   ;;
 esac
