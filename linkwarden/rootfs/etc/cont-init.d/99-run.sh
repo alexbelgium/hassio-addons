6c9b77a756f58ef572737846f3efccd59fcf21cf
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-03-14 16:12:33 +0100

    Update 99-run.sh

--- linkwarden/rootfs/etc/cont-init.d/99-run.sh
+++ linkwarden/rootfs/etc/cont-init.d/99-run.sh
@@ -1,11 +1,17 @@
 #!/command/with-contenv bashio
 # shellcheck shell=bash
 set -e
-
+  
 bashio::log.info "Creating folders"
 mkdir -p /config/library
 mkdir -p /config/database
 touch /config/database/linkwarden.sqlite
+
+bashio::log.info "Applying migrations"
+rm -r /data_linkwarden/prisma/migrations
+cd /data_linkwarden
+npx prisma db push
+npx prisma generate
 
 bashio::log.info "Starting app..."
 yarn prisma migrate deploy
