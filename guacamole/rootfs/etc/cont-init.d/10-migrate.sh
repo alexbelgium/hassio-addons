4f60bccedf32efcca7dd8c689444a9e5fcd96203
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-03-17 20:30:42 +0100

    Create 10-migrate.sh

---
+++ guacamole/rootfs/etc/cont-init.d/10-migrate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+####################
+# Migrate database #
+####################
+
+slug=guacamole
+
+if [ -d /homeassistant/addons_config/"$slug" ] && [ ! -d /config/postgres ]; then
+    echo "Moving database to new location /config"
+    cp -rf /homeassistant/addons_config/"$slug"/* /config/
+fi
