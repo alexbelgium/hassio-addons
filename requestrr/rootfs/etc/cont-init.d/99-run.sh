ac58650e6e7ca42bd32df153370d6feba8b732a6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-03-27 15:41:36 +0100

    Update 99-run.sh

--- requestrr/rootfs/etc/cont-init.d/99-run.sh
+++ requestrr/rootfs/etc/cont-init.d/99-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Start app
+bashio::log.info "Please run a first time the addon, then customize using my Filebrowser addon the config file created in /addon_configs/db21ed7f_requestrr"
 /root/Requestrr.WebApi -c /config
