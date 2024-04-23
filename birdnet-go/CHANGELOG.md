db89d1a8f4ee598eef4b4ec7eff99a2585a6fcb1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-04-23 12:13:54 +0200

    allow Mariadb

--- birdnet-go/CHANGELOG.md
+++ birdnet-go/CHANGELOG.md
@@ -1,3 +1,5 @@
+- Feat : provide mariadb information in the startup log to allow its usage
+
 ## 0.5.1-3 (23-04-2024)
 - Feat : Allow mounting of SMB and local drives to store the audio clips on an external drive
 
---
+++ birdnet-go/rootfs/etc/cont-init.d/33-mariadb.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+# Gives mariadb information
+
+if bashio::services.available 'mysql'; then
+    bashio::log.green "---"
+    bashio::log.yellow "MariaDB addon is active on your system! If you want to use it instead of sqlite, here are the informations to encode :"
+    bashio::log.blue "Database user : $(bashio::services "mysql" "username")"    bashio::log.blue "Database user : $(bashio::services "mysql" "username")"
+    bashio::log.blue "Database password : $(bashio::services "mysql" "password")"
+    bashio::log.blue "Database name : nextcloud"
+    bashio::log.blue "Host-name : $(bashio::services "mysql" "host"):$(bashio::services "mysql" "port")"
+    bashio::log.green "---"
+fi
