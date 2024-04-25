5a92a7d8d9cf0991317eacea4c446f51e24e557d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-04-25 20:34:45 +0200

    Create 01-migrate.sh
    https://github.com/alexbelgium/hassio-addons/issues/1354

---
+++ baikal/rootfs/etc/cont-init.d/01-migrate.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+####################
+# Migrate database #
+####################
+
+if [ -d /homeassistant/addons_config/baikal ]; then
+    echo "Moving database to new location /config, you won't be able to restore previous versions of the addon"
+    cp -rf /homeassistant/addons_config/baikal/* /config/
+    rm -r /homeassistant/addons_config/baikal
+fi
