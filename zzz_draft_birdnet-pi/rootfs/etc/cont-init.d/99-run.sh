55a9a22c1f1d021b233c097e4dc5ce61fa63a2ff
Alexandre <alexandre.pary@gmail.com>
2024-04-26 21:26:47 +0000

    Clean birdnet-pi

--- zzz_draft_birdnet-pi/rootfs/etc/cont-init.d/99-run.sh
+++ zzz_draft_birdnet-pi/rootfs/etc/cont-init.d/99-run.sh
@@ -7,18 +7,29 @@ set -e
 #################
 
 # Starting dbus
-echo "Starting service: dbus"
+bashio::log.info "Starting system services..."
+echo "... dbus"
 service dbus start
 
-bashio::log.info "ALSA_CARD option is set to $(bashio::config "ALSA_CARD"). If the microphone doesn't work, please adapt it"
-echo " "
+# Starting php service
+echo "... php"
+until [[ -e /var/run/dbus/system_bus_socket ]]; do
+    sleep 1s
+done
+systemctl start php*service
 
-########################
-# CONFIGURE birdnet-pi #
-########################
+# Starting avahi service
+echo "... avahi"
+systemctl start avahi*service
 
-bashio::log.info "Starting app..."
-
+# Restarting all services
+bashio::log.info "Ensuring birdnet.conf is in /config ; please customize as needed"
 if [ ! -f /config/birdnet.conf ]; then
   cp /etc/birdnet/birdnet.conf /config
 fi
+
+bashio::log.info "Starting BirdNET-Pi services"
+chmod +x $HOME/BirdNET-Pi/scripts/restart_services
+/.$HOME/BirdNET-Pi/scripts/restart_services &>/proc/1/fd/1
+
+bashio::log.info "App is accessible from webui"
