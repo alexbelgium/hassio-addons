047d38fde8167343b155befa336e7a51a8194a86
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-04-27 21:44:38 +0200

    Update 99-run.sh

--- birdnet-pi/rootfs/etc/cont-init.d/99-run.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/99-run.sh
@@ -2,23 +2,24 @@
 # shellcheck shell=bash
 set -e
 
-#################
-# INITALISATION #
-#################
+###############
+# SET /CONFIG #
+###############
 
-# Correcting systemctl
-curl -f -L -s -S https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -o /bin/systemctl
-chmod a+x /bin/systemctl
+bashio::log.info "Ensuring permissions are good"
 
-# Starting dbus
-bashio::log.info "Starting system services..."
-echo "... dbus"
-service dbus start
+# Define structure
+echo "... making sure structure is correct"
+touch /config/apprise.txt
+mkdir -p /config/By_Date
+mkdir -p /config/Charts
+
+# Permissions
+echo "... set permissions to user pi"
+chown -R 1000:1000 /config /etc/birdnet
 
 # Symlink files
 bashio::log.info "Ensuring files are in /config ; please customize as needed"
-touch /config/apprise.txt
-chown -R 1000:1000 /config /etc/birdnet
 for files in "$HOME/BirdNET-Pi/birdnet.conf" "$HOME/BirdNET-Pi/scripts/birds.db" "$HOME/BirdNET-Pi/apprise.txt"; do
     filename="${files##*/}"
     echo "... setting $filename"
@@ -28,6 +29,26 @@ for files in "$HOME/BirdNET-Pi/birdnet.conf" "$HOME/BirdNET-Pi/scripts/birds.db"
     sudo -u pi ln -fs /config/"$filename" "$files"
     sudo -u pi ln -fs /config/"$filename" /etc/birdnet/"$filename"
 done
+
+# Symlink folders
+for files in "$HOME/BirdSongs/Extracted/By_Date" "$HOME/BirdSongs/Extracted/Charts"; do
+    filename="${files##*/}"
+    echo "... setting folder BirdSongs/Extracted/$filename"
+    sudo -u pi ln -fs /config/"$filename" "$files"
+done
+
+##############
+# SET SYSTEM #
+##############
+
+# Correcting systemctl
+curl -f -L -s -S https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -o /bin/systemctl
+chmod a+x /bin/systemctl
+
+# Starting dbus
+bashio::log.info "Starting system services..."
+echo "... dbus"
+service dbus start
 
 # Starting services
 bashio::log.info "Starting BirdNET-Pi services"
