3d6d04b80268337764bb8229250cdc6065104295
Alexandre <alexandre.pary@gmail.com>
2024-04-28 13:55:37 +0200

    ingress preparation

---
+++ birdnet-pi/rootfs/etc/cont-init.d/32-nginx_ingress.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+#################
+# NGINX SETTING #
+#################
+
+declare ingress_interface
+declare ingress_port
+declare ingress_entry
+
+# Variables
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+ingress_entry=$(bashio::addon.ingress_entry)
+
+# Quits if ingress not active
+if [ -z "$ingress_entry" ]; then exit 0; fi
+
+echo "Adapting for ingress"
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
