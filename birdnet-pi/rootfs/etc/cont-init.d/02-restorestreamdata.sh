a5e4045c1d7d7923a04656005c46b5a998cc4ce5
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-01 17:37:33 +0200

    Avoid hardcoding /tmp https://github.com/alexbelgium/hassio-addons/issues/1369#issuecomment-2088623801

--- birdnet-pi/rootfs/etc/cont-init.d/02-restorestreamdata.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/02-restorestreamdata.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-# Check if there are files in /tmp/StreamData and move them to /data/StreamData
-if [ -d /data/StreamData ] && [ "$(ls -A /tmp/StreamData)" ]; then
+# Check if there are files in "$HOME"/BirdSongs/StreamData and move them to /data/StreamData
+if [ -d /data/StreamData ] && [ "$(ls -A "$HOME"/BirdSongs/StreamData)" ]; then
 
-    bashio::log.warning "Container was stopped while files were still being analysed, restoring them"  
+    bashio::log.warning "Container was stopped while files were still being analysed, restoring them"
 
     # Copy files
     if [ "$(ls -A /data/StreamData)" ]; then
-        mv -v /data/StreamData/* /tmp/StreamData/
+        mv -v /data/StreamData/* "$HOME"/BirdSongs/StreamData/
     fi
     echo "... done"
     echo ""
 
     # Setting permissions
-    chown -R pi:pi /tmp
+    chown -R pi:pi "$HOME"/BirdSongs
 
     # Cleaning folder
     rm -r /data/StreamData
