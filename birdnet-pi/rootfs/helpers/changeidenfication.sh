7cf0367dd977af137ef9643359f898f2562f4f3c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-03 10:09:00 +0200

    Update changeidenfication.sh

--- birdnet-pi/rootfs/helpers/changeidenfication.sh
+++ birdnet-pi/rootfs/helpers/changeidenfication.sh
@@ -12,6 +12,8 @@ source /etc/birdnet/birdnet.conf
 
 #OLDNAME="Mésange_charbonnière-78-2024-05-02-birdnet-RTSP_1-18:14:08.mp3"
 #NEWNAME="Lapinus atricapilla_Lapinu à tête noire"
+OLDNAME=""
+NEWNAME=""
 OLDNAME="$1"
 NEWNAME="$2"
 LABELS_FILE="$HOME/BirdNET-Pi/model/labels.txt"
