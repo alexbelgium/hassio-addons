a909104305e8db9ff3512971c6d15a54908371e3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-13 15:35:51 +0200

    Update 31-ssl.sh

--- birdnet-pi/rootfs/etc/cont-init.d/31-ssl.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/31-ssl.sh
@@ -10,6 +10,6 @@ if bashio::config.true 'ssl'; then
     bashio::config.require.ssl
     certfile=$(bashio::config 'certfile')
     keyfile=$(bashio::config 'keyfile')
-    sed -i "/root/a tls /ssl/${certfile}/ssl/${keyfile}" /etc/caddy/Caddyfile
+    sed -i "/root/a tls /ssl/${certfile} /ssl/${keyfile}" /etc/caddy/Caddyfile
     sed -i "s|http://|https://|g" /etc/caddy/Caddyfile
 fi
