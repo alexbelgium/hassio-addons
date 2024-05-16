bce985e6e6ce6a33df36cd3b03543b046b3028f8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-16 21:15:02 +0200

    Remove new features

--- birdnet-pi/rootfs/etc/cont-init.d/71-modifications.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/71-modifications.sh
@@ -18,33 +18,6 @@ echo "... removing Ram drive from webui as it is handled from HA"
 sed -i '/Ram drive/{n;s/center"/center" style="display: none;"/;}' "$HOME"/BirdNET-Pi/scripts/service_controls.php
 sed -i '/Ram drive/d' "$HOME"/BirdNET-Pi/scripts/service_controls.php
 
-# Set the online birds info system
-if [[ "$(bashio::config "BIRDS_ONLINE_INFO")" == *"ebird"* ]]; then
-    echo "... using ebird instead of allaboutbirds"
-    # Set ebird database
-    mv /helpers/ebird.txt /home/pi/BirdNET-Pi/model/ebird.txt
-    chown pi:pi /home/pi/BirdNET-Pi/model/ebird.txt
-    # Get language
-    export "$(grep "^DATABASE_LANG" /config/birdnet.conf)"
-    # shellcheck disable=SC2016
-    sed -i '/$sciname =/a \\t$ebirdname = shell_exec("grep \\"$( echo \\"$sciname\\" | sed '\''s/_/ /g'\'')\\" /home/pi/BirdNET-Pi/model/ebird.txt | cut -d'\''_'\'' -f2 | sed '\''s/ /_/g'\''");' "$HOME"/BirdNET-Pi/scripts/todays_detections.php
-    sed -i "s|https://allaboutbirds.org/guide/<?php echo \$comname;?>|https://ebird.org/species/<?php echo \$ebirdname;?>?siteLanguage=${DATABASE_LANG}_${DATABASE_LANG}|g" "$HOME"/BirdNET-Pi/scripts/todays_detections.php
-    # shellcheck disable=SC2016
-    sed -i '/$sciname =/a \\t$ebirdname = shell_exec("grep \\"$( echo \\"$sciname\\" | sed '\''s/_/ /g'\'')\\" /home/pi/BirdNET-Pi/model/ebird.txt | cut -d'\''_'\'' -f2 | sed '\''s/ /_/g'\''");' "$HOME"/BirdNET-Pi/scripts/stats.php
-    sed -i "s|https://allaboutbirds.org/guide/\$comname|https://ebird.org/species/\$ebirdname?siteLanguage=${DATABASE_LANG}_${DATABASE_LANG}|g" "$HOME"/BirdNET-Pi/scripts/stats.php
-else
-    # Correct allaboutbirds for non-english names
-    echo "... using allaboutbirds, with correction for non-english names"
-    # shellcheck disable=SC2016
-    sed -i 's|allaboutbirds.org/guide/<?php echo $comname|allaboutbirds.org/guide/<?php echo $comnameen|g' "$HOME"/BirdNET-Pi/scripts/todays_detections.php
-    # shellcheck disable=SC2016
-    sed -i '/$sciname =/a \\t$comnameen = shell_exec("grep \\"$( echo \\"$sciname\\" | sed '\''s/_/ /g'\'')\\" /home/pi/BirdNET-Pi/model/labels.bak | cut -d'\''_'\'' -f2 | sed '\''s/ /_/g'\''");' "$HOME"/BirdNET-Pi/scripts/todays_detections.php
-    # shellcheck disable=SC2016
-    sed -i '/$sciname =/a \\t$comnameen = shell_exec("grep \\"$( echo \\"$sciname\\" | sed '\''s/_/ /g'\'')\\" /home/pi/BirdNET-Pi/model/labels.bak | cut -d'\''_'\'' -f2 | sed '\''s/ /_/g'\''");' "$HOME"/BirdNET-Pi/scripts/stats.php
-    # shellcheck disable=SC2016
-    sed -i "s|https://allaboutbirds.org/guide/\$comname|https://allaboutbirds.org/guide/\$comnameen|g" "$HOME"/BirdNET-Pi/scripts/stats.php
-fi
-
 # Correct services to start as user pi
 echo "... correct services to start as pi"
 for file in $(find "$HOME"/BirdNET-Pi/templates/birdnet*.service -print0 | xargs -0 basename -a) livestream.service chart_viewer.service chart_viewer.service spectrogram_viewer.service; do
@@ -59,22 +32,6 @@ for file in $(find "$HOME"/BirdNET-Pi/templates/birdnet*.service -print0 | xargs
     sed -i "/Service/a StandardError=append:/proc/1/fd/1" "$HOME/BirdNET-Pi/templates/$file"
     sed -i "/Service/a StandardOutput=append:/proc/1/fd/1" "$HOME/BirdNET-Pi/templates/$file"
 done
-
-# Add birds change option
-if [ ! -f /home/pi/BirdNET-Pi/scripts/birdnet_changeidentification.sh ]; then
-    # Clean previous files
-    rm /home/pi/BirdNET-Pi/scripts/play.php
-    rm /home/pi/BirdNET-Pi/homepage/style.css
-    # Download new files
-    curl -o /home/pi/BirdNET-Pi/homepage/images/bird.svg https://raw.githubusercontent.com/alexbelgium/BirdNET-Pi/patch-1/homepage/images/bird.svg
-    curl -o /home/pi/BirdNET-Pi/scripts/birdnet_changeidentification.sh https://raw.githubusercontent.com/alexbelgium/BirdNET-Pi/patch-1/scripts/birdnet_changeidentification.sh
-    curl -o /home/pi/BirdNET-Pi/scripts/play.php https://raw.githubusercontent.com/alexbelgium/BirdNET-Pi/patch-1/scripts/play.php
-    curl -o /home/pi/BirdNET-Pi/homepage/style.css https://raw.githubusercontent.com/alexbelgium/BirdNET-Pi/patch-1/homepage/style.css
-    # Correct permissions
-    chmod 777 /home/pi/BirdNET-Pi/scripts/birdnet_changeidentification.sh
-    chmod 777 /home/pi/BirdNET-Pi/scripts/play.php
-    chmod 777 /home/pi/BirdNET-Pi/homepage/style.css
-fi
 
 # Avoid preselection in include and exclude lists
 echo "... avoid preselecting options in include and exclude lists"
