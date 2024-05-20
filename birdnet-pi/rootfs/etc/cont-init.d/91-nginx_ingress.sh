861ef2aa104090365dcd084aba2415cac0e8ec6e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-20 19:07:03 +0200

    Update 91-nginx_ingress.sh

--- birdnet-pi/rootfs/etc/cont-init.d/91-nginx_ingress.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/91-nginx_ingress.sh
@@ -25,4 +25,8 @@ sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
 
+echo "... ensuring restricted area access"
+echo "${ingress_entry}" > /ingress_url
+sed -i "/function is_authenticated/a if (strpos(\$_SERVER['HTTP_REFERER'], '/api/hassio_ingress') !== false && strpos(\$_SERVER['HTTP_REFERER'], trim(file_get_contents('/ingress_url'))) !== false) { \$ret = true; return \$ret; }" "$HOME"/BirdNET-Pi/scripts/common.php
+
 echo " "
