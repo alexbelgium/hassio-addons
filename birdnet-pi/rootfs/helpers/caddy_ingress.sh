47fcee7006a6c3e7b477d0560918c0730b3c75c7
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-21 08:27:36 +0200

    Create caddy_ingress.sh

---
+++ birdnet-pi/rootfs/helpers/caddy_ingress.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# shellcheck shell=bash
+
+# Get values
+source /etc/birdnet/birdnet.conf
+
+# Create ingress configuration for Caddyfile
+  cat < EOF >> /etc/caddy/Caddyfile
+http://:8082 {
+  root * ${EXTRACTED}
+  file_server browse
+  handle /By_Date/* {
+    file_server browse
+  }
+  handle /Charts/* {
+    file_server browse
+  }
+  reverse_proxy /stream localhost:8000
+  php_fastcgi unix//run/php/php-fpm.sock
+  reverse_proxy /log* localhost:8080
+  reverse_proxy /stats* localhost:8501
+  reverse_proxy /terminal* localhost:8888
+}
+EOF
