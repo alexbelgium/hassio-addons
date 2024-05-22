2c7a8bacd8cc9447ee1cab2e5b57db465a8b7cf6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-05-22 20:12:24 +0200

    Update 92-nginx_ssl.sh

--- birdnet-pi/rootfs/etc/cont-init.d/92-nginx_ssl.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/92-nginx_ssl.sh
@@ -12,8 +12,8 @@ if bashio::config.true 'ssl'; then
     certfile=$(bashio::config 'certfile')
     keyfile=$(bashio::config 'keyfile')
     sed -i "/root/a tls /ssl/${certfile} /ssl/${keyfile}" /etc/caddy/Caddyfile
-    sed -i "s|http://|https://|g" /etc/caddy/Caddyfile
-    sed -i "s|http://|https://|g" "$HOME"/BirdNET-Pi/update_caddyfile.sh
+    sed -i "s|http://:8081|https://:8081|g" /etc/caddy/Caddyfile
+    sed -i "s|http://:8081|https://:8081|g" "$HOME"/BirdNET-Pi/update_caddyfile.sh
 fi
 
 echo " "
