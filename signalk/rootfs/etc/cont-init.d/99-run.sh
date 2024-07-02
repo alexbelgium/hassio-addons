822f9cb0f9022e05020000b97c4a66df3e7c2d64
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-07-02 19:27:27 +0200

    Define permissions for /dev/ttyUSB
    https://github.com/alexbelgium/hassio-addons/issues/1450#issuecomment-2203881951

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -3,6 +3,9 @@
 set -e
 # hadolint ignore=SC2155
 
+# Variables
+USER=node
+
 # Set configuration directory
 if [ -d /home/node/.signalk ]; then
     rm -r /home/node/.signalk
@@ -11,6 +14,15 @@ ln -sf /config /home/node/.signalk
 chown -R node:node /config
 chown -R node:node /home/node/.signalk
 
+# Define permissions for /dev/ttyUSB
+for device in /dev/ttyUSB /dev/ttyUSB0 /dev/ttyUSB1; do
+    if [ -f "$device" ]; do
+        usermod -a -G "$(stat -c "%G" "$device")" $USER
+        chmod 777 "$device"
+        chown "$USER" "$device"
+    done
+done
+
 bashio::log.info "Starting application"
 
-sudo -u node -s /bin/sh -c "/home/node/signalk/startup.sh"
+sudo -u "$USER" -s /bin/sh -c "/home/node/signalk/startup.sh"
