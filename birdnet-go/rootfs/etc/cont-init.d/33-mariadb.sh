47c8a15d7b7aec7532800febb40150dcbbaeafdd
ghisch <168481965+ghisch@users.noreply.github.com>
2024-07-03 19:43:14 +0200

    fix db name & port

--- birdnet-go/rootfs/etc/cont-init.d/33-mariadb.sh
+++ birdnet-go/rootfs/etc/cont-init.d/33-mariadb.sh
@@ -9,7 +9,8 @@ if bashio::services.available 'mysql'; then
     bashio::log.yellow "MariaDB addon is active on your system! If you want to use it instead of sqlite, here are the informations to encode :"
     bashio::log.blue "Database user : $(bashio::services "mysql" "username")"
     bashio::log.blue "Database password : $(bashio::services "mysql" "password")"
-    bashio::log.blue "Database name : nextcloud"
-    bashio::log.blue "Host-name : $(bashio::services "mysql" "host"):$(bashio::services "mysql" "port")"
+    bashio::log.blue "Database name : birdnet"
+    bashio::log.blue "Host-name : $(bashio::services "mysql" "host")"
+    bashio::log.blue "Port : $(bashio::services "mysql" "port")"
     bashio::log.green "---"
 fi
