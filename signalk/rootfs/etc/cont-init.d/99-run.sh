5985df93e4cd7e74712fe99b9151d3dc52edd3e8
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-07-11 07:48:03 +0200

    Correct permissions for tty
    https://github.com/alexbelgium/hassio-addons/issues/1450#issuecomment-2222072187

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -3,14 +3,24 @@
 set -e
 # hadolint ignore=SC2155
 
+# Variables
+USER=node
+
 # Set configuration directory
-if [ -d /home/node/.signalk ]; then
-    rm -r /home/node/.signalk
+if [ -d "/home/$USER/.signalk" ]; then
+    rm -r "/home/$USER/.signalk"
 fi
-ln -sf /config /home/node/.signalk
-chown -R node:node /config
-chown -R node:node /home/node/.signalk
+ln -sf /config "/home/$USER/.signalk"
+chown -R "$USER:$USER" /config
+chown -R "$USER:$USER" "/home/$USER/.signalk"
+
+# Define permissions for /dev/ttyUSB
+for device in /dev/ttyUSB /dev/ttyUSB0 /dev/ttyUSB1; do
+    if [ -e "$device" ]; then
+        usermod -a -G "$(stat -c "%G" "$device")" $USER
+    fi
+done
 
 bashio::log.info "Starting application"
 
-sudo -u node -s /bin/sh -c "/home/node/signalk/startup.sh"
+sudo -u "$USER" -s /bin/sh -c "/home/node/signalk/startup.sh"
