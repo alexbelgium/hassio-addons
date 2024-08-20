18149e519e1cb40f124cec5c36bbcfe3a224e59f
Jim Geraci <james.geraci@bicworld.com>
2024-08-20 14:10:35 -0400

    Update configuration
    Update configuration to fix a misspelling  in the config folder name.
    Update config.json to increment version
    Update Change log.

--- jellyseerr/rootfs/etc/cont-init.d/90-run.sh
+++ jellyseerr/rootfs/etc/cont-init.d/90-run.sh
@@ -3,6 +3,7 @@
 set -e
 
 # Create files
+OLD_CONFIG_LOCATION="/config/addons_config/jellyseer"
 CONFIG_LOCATION="/config/addons_config/jellyseerr"
 bashio::log.info "Config stored in $CONFIG_LOCATION"
 mkdir -p "$CONFIG_LOCATION"
@@ -10,6 +11,21 @@ cp -rnT /app/config "$CONFIG_LOCATION"/
 rm -r /app/config
 ln -s "$CONFIG_LOCATION" /app/config
 chmod -R 755 "$CONFIG_LOCATION"
+
+#Move files that may be in misspelled directory
+if [ -d "$OLD_CONFIG_LOCATION" ];
+	then
+    #Directory Exists
+	if [ -z "$( ls -A '$OLD_CONFIG_LOCATION' )" ];
+		then
+			#Empty
+		else
+			#Not Empty
+			bashio::log.info "Moving old configuration settings from $OLD_CONFIG_LOCATION to $CONFIG_LOCATION"
+			cp -rnT "$OLD_CONFIG_LOCATION" "$CONFIG_LOCATION"/
+			rm -r "$OLD_CONFIG_LOCATION"
+	  fi
+fi
 
 # Create files
 JELLYFIN_TYPE=$(bashio::config 'TYPE')
