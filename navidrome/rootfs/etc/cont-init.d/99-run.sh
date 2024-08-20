31d3b04646fb4a5ea9097a9f15c826367424d88f
Arunan Balasubramaniam <foss@abala.me>
2024-08-20 08:04:42 +0100

    Navidrome: Expose more Navidrome config options through the addon config

--- navidrome/rootfs/etc/cont-init.d/99-run.sh
+++ navidrome/rootfs/etc/cont-init.d/99-run.sh
@@ -14,6 +14,36 @@ if bashio::config.true 'ssl'; then
     ND_TLSCERT=$(bashio::config 'certfile')
     ND_TLSKEY=$(bashio::config 'keyfile')
 fi
+if bashio::config.has_value 'default_language'; then
+    ND_DEFAULTLANGUAGE=$(bashio::config 'default_language')
+fi
+if bashio::config.has_value 'image_cache_size'; then
+    ND_IMAGECACHESIZE=$(bashio::config 'image_cache_size')
+fi
+if bashio::config.has_value 'lastfm_api_key'; then
+    ND_LASTFM_APIKEY=$(bashio::config 'lastfm_api_key')
+fi
+if bashio::config.has_value 'lastfm_secret'; then
+    ND_LASTFM_SECRET=$(bashio::config 'lastfm_secret')
+fi
+if bashio::config.has_value 'password_encryption_key'; then
+    ND_PASSWORDENCRYPTIONKEY=$(bashio::config 'password_encryption_key')
+fi
+if bashio::config.has_value 'scan_schedule'; then
+    ND_SCANSCHEDULE=$(bashio::config 'scan_schedule')
+fi
+if bashio::config.has_value 'spotify_id'; then
+    ND_SPOTIFY_ID=$(bashio::config 'spotify_id')
+fi
+if bashio::config.has_value 'spotify_secret'; then
+    ND_SPOTIFY_SECRET=$(bashio::config 'spotify_secret')
+fi
+if bashio::config.has_value 'transcoding_cache_size'; then
+    ND_TRANSCODINGCACHESIZE=$(bashio::config 'transcoding_cache_size')
+fi
+if bashio::config.has_value 'welcome_message'; then
+    ND_UIWELCOMEMESSAGE=$(bashio::config 'welcome_message')
+fi
 
 
 ##############
