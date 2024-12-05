563af10ae5ca88ffa3f6a41d1a8505e19197ac75
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-12-05 11:48:49 +0100

    Improve PID

--- .templates/ha_entrypoint.sh
+++ .templates/ha_entrypoint.sh
@@ -59,7 +59,7 @@ if [ "$$" -eq 1 ]; then
         echo "Termination signal received, forwarding to subprocesses..."
         
         # Gracefully terminate open subprocesses
-        for pid in $(ls /proc/*/cmdline 2>/dev/null | grep -oP '/proc/\K[0-9]+'); do
+        for pid in $(grep -l "/etc/cont-init.d" /proc/*/cmdline 2>/dev/null | grep -oP '/proc/\K[0-9]+'); do
             echo "Terminating PID $pid"
             kill -TERM "$pid" 2>/dev/null || true
         done
