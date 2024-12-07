5b0ca00e8160775400fe970da28bbaa10ea93356
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-12-07 15:42:54 +0100

    Update 00-migrate_files.sh

--- nextcloud/rootfs/etc/cont-init.d/00-migrate_files.sh
+++ nextcloud/rootfs/etc/cont-init.d/00-migrate_files.sh
@@ -5,10 +5,10 @@ slug="nextcloud-ocr"
 if [ -f "/homeassistant/addons_config/$slug/config.yaml" ] && [ ! -f "/homeassistant/addons_config/$slug/migrated" ]; then
     bashio::log.warning "Migrating config.yaml"
     mv "/homeassistant/addons_config/$slug"/* /config/ || true
-    echo "Migrated to internal config folder accessible at /addon_configs/xxx-nextcloud" > "/homeassistant/addons_config/$slug/migrated"
+    echo "Migrated to internal config folder accessible at /addon_configs/xxx-$slug" > "/homeassistant/addons_config/$slug/migrated"
 fi
 
 if [ -f "/homeassistant/addons_autoscripts/$slug.sh" ]; then
     bashio::log.warning "Migrating autoscript"
-    mv /homeassistant/addons_autoscripts/nextcloud-ocr.sh /config/ || true
+    mv /homeassistant/addons_autoscripts/$slug.sh /config/ || true
 fi
