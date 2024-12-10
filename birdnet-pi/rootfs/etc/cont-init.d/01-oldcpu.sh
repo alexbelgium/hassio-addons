014da9ef4f23f28fdca5469298e6b7dec9c8f31d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-12-10 13:59:33 +0100

    Update 01-oldcpu.sh

--- birdnet-pi/rootfs/etc/cont-init.d/01-oldcpu.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/01-oldcpu.sh
@@ -7,6 +7,6 @@ if lscpu | grep -q avx2; then
     bashio::log.warning "NON SUPPORTED CPU DETECTED"
     bashio::log.warning "Your cpu doesn't support avx2, the analyzer service will likely won't work"
     bashio::log.warning "Trying to install tensorflow instead of tflite_runtime instead"
-    $PYTHON_VIRTUAL_ENV pip3 uninstall -y tflite_runtime
-    $PYTHON_VIRTUAL_ENV pip3 install tensorflow
+    $PYTHON_VIRTUAL_ENV /usr/bin/pip3 uninstall -y tflite_runtime
+    $PYTHON_VIRTUAL_ENV /usr/bin/pip3 install tensorflow
 fi
