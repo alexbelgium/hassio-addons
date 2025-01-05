991e212f4ee4267371006029d5985bc6b8bdacdf
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-01-05 21:15:08 +0100

    Update launcher.sh

--- webtrees/rootfs/etc/scripts/launcher.sh
+++ webtrees/rootfs/etc/scripts/launcher.sh
@@ -2,14 +2,21 @@
 # shellcheck shell=bash
 set -e
 
-if bashio::services.available 'mysql'; then
-    bashio::log.green "---"
-    bashio::log.yellow "MariaDB addon discovered on your system. If you want to use it, you need to use those values when doing the initial startup wizard, or modify manually the config.ini.php file in /config/data (mapped to /addon_configs/xxx-webtrees/data when accessing using a third party tool)"
-    bashio::log.blue "Host-name : $(bashio::services "mysql" "host")"
-    bashio::log.blue "Port : $(bashio::services "mysql" "port")"
-    bashio::log.blue "Database user : $(bashio::services "mysql" "username")"
-    bashio::log.blue "Database password : $(bashio::services "mysql" "password")"
-    bashio::log.blue "Database name : webtrees"
-    bashio::log.blue "Database prefix : wt_"
-    bashio::log.green "---"
+bashio::log.green "---"
+if [ ! -f "%%data_location%%/config.ini.php" ]; then
+    bashio::log.info "First boot : open the UI at $BASE_URL to access the start-up wizard"
+    if bashio::services.available 'mysql'; then
+        bashio::log.info "MariaDB is available, if you want to use it please fill the values below"
+        bashio::log.blue "Host-name : $(bashio::services "mysql" "host")"
+        bashio::log.blue "Port : $(bashio::services "mysql" "port")"
+        bashio::log.blue "Database user : $(bashio::services "mysql" "username")"
+        bashio::log.blue "Database password : $(bashio::services "mysql" "password")"
+        bashio::log.blue "Database name : webtrees"
+        bashio::log.blue "Database prefix : wt_"
+        bashio::log.green "---"
+    else
+        bashio::log.info "As you don't have the MariaDB addon running, you should likely select sqlite as database, when the name webtrees"
+    fi
+else
+    bashio::log.info "Webtrees started. You can access your webui at : %%baseurl%%"
 fi
