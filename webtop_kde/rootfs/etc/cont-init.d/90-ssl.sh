c0e6a1285e44306137d6b3ecec2ba8e22d592e6e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-01-24 12:47:49 +0100

    Update

--- webtop_kde/rootfs/etc/cont-init.d/90-ssl.sh
+++ webtop_kde/rootfs/etc/cont-init.d/90-ssl.sh
@@ -3,20 +3,19 @@
 set -e
 
 if bashio::config.true 'use_own_certs'; then
-    bashio::log.green "Using referenced ssl certificates. Please remember to open the ssl port in the addon options"
+    bashio::log.green "Using referenced ssl certificates to connect with https. Please remember to open the ssl port in the addon options"
     CERTFILE=$(bashio::config 'certfile')
     KEYFILE=$(bashio::config 'keyfile')
 
-    # Validate ssl
-    bashio::config.require.ssl
-
     #Check if files exist
     echo "... checking if referenced files exist"
-    [ ! -f /ssl/"$CERTFILE" ] && bashio::log.fatal "... use_own_certs is true but certificate /ssl/$CERTFILE not found" && bashio::exit.nok
-    [ ! -f /ssl/"$KEYFILE" ] && bashio::log.fatal "... use_own_certs is true but certificate /ssl/$KEYFILE not found" && bashio::exit.nok
+    if [ -f /ssl/"$CERTFILE" ] && [ -f /ssl/"$KEYFILE" ]; then
+        # Add ssl file
+        sed -i "s|/config/data/ssl/cert.pem|/ssl/$CERTFILE|g" "$NGINX_CONFIG"
+        sed -i "s|/config/data/ssl/cert.key|/ssl/$KEYFILE|g" "$NGINX_CONFIG"
+        echo "... done"
+    else
+        bashio::log.warning "...  certificate /ssl/$CERTFILE and /ssl/$KEYFILE and not found, using self-generated certificates"
+    fi
 
-
-    # Add ssl file
-    sed -i "s|/config/data/ssl/cert.pem|/ssl/$(bashio::config 'certfile')|g" "$NGINX_CONFIG"
-    sed -i "s|/config/data/ssl/cert.key|/ssl/$(bashio::config 'keyfile')|g" "$NGINX_CONFIG"
 fi
