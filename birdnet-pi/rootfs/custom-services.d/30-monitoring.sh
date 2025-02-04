f059ddd64acb3a4406b5e364cdce9871f79d359f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-02-04 20:40:06 +0100

    Add log lines

--- birdnet-pi/rootfs/custom-services.d/30-monitoring.sh
+++ birdnet-pi/rootfs/custom-services.d/30-monitoring.sh
@@ -89,6 +89,7 @@ apprisealert() {
     notification+="$stopped_service"
     notification+="<br><b>System:</b> ${SITE_NAME:-$(hostname)}"
     notification+="<br>Available disk space: $(df -h "$HOME/BirdSongs" | awk 'NR==2 {print $4}')"
+    notification+="<br>3 last log lines: $(timeout 15 cat /proc/1/fd/1 | head -n 3)"
     [[ -n "$BIRDNETPI_URL" ]] && notification+="<br><a href=\"$BIRDNETPI_URL\">Access your BirdNET-Pi</a>"
 
     local TITLE="BirdNET-Analyzer Alert"
