ff0e94c5adfcbe5ce618b96b11461b673ffca22d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-02-06 11:00:27 +0100

    Revert

--- fireflyiii_data_importer/rootfs/etc/cont-init.d/98-fix.sh
+++ fireflyiii_data_importer/rootfs/etc/cont-init.d/98-fix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Fix for Nordigen"
-sudo sed -i $"s/'description'.*/\'description\'        => \'\' !== trim((string)(\$entry->getDescription() ?? \'\')) ? \$entry->getDescription() : \'(no description)\',/" /var/www/html/app/Services/Nordigen/Conversion/Routine/GenerateTransactions.php
+sed -i $"s/'description'.*/\'description\'        => \'\' !== trim((string)(\$entry->getDescription() ?? \'\')) ? \$entry->getDescription() : \'(no description)\',/" /var/www/html/app/Services/Nordigen/Conversion/Routine/GenerateTransactions.php
