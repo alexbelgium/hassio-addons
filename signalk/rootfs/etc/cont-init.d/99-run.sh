88566ad039831e33858e820e929428d8b41c9fe6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-02-10 22:17:27 +0100

    Update 99-run.sh

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -6,14 +6,18 @@ set -e
 # Set configuration directory
 mkdir -p /config/.signalk
 if [ -d "/home/node/.signalk" ]; then
+    if [ "$(ls -A /home/node/.signalk)" ]; then
+        cp -r /home/node/.signalk/* /config/.signalk/
+    fi
     rm -r "/home/node/.signalk"
 fi
-ln -sf /config "/home/node/.signalk"
+ln -sf /config/.signalk "/home/node/.signalk"
 
 # Set single user for ssl files
 for files in ssl-key.pem ssl-cert.pem; do
-    if [ -f /config/.signalk/"$files" ]; then
-        chmod -600 /config/.signalk/"$files"
+    if [ -e /config/.signalk/"$files" ]; then
+        chown `${(id -u):}$`(id -g) /config/.signalk/"$files"
+        chmod 600 /config/.signalk/"$files"
     fi
 done
 
