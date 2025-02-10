315d626bf27963a197389d9054d9f523e494a1ff
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-02-10 22:49:07 +0100

    Update 99-run.sh

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -6,20 +6,20 @@ set -e
 # Set configuration directory
 mkdir -p /config/.signalk
 if [ -d "/home/node/.signalk" ]; then
-    if [ "$(ls -A /home/node/.signalk)" ]; then
+    if [ -n "$(find /home/node/.signalk -mindepth 1 -print -quit 2>/dev/null)" ]; then
         cp -r /home/node/.signalk/* /config/.signalk/
     fi
-    rm -r "/home/node/.signalk"
+    rm -rf "/home/node/.signalk"
 fi
 ln -sf /config/.signalk "/home/node/.signalk"
 
-# Set single user for ssl files
+# Set single user for SSL files
 for file in ssl-key.pem ssl-cert.pem; do
-    if [ -e /config/.signalk/"$file" ]; then
-        chown "$(id -u):$(id -g)" /config/.signalk/"$file"
-        chmod 600 /config/.signalk/"$file"
+    if [ -e "/config/.signalk/$file" ]; then
+        chown "$(id -u):$(id -g)" "/config/.signalk/$file"
+        chmod 600 "/config/.signalk/$file"
     fi
 done
 
 bashio::log.info "Starting application"
-/./home/node/signalk/startup.sh
+exec /home/node/signalk/startup.sh
