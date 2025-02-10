fd22b257eb138ade0ccb6c47307f9e388bf7449a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-02-10 17:38:17 +0100

    Use root

--- signalk/rootfs/etc/cont-init.d/99-run.sh
+++ signalk/rootfs/etc/cont-init.d/99-run.sh
@@ -3,30 +3,20 @@
 set -e
 # hadolint ignore=SC2155
 
-# Variables
-USER=root
-
 # Set configuration directory
-if [ -d "/home/$USER/.signalk" ]; then
-    rm -r "/home/$USER/.signalk"
+mkdir -p /config/.signalk
+if [ -d "/home/node/.signalk" ]; then
+    cp -rnf /home/node/.signalk/* /config/.signalk/
+    rm -r "/home/node/.signalk"
 fi
-ln -sf /config "/home/$USER/.signalk"
-chown -R "$USER:$USER" /config
-chown -R "$USER:$USER" "/home/$USER/.signalk"
-if [ -f /config/.signalk/ssl-key.pem ]; then
-    chmod -600 /config/.signalk/ssl-key.pem
-fi
-if [ -f /config/.signalk/ssl-cert.pem ]; then
-    chmod -600 /config/.signalk/ssl-cert.pem
-fi
+ln -sf /config "/home/node/.signalk"
 
-# Define permissions for /dev/ttyUSB
-for device in /dev/ttyUSB /dev/ttyUSB0 /dev/ttyUSB1; do
-    if [ -e "$device" ]; then
-        sudo usermod -a -G root node || true
+# Set single user for ssl files
+for files in ssl-key.pem ssl-cert.pem; do
+    if [ -f /config/.signalk/"$files" ]; then
+        chmod -600 /config/.signalk/"$files"
     fi
 done
 
 bashio::log.info "Starting application"
-
 /./home/node/signalk/startup.sh
