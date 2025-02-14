aad3bcfc2a2598c07e40c8a3330df8d4c60cc402
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-02-14 21:35:59 +0100

    Update 31-checks.sh

--- birdnet-pi/rootfs/etc/cont-init.d/31-checks.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/31-checks.sh
@@ -59,5 +59,6 @@ sed -i "/update_caddyfile/c echo \"yes\"" "$HOME"/BirdNET-Pi/scripts/update_bird
 
 # Execute update_birdnet_snippets
 export RECS_DIR="$HOME/BirdSongs"
+export EXTRACTED="$HOME/BirdSongs/Extracted"
 chmod +777 "$HOME"/BirdNET-Pi/scripts/update_birdnet_snippets.sh
 "$HOME"/BirdNET-Pi/scripts/update_birdnet_snippets.sh
