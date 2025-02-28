b693134a2c734766713070e491cf62a33d3f3d8c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-02-28 07:09:13 +0100

    ntfs-3g-progs

--- .templates/ha_automatic_packages.sh
+++ .templates/ha_automatic_packages.sh
@@ -64,7 +64,7 @@ for files in "/etc/cont-init.d" "/etc/services.d"; do
     COMMAND="mount"
     if grep -q -rnw "$files/" -e "$COMMAND"; then
         [ "$VERBOSE" = true ] && echo "$COMMAND required"
-        [ "$PACKMANAGER" = "apk" ] && PACKAGES="$PACKAGES exfatprogs ntfs-3g squashfs-tools fuse lsblk"
+        [ "$PACKMANAGER" = "apk" ] && PACKAGES="$PACKAGES exfatprogs ntfs-3g ntfs-3g-progs squashfs-tools fuse lsblk"
         [ "$PACKMANAGER" = "apt" ] && PACKAGES="$PACKAGES exfat* ntfs* squashfs-tools util-linux"
         #[ "$PACKMANAGER" = "pacman" ] && PACKAGES="$PACKAGES ntfs-3g"
     fi
