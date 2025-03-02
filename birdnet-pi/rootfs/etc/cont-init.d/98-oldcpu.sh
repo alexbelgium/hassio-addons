bccc34ab5717dd49dfacac858dc299ceac48c9cc
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-03-02 21:44:07 +0100

    Fix /home/pi/.cache/pip

--- birdnet-pi/rootfs/etc/cont-init.d/98-oldcpu.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/98-oldcpu.sh
@@ -9,6 +9,7 @@ if [[ "$(uname -m)" = "x86_64" ]]; then
         bashio::log.warning "NON SUPPORTED CPU DETECTED"
         bashio::log.warning "Your cpu doesn't support avx2, the analyzer service will likely won't work"
         bashio::log.warning "Trying to install tensorflow instead of tflite_runtime instead"
+        mkdir -p /home/pi/.cache/pip
         chown "0:0" /home/pi/.cache/pip
         $PYTHON_VIRTUAL_ENV /usr/bin/pip3 uninstall -y tflite_runtime
         $PYTHON_VIRTUAL_ENV /usr/bin/pip3 install tensorflow
