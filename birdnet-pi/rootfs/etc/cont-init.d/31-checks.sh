d917dd57028c9c2d3ca547b46e8fd5afa761674b
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-03-03 09:23:17 +0100

    Update 31-checks.sh

--- birdnet-pi/rootfs/etc/cont-init.d/31-checks.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/31-checks.sh
@@ -2,6 +2,14 @@
 # shellcheck shell=bash
 set -e
 
+# Save a copy of the script for further restarts
+if [ -f /etc/cont-init.d/99-run.sh ]; then
+    mkdir -p /etc/scripts-init
+    sed -i "s|/etc/cont-init.d|/etc/scripts-init|g" /ha_entrypoint.sh
+    sed -i "/ rm/d" /ha_entrypoint.sh
+    cp /etc/cont-init.d/99-run.sh /etc/scripts-init/
+fi
+
 ######################
 # CHECK BIRDNET.CONF #
 ######################
