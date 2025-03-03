181d0e0454fd02e5506eae787cd612dda851ae61
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-03-03 18:05:28 +0100

    Improve installation command
    https://github.com/alexbelgium/hassio-addons/issues/1798#issuecomment-2694630274

--- birdnet-pi/rootfs/etc/cont-init.d/98-oldcpu.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/98-oldcpu.sh
@@ -10,10 +10,11 @@ if [[ "$(uname -m)" = "x86_64" ]]; then
         bashio::log.warning "Your cpu doesn't support avx2, the analyzer service will likely won't work"
         bashio::log.warning "Trying to install tensorflow instead of tflite_runtime instead"
         mkdir -p /home/pi/.cache/pip
-        chown "0:0" /home/pi/.cache/pip
-        $PYTHON_VIRTUAL_ENV /usr/bin/pip3 uninstall -y tflite_runtime
-        $PYTHON_VIRTUAL_ENV /usr/bin/pip3 install tensorflow
-        chown "$PUID:$PGID" /home/pi/.cache/pip
+        chmod 777 /home/pi/.cache/pip
+        source BirdNET-Pi/birdnet/bin/activate
+        pip3 uninstall -y tflite_runtime
+        pip3 install tensorflow
+        deactivate
     fi
   fi
 fi
