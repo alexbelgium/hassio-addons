51ac1cac90bc5dbd695a35e142ed58a7b854bf53
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-03-06 12:09:41 +0100

    Update 98-oldcpu.sh

--- birdnet-pi/rootfs/etc/cont-init.d/98-oldcpu.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/98-oldcpu.sh
@@ -24,7 +24,7 @@ if [[ "$(uname -m)" = "x86_64" ]]; then
         bashio::log.warning "NON SUPPORTED CPU DETECTED"
         bashio::log.warning "Your cpu doesn't support avx2, the analyzer service will likely won't work"
         bashio::log.warning "Trying to install tensorflow instead of tflite_runtime instead. This might take some time (up to 5 minutes)."
-        bashio::log.warning "You would have much better chance trying Birdnet-Go, which has an addon and a docker image, and supports your cpu"
+        bashio::log.warning "You could try also Birdnet-Go which should supports your cpu"
         source /home/pi/BirdNET-Pi/birdnet/bin/activate
         mkdir -p /home/pi/.cache/pip || true &>/dev/null
         chmod 777 /home/pi/.cache/pip || true &>/dev/null
