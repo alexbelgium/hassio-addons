02c69be3a36235527e23587a68a57f097bf7b175
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-03-06 18:03:22 +0100

    Update 91-nginx_ingress.sh

--- birdnet-pi/rootfs/etc/cont-init.d/91-nginx_ingress.sh
+++ birdnet-pi/rootfs/etc/cont-init.d/91-nginx_ingress.sh
@@ -24,7 +24,7 @@ ingress_entry=$(bashio::addon.ingress_entry)
 
 # Quits if ingress is not active
 if [ -z "$ingress_entry" ]; then
-    bashio::log.warning "Ingress entry is not set, exiting configuration."
+    bashio::log.info "Ingress entry is not set, exiting configuration."
     sed -i "1a sleep infinity" /custom-services.d/02-nginx.sh
     exit 0
 fi
