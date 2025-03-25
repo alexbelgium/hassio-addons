939d26579e85666e79e14a8853212ff25d304a33
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-03-25 11:01:32 +0100

    Update 01-battybirdnet.sh

--- battybirdnet-pi/rootfs/01-battybirdnet.sh
+++ battybirdnet-pi/rootfs/01-battybirdnet.sh
@@ -3,5 +3,5 @@
 set -e
 
 # Remove not working elements
-sed -i "1a exit 0" /etc/cont-init.d/33-mqtt.sh
-sed -i "1a sleep infinity" /custom-services.d/30-monitoring.sh
+#sed -i "1a exit 0" /etc/cont-init.d/33-mqtt.sh
+#sed -i "1a sleep infinity" /custom-services.d/30-monitoring.sh
