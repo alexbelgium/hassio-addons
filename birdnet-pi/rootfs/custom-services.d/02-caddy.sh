5c2b9191b89ddc75cc479cdca148cea965cf1f97
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-06 09:34:24 +0200

    Update 02-caddy.sh

--- birdnet-pi/rootfs/custom-services.d/02-caddy.sh
+++ birdnet-pi/rootfs/custom-services.d/02-caddy.sh
@@ -14,6 +14,10 @@ sed -i "s|/run/php/php-fpm.sock|$sockfile|g" /helpers/caddy_ingress.sh
 sed -i "s|/run/php/php-fpm.sock|$sockfile|g" /etc/caddy/Caddyfile
 sed -i "s|/run/php/php-fpm.sock|$sockfile|g" "$HOME"/BirdNET-Pi/scripts/update_caddyfile.sh
 
+# Set timezone
+TZ_VALUE="$(timedatectl show -p Timezone --value)"
+export TZ="$TZ_VALUE"
+
 # Update caddyfile with password
 /."$HOME"/BirdNET-Pi/scripts/update_caddyfile.sh &>/dev/null || true
 
