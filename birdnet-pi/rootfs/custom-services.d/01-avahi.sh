7e069da12961073a9d8d9019e8407781e5b8903f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-06 09:34:09 +0200

    Update 01-avahi.sh

--- birdnet-pi/rootfs/custom-services.d/01-avahi.sh
+++ birdnet-pi/rootfs/custom-services.d/01-avahi.sh
@@ -4,6 +4,9 @@ until [[ -e /var/run/dbus/system_bus_socket ]]; do
     sleep 1s
 done
 
+TZ_VALUE="$(timedatectl show -p Timezone --value)"
+export TZ="$TZ_VALUE"
+
 echo "Starting service: avahi daemon"
 exec \
     avahi-daemon --no-chroot
