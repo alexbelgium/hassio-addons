2c084d333fa553578253e9259f14216de3bfa570
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-06 09:32:14 +0200

    Update 00-php_pfm.sh

--- birdnet-pi/rootfs/custom-services.d/00-php_pfm.sh
+++ birdnet-pi/rootfs/custom-services.d/00-php_pfm.sh
@@ -4,12 +4,12 @@
 # Correct /config permissions after startup
 chown pi:pi /config
 
-TZ_VALUE="$(timedatectl show -p Timezone --value)"
-export TZ="$TZ_VALUE"
-
 # Waiting for dbus
 until [[ -e /var/run/dbus/system_bus_socket ]]; do
     sleep 1s
 done
+
+TZ_VALUE="$(timedatectl show -p Timezone --value)"
+export TZ="$TZ_VALUE"
 echo "Starting service: php pfm"
 exec /usr/sbin/php-fpm* -F
