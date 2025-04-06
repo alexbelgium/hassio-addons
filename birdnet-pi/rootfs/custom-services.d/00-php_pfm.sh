3d40cb6260f3de3803f6b809770dff3bedaa0bba
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-06 09:31:58 +0200

    Define timezone for php
    https://github.com/alexbelgium/hassio-addons/issues/1664

--- birdnet-pi/rootfs/custom-services.d/00-php_pfm.sh
+++ birdnet-pi/rootfs/custom-services.d/00-php_pfm.sh
@@ -4,6 +4,9 @@
 # Correct /config permissions after startup
 chown pi:pi /config
 
+TZ_VALUE="$(timedatectl show -p Timezone --value)"
+export TZ="$TZ_VALUE"
+
 # Waiting for dbus
 until [[ -e /var/run/dbus/system_bus_socket ]]; do
     sleep 1s
