5049a6ff961140e9c06dbbfa3c860f409ae6081e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-11 17:14:26 +0200

    Update 01-battybirdnet.sh

--- battybirdnet-pi/rootfs/01-battybirdnet.sh
+++ battybirdnet-pi/rootfs/01-battybirdnet.sh
@@ -3,5 +3,10 @@
 set -e
 
 # Remove not working elements
+if bashio::config.true "NO_NOISE_MODEL"; then
+  bashio::log.info "Activating the no_noise models as NO_NOISE_MODEL is true"
+  sed -i "s|server.py --area|server.py --no_noise on --area|g" "$HOME"/BirdNET-Pi/scripts/batnet_analysis.sh
+fi
+
 #sed -i "1a exit 0" /etc/cont-init.d/33-mqtt.sh
 #sed -i "1a sleep infinity" /custom-services.d/30-monitoring.sh
