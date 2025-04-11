6a3a3f865fe70a17d517be1b13c7f53deaf7434c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-04-11 22:36:28 +0200

    Fix gotty

--- battybirdnet-pi/rootfs/01-battybirdnet.sh
+++ battybirdnet-pi/rootfs/01-battybirdnet.sh
@@ -8,5 +8,14 @@ if bashio::config.true "NO_NOISE_MODEL"; then
   sed -i "s|server.py --area|server.py --no_noise on --area|g" "$HOME"/BirdNET-Pi/scripts/batnet_analysis.sh
 fi
 
+# Install gotty for amd64
+if [ "$(uname -m)" == "x86_64" ]; then
+  bashio::log.info "Install gotty for amd64"
+  curl -L https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz | tar xz -C /tmp
+  mv /tmp/gotty /usr/local/bin/gotty
+  chmod +x /usr/local/bin/gotty
+  sed -i "s| -P log| log|g" "$HOME"/BirdNET-Pi/templates/birdnet_log.service
+fi
+
 #sed -i "1a exit 0" /etc/cont-init.d/33-mqtt.sh
 #sed -i "1a sleep infinity" /custom-services.d/30-monitoring.sh
