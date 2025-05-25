4f995c65664df8829027ec31a578a969c954b154
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-05-25 13:15:07 +0200

    Clean

--- calibre_web/rootfs/etc/cont-init.d/00-a_migrate.sh
+++ calibre_web/rootfs/etc/cont-init.d/00-a_migrate.sh
@@ -11,6 +11,3 @@ if [ -d /homeassistant/addons_config/calibre-web ] && [ ! -L /homeassistant/addo
     cp -rf /homeassistant/addons_config/calibre-web/* /config/
     rm -r /homeassistant/addons_config/calibre-web
 fi
-
-# Provide retrocompatibility
-ln -sf /config /homeassistant/addons_config/calibre-web
