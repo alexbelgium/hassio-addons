98a2d927aa1bdc3750a22e1e1a6f3c0b9fff24a6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-06-01 17:01:23 +0200

    Update 99-run.sh

--- immich_frame/rootfs/etc/cont-init.d/99-run.sh
+++ immich_frame/rootfs/etc/cont-init.d/99-run.sh
@@ -2,7 +2,11 @@
 
 bashio::log.info "Starting Immich Frame"
 
-mkdir -p /config/config
+mkdir -p /config/Config
+if [ -f /app/Config]; then
+  rm -r /app/Config
+fi
 ln -sf /app/Config /config/Config
 
+bashio::log.info "Starting app"
 exec dotnet ImmichFrame.WebApi.dll
