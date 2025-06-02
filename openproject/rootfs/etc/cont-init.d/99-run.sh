da974f71f176561a7e9cfc356294771d68d499a0
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-06-02 13:15:11 +0200

    Update 99-run.sh

--- openproject/rootfs/etc/cont-init.d/99-run.sh
+++ openproject/rootfs/etc/cont-init.d/99-run.sh
@@ -4,18 +4,18 @@ bashio::log.info "Starting OpenProject"
 
 # Ensure persistence for PGDATA and asset folders
 for folder in pg assets; do
-    mkdir -p /config/"$folder"
-    if [ -d /data/"$folder" ] && [ "$(ls -A /data/"$folder")" ]; then
+    mkdir -p /data/"$folder"
+    if [ -d /config/"$folder" ] && [ "$(ls -A /config/"$folder")" ]; then
         # Copy only if source is non-empty
-        cp -a /data/"$folder"/. /config/"$folder"/
-        rm -rf /data/"$folder"
+        cp -rf /config/"$folder"/. /data/"$folder"/
+        rm -rf /config/"$folder"
     fi
-    chmod 700 /config/"$folder"
+    chmod 700 /data/"$folder"
 done
 
 echo "Setting permissions"
-mkdir -p /config/assets/files
-chown -R postgres:postgres /config
+mkdir -p /data/assets/files
+chown -R postgres:postgres /data
 
 cd /app || true
 
