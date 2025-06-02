ea077483ff7f01b5dbb0136c5a9d99ae3a1ba87e
ash286 <ash286@gmail.com>
2025-06-02 11:06:46 +0100

    Update 99-run.sh
    If moving pgdata is not important in order enable the config.yaml env exports, we'd let users to decide attachments location by specifying OPENPROJECT_ATTACHMENTS__STORAGE__PATH.

--- openproject/rootfs/etc/cont-init.d/99-run.sh
+++ openproject/rootfs/etc/cont-init.d/99-run.sh
@@ -3,19 +3,19 @@
 bashio::log.info "Starting OpenProject"
 
 # Ensure persistence for PGDATA and asset folders
-for folder in pg assets; do
-    mkdir -p /config/"$folder"
-    if [ -d /data/"$folder" ] && [ "$(ls -A /data/"$folder")" ]; then
+#for folder in pg assets; do
+#    mkdir -p /config/"$folder"
+#    if [ -d /data/"$folder" ] && [ "$(ls -A /data/"$folder")" ]; then
         # Copy only if source is non-empty
-        cp -a /data/"$folder"/. /config/"$folder"/
-        rm -rf /data/"$folder"
-    fi
-    chmod 700 /config/"$folder"
-done
+#        cp -a /data/"$folder"/. /config/"$folder"/
+#        rm -rf /data/"$folder"
+#    fi
+#    chmod 700 /config/"$folder"
+#done
 
-echo "Setting permissions"
-mkdir -p /config/assets/files
-chown -R postgres:postgres /config
+#echo "Setting permissions"
+#mkdir -p /config/assets/files
+#chown -R postgres:postgres /config
 
 cd /app || true
 
