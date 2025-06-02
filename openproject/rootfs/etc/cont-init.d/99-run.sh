a10e3777e955a6d6649d26e75e8da09deb80dcd1
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-06-02 18:39:03 +0200

    Update 99-run.sh

--- openproject/rootfs/etc/cont-init.d/99-run.sh
+++ openproject/rootfs/etc/cont-init.d/99-run.sh
@@ -5,7 +5,7 @@ bashio::log.info "Starting OpenProject"
 # Ensure persistence for PGDATA and asset folders
 for folder in pg assets; do
     mkdir -p /data/"$folder"
-    if [ -d /config/"$folder" ] && [ "$(ls -A /config/"$folder")" ]; then
+    if [ -d /config/"$folder" ] && [ "$(ls -A /config/"$folder"/)" ]; then
         # Copy only if source is non-empty
         cp -rf /config/"$folder"/. /data/"$folder"/
         rm -rf /config/"$folder"
