c29e7f94c25aed6919143b9df42b564a1fed0b35
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-06-09 07:37:23 +0200

    Update 99-run.sh

--- flaresolverr/rootfs/etc/cont-init.d/99-run.sh
+++ flaresolverr/rootfs/etc/cont-init.d/99-run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bashio
 # shellcheck shell=bash
-set -e
+set -euo pipefail
 
-bashio::log.warning "Warning - minimum configuration recommended : 2 cpu cores and 4 GB of memory. Otherwise the system will become unresponsive and crash."
+bashio::log.warning "Warning - minimum configuration recommended: 2 CPU cores and 4 GB of memory. Otherwise the system may become unresponsive or crash."
 
 ##############
 # LAUNCH APP #
 ##############
 
-python -u /app/flaresolverr.py
+exec /usr/bin/dumb-init -- python -u /app/flaresolverr.py
