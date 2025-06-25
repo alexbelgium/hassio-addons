32a384053a5e71bbda995b5fd617edb651afefda
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-06-25 17:19:58 +0200

    Update 00-migrate.sh

--- resiliosync/rootfs/etc/cont-init.d/00-migrate.sh
+++ resiliosync/rootfs/etc/cont-init.d/00-migrate.sh
@@ -9,7 +9,7 @@ config_location="${config_location/\/config/\/homeassistant}"
 
 if [ -f "$config_location/config.yaml" ] && [ ! -f "$config_location/migrated" ]; then
 	bashio::log.warning "Migrating config.yaml"
-	mv "/homeassistant/addons_config/$slug"/* /config/ || true
+	cp -rf "/homeassistant/addons_config/$slug"/* /config/ || true
 	echo "Migrated to internal config folder accessible at /addon_configs/xxx-$slug" >"/homeassistant/addons_config/$slug/migrated"
 fi
 
