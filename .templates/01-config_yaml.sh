932cbdb158b29aa0bbe173b0cea637d1c3171a68
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-07-06 20:44:50 +0200

    Update 01-config_yaml.sh

--- .templates/01-config_yaml.sh
+++ .templates/01-config_yaml.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-#################
-# CODE INJECTOR #
-#################
-
-# Any commands written in this bash script will be executed at addon start
-# See guide here : https://github.com/alexbelgium/hassio-addons/wiki/Add%E2%80%90ons-feature-:-customisation
-
-apk add yamllint || apt-get install -y yamllint
-
 ##################
 # INITIALIZATION #
 ##################
