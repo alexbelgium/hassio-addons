211bcc6556755f115f3cf878795f715c83445e03
Alexandre <alexandre.pary@gmail.com>
2025-07-15 21:32:32 +0200

    test2

--- ente/rootfs/etc/cont-init.d/99-run.sh
+++ ente/rootfs/etc/cont-init.d/99-run.sh
@@ -3,11 +3,16 @@
 # shellcheck disable=SC2155,SC2016
 set -e
 
-bashio::log.info "Starting ente..."
-exec /usr/bin/museum &
+bashio::log.info "Starting services"
 
-bashio::log.info "Starting minio..."
-exec /usr/local/bin/minio server /data --address ":3200" &
-
-bashio::log.info "Starting ente-web..."
-[ -n "$DISABLE_WEB_UI" ] || exec /usr/bin/ente-web
+# Loop through each directory in /etc/services.d/
+for dir in /etc/services.d/*; do
+    # Check if the directory contains a 'run' file
+    if [ -f "$dir/run" ]; then
+        # Execute the 'run' file
+        echo "Starting service in $dir"
+        bash "$dir/run"
+    else
+        echo "No run file found in $dir"
+    fi
+done
