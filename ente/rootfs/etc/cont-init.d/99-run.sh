8f530d391a15c6f41162e39690a3583400d6831d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-07-16 11:01:33 +0200

    Update 99-run.sh

--- ente/rootfs/etc/cont-init.d/99-run.sh
+++ ente/rootfs/etc/cont-init.d/99-run.sh
@@ -41,29 +41,27 @@ if bashio::config.true 'USE_EXTERNAL_DB'; then
 	DB_NAME="$(bashio::config 'DB_DATABASE_NAME')"
 fi
 
-bashio::log.info "Bootstrapping Postgres cluster…"
+bashio::log.info "Postgres-init: waiting for local Postgres..."
+until pg_isready -q -h 127.0.0.1 -p 5432 -U postgres; do
+    sleep 1
+done
 
-# Wait for postgres service (localhost)
-until pg_isready -q -h localhost -p 5432 -U postgres; do
-	bashio::log.info "Waiting for Postgres to accept connections…"
-	sleep 1
-
-	bashio::log.info "Creating role + database if needed…"
-	su - postgres -c psql <<SQL
+bashio::log.info "Postgres-init: creating role/database if missing..."
+# psql search_path safe quoting using dollar-quoted strings for password
+psql -v ON_ERROR_STOP=1 -h 127.0.0.1 -p 5432 -U postgres <<SQL
 DO \$\$
 BEGIN
-   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${DB_USER}') THEN
-      CREATE ROLE ${DB_USER} LOGIN PASSWORD '${DB_PASS}';
+   IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '${DB_USER}') THEN
+      EXECUTE 'CREATE ROLE ${DB_USER} LOGIN PASSWORD ''' || replace('${DB_PASS}','''','''''') || '''';
    END IF;
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}') THEN
-      CREATE DATABASE ${DB_NAME} OWNER ${DB_USER};
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${DB_NAME}') THEN
+      EXECUTE 'CREATE DATABASE ${DB_NAME} OWNER ${DB_USER}';
    END IF;
 END
 \$\$;
 SQL
-	bashio::log.info "Internal Postgres ready."
 
-done
+bashio::log.info "Postgres-init: done."
 
 #################
 # Minio startup #
