332d9b1935c14d8322ae85482320adca1956fa1a
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-07-17 08:04:12 +0200

    Update ha_autoapps.sh

--- .templates/ha_autoapps.sh
+++ .templates/ha_autoapps.sh
@@ -20,12 +20,7 @@ if ! command -v curl >/dev/null 2>/dev/null; then
 fi
 
 # Call apps installer script if needed
-if [ ! -f /ha_automatic_packages.sh ]; then
-	if ! curl -f -L -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/ha_automatic_packages.sh" --output /ha_automatic_packages.sh; then
-		echo "Failed to download the file" >&2
-		exit 1
-	fi
-fi
+curl -f -L -S "https://raw.githubusercontent.com/alexbelgium/hassio-addons/master/.templates/ha_automatic_packages.sh" --output /ha_automatic_packages.sh
 chmod 777 /ha_automatic_packages.sh
 eval /./ha_automatic_packages.sh "${PACKAGES:-}"
 
