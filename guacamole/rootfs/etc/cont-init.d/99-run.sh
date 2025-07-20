dcafa6bae044792eb68754146489e476b9dbec74
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-07-20 16:25:07 +0200

    Update 99-run.sh

--- guacamole/rootfs/etc/cont-init.d/99-run.sh
+++ guacamole/rootfs/etc/cont-init.d/99-run.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-
-for files in /etc/services.d/*; do
-    if [[ -f /etc/services.d/$files/run ]]; then
-        echo "Starting $files"
-        chmod +x /etc/services.d/$files/run
-        exec /etc/services.d/$files/run & sleep 10
-    fi
-done
