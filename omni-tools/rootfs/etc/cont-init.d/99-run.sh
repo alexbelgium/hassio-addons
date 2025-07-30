a59caf0581a8f55ac4ffbc8b02a5642c354f8f4e
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-07-30 10:22:11 +0200

    update

--- omni-tools/rootfs/etc/cont-init.d/99-run.sh
+++ omni-tools/rootfs/etc/cont-init.d/99-run.sh
@@ -7,11 +7,4 @@
 
 # Start omni-tools container content
 bashio::log.info "Starting application"
-/./docker-entrypoint.sh & true
-
-# Wait for app to become available
-bashio::net.wait_for 8096 localhost 900
-
-# Start nginx
-bashio::log.info "Starting NGinx..."
-exec nginx &>/proc/1/fd/1
+/./docker-entrypoint.sh "nginx -g daemon" &>/proc/1/fd/1
