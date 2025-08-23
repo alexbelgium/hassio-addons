f627cbca0619009be00a712ace17d9d4d311fbfb
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-08-23 08:40:25 +0200

    Update 90-ingress.sh

--- calibre/rootfs/etc/cont-init.d/90-ingress.sh
+++ calibre/rootfs/etc/cont-init.d/90-ingress.sh
@@ -16,18 +16,20 @@ mv tmpfile "${NGINX_CONFIG}"
 # Remove ipv6
 sed -i '/listen \[::\]/d' "${NGINX_CONFIG}"
 
-# Use ingress port
+# Add ingress parameters
 sed -i "s|3000|$(bashio::addon.ingress_port)|g" "${NGINX_CONFIG}"
-
-# Put /devmode under the ingress path and fix SUBFOLDER-prefixed paths
-sed -i 's|location /devmode|location SUBFOLDER/devmode|g' "${NGINX_CONFIG}"
-sed -i 's|SUBFOLDER\([A-Za-z]\)|SUBFOLDER/\1|g' "${NGINX_CONFIG}"
-sed -i 's|SUBFOLDER50x\.html|SUBFOLDER/50x.html|g' "${NGINX_CONFIG}"
-sed -i "s|SUBFOLDER|${SUBFOLDER%/}|g" "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a proxy_set_header Accept-Encoding "";' "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a sub_filter_once off;' "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a sub_filter_types *;' "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a sub_filter "vnc/index.html?autoconnect" "vnc/index.html?path=%%path%%/websockify?autoconnect";' "${NGINX_CONFIG}"
+sed -i "s|%%path%%|${SUBFOLDER:1}|g" "${NGINX_CONFIG}"
 
 # Replace placeholders
 sed -i "s|CWS|8082|g" "${NGINX_CONFIG}"
 sed -i "s|REPLACE_HOME|${HOME:-/root}|g" "${NGINX_CONFIG}"
+
+# Correct image
+sed -i "s|SUBFOLDERwebsock|/websock|g" "${NGINX_CONFIG}"
 
 # Enable ingress
 cp /etc/nginx/sites-available/ingress.conf /etc/nginx/sites-enabled
