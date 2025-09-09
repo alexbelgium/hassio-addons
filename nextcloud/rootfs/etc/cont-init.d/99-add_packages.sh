0f3d54f8d81b7250a5230123e4a691246d95e7d6
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-09-09 11:07:45 +0200

    Remove pdfannotate installation from init script
    Removed the installation of the pdfannotate app and its dependencies.

--- nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
+++ nextcloud/rootfs/etc/cont-init.d/99-add_packages.sh
@@ -6,16 +6,6 @@ set -e
 # shellcheck disable=SC2128
 if [ ! -f /app/www/public/occ ]; then cp /etc/cont-init.d/"$(basename "${BASH_SOURCE}")" /scripts/ && exit 0; fi
 
-# Install specific packages
-if [ ! -d /data/config/www/nextcloud/apps/pdfannotate ]; then
-    CURRENT="$PWD"
-    cd /data/config/www/nextcloud/apps || exit
-    git clone https://gitlab.com/nextcloud-other/nextcloud-annotate pdfannotate
-    cd "$CURRENT" || exit
-    apk add --no-cache ghostscript > /dev/null
-    echo "Nextcloud annotate app added to Nextcloud app store"
-fi
-
 # Install specific apps
 if bashio::config.has_value 'additional_apps'; then
     bashio::log.info "Installing additional apps :"
