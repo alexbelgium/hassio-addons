613c1767d5b21b42061ac251102d6bd1d600e7be
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-11-16 20:34:55 +0100

    Enhance NGINX config modifications in 90-ingress.sh
    Added additional sed commands to modify NGINX configuration for Home Assistant ingress.

--- calibre/rootfs/etc/cont-init.d/90-ingress.sh
+++ calibre/rootfs/etc/cont-init.d/90-ingress.sh
@@ -25,7 +25,13 @@ sed -i '/listen \[::\]/d' "${NGINX_CONFIG}"
 sed -i "s|3000|$(bashio::addon.ingress_port)|g" "${NGINX_CONFIG}"
 sed -i "s|SUBFOLDER|${SUBFOLDER}|g" "${NGINX_CONFIG}"
 sed -i "s|CWS|8082|g" "${NGINX_CONFIG}"
+sed -i "s|REPLACE_HOME|${HOME:-/root}|g" "${NGINX_CONFIG}"
 sed -i "s|REPLACE_DOWNLOADS_PATH|${HOME:-/config}|g" "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a proxy_set_header Accept-Encoding "";' "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a sub_filter_once off;' "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a sub_filter_types *;' "${NGINX_CONFIG}"
+sed -i '/proxy_buffering/a sub_filter "vnc/index.html?autoconnect" "vnc/index.html?path=%%path%%/websockify?autoconnect";' "${NGINX_CONFIG}"
+sed -i "s|%%path%%|${SUBFOLDER:1}|g" "${NGINX_CONFIG}"
 
 # Avoid content encoding on proxied responses to keep Selkies happy
 sed -i '/proxy_buffering/a \
