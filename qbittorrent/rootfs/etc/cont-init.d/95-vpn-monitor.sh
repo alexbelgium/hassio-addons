8aa7ce13cebf8c6662150a6eb2dd2c7927ed90b3
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-11-24 22:07:34 +0100

    Update 95-vpn-monitor.sh

--- qbittorrent/rootfs/etc/cont-init.d/95-vpn-monitor.sh
+++ qbittorrent/rootfs/etc/cont-init.d/95-vpn-monitor.sh
@@ -2,18 +2,7 @@
 # shellcheck shell=bash
 set -euo pipefail
 
-vpn_openvpn=false
-vpn_wireguard=false
-
-if bashio::config.true 'openvpn_enabled'; then
-    vpn_openvpn=true
-fi
-
-if bashio::config.true 'wireguard_enabled'; then
-    vpn_wireguard=true
-fi
-
-if [[ "${vpn_openvpn}" != true && "${vpn_wireguard}" != true ]]; then
+if ! bashio::config.true 'openvpn_enabled' && ! bashio::config.true 'wireguard_enabled'; then
     # No VPN enabled: remove monitoring service to avoid unnecessary restarts
     rm -rf /etc/services.d/vpn-monitor
 fi
