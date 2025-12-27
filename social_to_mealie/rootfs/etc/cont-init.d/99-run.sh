05c4f09e5fa5e71b34d0de52e3a4fe9bd1688b27
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-12-27 09:44:57 +0100

    Update 99-run.sh

--- social_to_mealie/rootfs/etc/cont-init.d/99-run.sh
+++ social_to_mealie/rootfs/etc/cont-init.d/99-run.sh
@@ -2,52 +2,6 @@
 # shellcheck shell=bash
 set -e
 
-required_vars=(
-    OPENAI_URL
-    OPENAI_API_KEY
-    TRANSCRIPTION_MODEL
-    TEXT_MODEL
-    MEALIE_URL
-    MEALIE_API_KEY
-)
-
-for var in "${required_vars[@]}"; do
-    if ! bashio::config.has_value "$var"; then
-        bashio::exit.nok "Configuration option $var is required"
-    fi
-    export "$var"="$(bashio::config "$var")"
-    sed -i "1a export $var=$(bashio::config "$var")" /app/entrypoint.sh    
-    bashio::log.info "$var configured"
-done
-
-optional_vars=(
-    MEALIE_GROUP_NAME
-    EXTRA_PROMPT
-    YTDLP_VERSION
-    COOKIES
-)
-
-for var in "${optional_vars[@]}"; do
-    if bashio::config.has_value "$var"; then
-        export "$var"="$(bashio::config "$var")"
-        sed -i "1a export $var=$(bashio::config "$var")" /app/entrypoint.sh
-        bashio::log.info "$var configured"
-    fi
-done
-
-if bashio::config.has_value "env_vars"; then
-    for entry in $(bashio::config 'env_vars | map(@base64) | .[]'); do
-        item=$(echo "$entry" | base64 -d)
-        name=$(echo "$item" | jq -r '.name // empty')
-        value=$(echo "$item" | jq -r '.value // empty')
-        if [ -n "$name" ]; then
-            export "$name"="$value"
-            sed -i "1a export $name=$value" /app/entrypoint.sh
-            bashio::log.info "Custom env $name configured"
-        fi
-    done
-fi
-
 bashio::log.info "Starting Social to Mealie"
 cd /app || bashio::exit.nok "App directory not found"
-exec /bin/sh /app/entrypoint.sh node --run start
+/./app/entrypoint.sh node --run start
