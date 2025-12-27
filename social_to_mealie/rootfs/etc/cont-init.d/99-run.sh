69efb6bccf63aa51dc5f22825a0adaa16b59e80c
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-12-27 20:40:17 +0100

    Fix permissions for entrypoint.sh

--- social_to_mealie/rootfs/etc/cont-init.d/99-run.sh
+++ social_to_mealie/rootfs/etc/cont-init.d/99-run.sh
@@ -4,4 +4,6 @@ set -e
 
 bashio::log.info "Starting Social to Mealie"
 cd /app || bashio::exit.nok "App directory not found"
+chown nextjs /app/entrypoint.sh
+chmod +x /app/entrypoint.sh
 exec gosu nextjs /app/entrypoint.sh node --run start
