6b6e1cd1a0c999f28e825cfdd1761f0f14a0feef
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2026-01-16 11:29:48 +0100

    build

--- brave/rootfs/etc/cont-init.d/90-ingress.sh
+++ brave/rootfs/etc/cont-init.d/90-ingress.sh
@@ -20,10 +20,7 @@ sed -i '/proxy_buffering/a proxy_set_header Accept-Encoding "";' "${NGINX_CONFIG
 sed -i '/proxy_buffering/a sub_filter_once off;' "${NGINX_CONFIG}"
 sed -i '/proxy_buffering/a sub_filter_types *;' "${NGINX_CONFIG}"
 sed -i '/proxy_buffering/a sub_filter "vnc/index.html?autoconnect" "vnc/index.html?path=%%path%%/websockify?autoconnect";' "${NGINX_CONFIG}"
-sed -i "s|%%path%%|${SUBFOLDER:1}|g" "${NGINX_CONFIG}"
-
-# Correct image
-sed -i "s|SUBFOLDERwebsockify|/websockify|g" "${NGINX_CONFIG}"
+sed -i "s|SUBFOLDER|${SUBFOLDER}|g" "${NGINX_CONFIG}"
 
 # Enable ingress
 cp "${NGINX_CONFIG}" /etc/nginx/sites-enabled
