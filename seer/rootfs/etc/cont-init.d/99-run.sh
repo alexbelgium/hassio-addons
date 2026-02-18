e64fbdce3b6a0715247279b46502f16a2d12a5d0
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2026-02-18 09:18:02 +0100

    Refactor config handling in 99-run.sh
    Remove old migration logic and update config paths.

--- seer/rootfs/etc/cont-init.d/99-run.sh
+++ seer/rootfs/etc/cont-init.d/99-run.sh
@@ -2,25 +2,11 @@
 # shellcheck shell=bash
 set -e
 
-slug=seer
-
-if [ -d "/homeassistant/addons_config/$slug" ]; then
-    echo "Migrating /homeassistant/addons_config/$slug to /config"
-    cp -rnf /homeassistant/addons_config/"$slug"/. /config/ || true
-    mv /homeassistant/addons_config/"$slug" /homeassistant/addons_config/"$slug"_migrated
-fi
-
-if [ -d /config/addons_config/seer ]; then
-    echo "Migrating /config/addons_config/seer to /config"
-    cp -rnf /config/addons_config/seer/. /config/ || true
-fi
-
-if [ -d /config/addons_config ]; then
-    rm -rf /config/addons_config
-fi
+rm -r /app/config
+ln -sf /config /app/config
 
 chown -R "$PUID:$PGID" /config || true
 
-cd /config || true
+cd /app || true
 
 npm start
