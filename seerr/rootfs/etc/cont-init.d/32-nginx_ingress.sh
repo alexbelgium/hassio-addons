49187afb7f9db42f350e3c4cd258415ae0c4d666
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2026-02-20 10:52:15 +0100

    Update 32-nginx_ingress.sh

--- seerr/rootfs/etc/cont-init.d/32-nginx_ingress.sh
+++ seerr/rootfs/etc/cont-init.d/32-nginx_ingress.sh
@@ -16,6 +16,6 @@ ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s|%%port%%|${ingress_port}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%interface%%|${ingress_interface}|g" /etc/nginx/servers/ingress.conf
 sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
-sed -i "s|%%ingress_entry_escaped%%|${ingress_entry//\//\\\/}|g" /etc/nginx/servers/ingress.conf
+sed -i "s|%%ingress_entry_escaped%%|${ingress_entry//\//\\\\\/}|g" /etc/nginx/servers/ingress.conf
 
 bashio::log.info "Nginx ingress configured on ${ingress_interface}:${ingress_port}"
