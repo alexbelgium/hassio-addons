382988a9e7481321fa1fd8f1afe5772140f020de
root <root@a0d7b954-ssh.local.hass.io>
2026-03-16 16:15:40 -0400

    lint issues

--- netalertx/rootfs/etc/cont-init.d/91-configure.sh
+++ netalertx/rootfs/etc/cont-init.d/91-configure.sh
@@ -10,7 +10,7 @@ set -e
 # 1. Fix the directories
 for folder in /tmp/run/tmp /tmp/api /tmp/log /tmp/run /tmp/nginx/active-config "${TMP_DIR:-/tmp}" "${NETALERTX_DATA:-/data}" "${NETALERTX_DB:-/data/db}" "${NETALERTX_CONFIG:-/data/config}"; do
     mkdir -p "$folder"
-    chown -R ${PUID}:${PGID} "$folder"
+    chown -R "${PUID}":"${PGID}" "$folder"
     chmod -R 755 "$folder"
 done
 
@@ -21,17 +21,17 @@ chmod 666 /dev/stdout /dev/stderr
 
 # 3. Pre-create and chown log files
 touch /tmp/log/app.php_errors.log /tmp/log/cron.log /tmp/log/stdout.log /tmp/log/stderr.log
-chown ${PUID}:${PGID} /tmp/log/*.log
+chown "${PUID}":"${PGID}" /tmp/log/*.log
 
 # 4. Create Symlinks
 for item in db config; do
     # ADD THESE TWO LINES: Ensure the target exists and is owned by 20211
     mkdir -p "/config/$item"
-    chown -R ${PUID}:${PGID} "/config/$item"
+    chown -R "${PUID}":"${PGID}" "/config/$item"
 
     rm -rf "/data/$item"
     ln -sf "/config/$item" "/data/$item"
-    chown -R ${PUID}:${PGID} "/data/$item"
+    chown -R "${PUID}":"${PGID}" "/data/$item"
     chmod -R 755 "/data/$item"
 done
 
