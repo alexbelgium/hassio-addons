0214053b745411c25dc52c7f0f8973496735858b
petruknw <github19-06-24@weymouth.id.au>
2026-05-20 23:11:37 +1000

    revert: undo entrypoint override (config uses pre-built image, no Docker build)

--- aurral/rootfs/usr/local/bin/docker-entrypoint.sh
+++ aurral/rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-# Replacement for the upstream docker-entrypoint.sh.
-# The upstream version runs chown -R on /app/backend/data which fails when
-# that path is a symlink to a host-mounted HA volume (Operation not permitted).
-# All setup (symlinks, directory creation) is handled by the s6 run script.
 exec "$@"
