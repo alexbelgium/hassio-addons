ef4f1a1ef76cf83d504b8057b11cb96c66f3b79c
petruknw <github19-06-24@weymouth.id.au>
2026-05-21 23:28:16 +1000

    aurral: replace bashio with jq to read /data/options.json

--- aurral/rootfs/usr/local/bin/docker-entrypoint.sh
+++ aurral/rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-DOWNLOAD_FOLDER=$(bashio::config 'download_folder')
-WEEKLY_FLOW_SUFFIX=$(bashio::config 'weekly_flow_folder')
+DOWNLOAD_FOLDER=$(jq -r '.download_folder // "/share/aurral/downloads"' /data/options.json)
+WEEKLY_FLOW_SUFFIX=$(jq -r '.weekly_flow_folder // "weekly-flow"' /data/options.json)
 
 export DOWNLOAD_FOLDER
 export WEEKLY_FLOW_FOLDER="${DOWNLOAD_FOLDER}/${WEEKLY_FLOW_SUFFIX}"
