6cf7dc4a6ed1f180253ee274ad32694c5b187819
petruknw <github19-06-24@weymouth.id.au>
2026-05-21 23:37:39 +1000

    aurral: remove hardcoded fallbacks from entrypoint mkdirs

--- aurral/rootfs/usr/local/bin/docker-entrypoint.sh
+++ aurral/rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-# HA Supervisor injects addon options as env vars automatically.
-# DOWNLOAD_FOLDER and WEEKLY_FLOW_FOLDER are set by HA from options.json,
-# with the Dockerfile ENV values as fallbacks.
-
 mkdir -p /config/data
-mkdir -p "${DOWNLOAD_FOLDER:-/share/aurral/downloads}"
-mkdir -p "${WEEKLY_FLOW_FOLDER:-/share/aurral/downloads/weekly-flow}"
+mkdir -p "${DOWNLOAD_FOLDER}"
+mkdir -p "${WEEKLY_FLOW_FOLDER}"
 
 exec "$@"
