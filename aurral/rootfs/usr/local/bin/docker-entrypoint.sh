cf329eab0d874bba645def5d1e13e9a95ff534fe
petruknw <github19-06-24@weymouth.id.au>
2026-05-21 23:30:28 +1000

    aurral: revert to env vars injected by HA supervisor

--- aurral/rootfs/usr/local/bin/docker-entrypoint.sh
+++ aurral/rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -e
 
-DOWNLOAD_FOLDER=$(jq -r '.download_folder // "/share/aurral/downloads"' /data/options.json)
-WEEKLY_FLOW_SUFFIX=$(jq -r '.weekly_flow_folder // "weekly-flow"' /data/options.json)
+# HA Supervisor injects addon options as env vars automatically.
+# DOWNLOAD_FOLDER and WEEKLY_FLOW_FOLDER are set by HA from options.json,
+# with the Dockerfile ENV values as fallbacks.
 
-export DOWNLOAD_FOLDER
-export WEEKLY_FLOW_FOLDER="${DOWNLOAD_FOLDER}/${WEEKLY_FLOW_SUFFIX}"
-
-# Create persistent directories in HA volumes
 mkdir -p /config/data
-mkdir -p "${DOWNLOAD_FOLDER}"
-mkdir -p "${WEEKLY_FLOW_FOLDER}"
+mkdir -p "${DOWNLOAD_FOLDER:-/share/aurral/downloads}"
+mkdir -p "${WEEKLY_FLOW_FOLDER:-/share/aurral/downloads/weekly-flow}"
 
 exec "$@"
