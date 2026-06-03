5176c6f4bc6ae85b69706ff35abc72d15038831d
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2026-06-03 14:30:54 +0200

    Update 99-run.sh

--- filebrowser_quantum/rootfs/etc/cont-init.d/99-run.sh
+++ filebrowser_quantum/rootfs/etc/cont-init.d/99-run.sh
@@ -80,7 +80,7 @@ yq e -i ".server.port = 8080"            "$FILEBROWSER_CONFIG"
 yq e -i ".server.listen = \"0.0.0.0\""  "$FILEBROWSER_CONFIG"
 yq e -i ".server.database = \"/config/database.db\"" "$FILEBROWSER_CONFIG"
 yq e -i ".server.cacheDir = \"/cache\""  "$FILEBROWSER_CONFIG"
-yq e -i ".server.source.path = \"/\""  "$FILEBROWSER_CONFIG"
+yq e -i ".server.sources.path = \"/\""  "$FILEBROWSER_CONFIG"
 
 # --- Base URL (from env or config) ---
 bashio::log.info "... set base URL to allow ingress"
