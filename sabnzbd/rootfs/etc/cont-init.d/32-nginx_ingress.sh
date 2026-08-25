cc7527e40f9c550bd308c0336b0cdd8782639032
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2026-08-25 15:49:11 +0200

    Revert "feat(sabnzbd): enable Home Assistant ingress (#3019)"
    This reverts commit 8341e542fb6b357411476c4bad6af15251830235.

--- sabnzbd/rootfs/etc/cont-init.d/32-nginx_ingress.sh
+++ sabnzbd/rootfs/etc/cont-init.d/32-nginx_ingress.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
+# shellcheck disable=SC2317
 set -e
 
 #################
 # NGINX SETTING #
 #################
 
+exit 0
+
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
-ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-sed -i "s|%%ingress_entry%%|${ingress_entry}|g" /etc/nginx/servers/ingress.conf
+
+# Allows serving js
+sed -i 's/<!-- %if-not-debug% -->/<!-- %if-not-debug%  /g' /app/sabnzbd/webui/index.html
+sed -i 's/<!-- %end% -->/   %end% -->/g' /app/sabnzbd/webui/index.html
+sed -i 's/<!-- %if-debug%/<!-- %if-debug% -->/g' /app/sabnzbd/webui/index.html
+sed -i 's/	%end% -->/<!-- 	%end% -->/g' /app/sabnzbd/webui/index.html
